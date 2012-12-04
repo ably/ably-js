b619a89162126b5e399178c423331a2dfcfca5f5
Paddy Byers <paddy.byers@gmail.com>
2012-12-04 19:05:36 +0000

    Updates to make swf location relative

--- common/lib/transport/defaults.js
+++ common/lib/transport/defaults.js
@@ -4,14 +4,11 @@ var Defaults = {
 	WS_HOST:           'realtime.ably.io',
 	WS_PORT:           80,
 	WSS_PORT:          443,
-	CDN_HOST:          'rest.ably.io',
-	CDN_HTTP_PORT:     80,
-	CDN_HTTPS_PORT:    443,
-	CDN_STATIC_PATH:   '/static/0/',
 	connectTimeout:    15000,
 	disconnectTimeout: 30000,
 	suspendedTimeout:  120000,
 	cometRecvTimeout:  90000,
 	cometSendTimeout:  10000,
-	transports:        ['web_socket', 'flash_socket', 'xhr', 'jsonp']
+	transports:        ['web_socket', 'flash_socket', 'xhr', 'jsonp'],
+	flashTransport:   {swfLocation: 'swf/WebSocketMainInsecure.swf'}
 };
