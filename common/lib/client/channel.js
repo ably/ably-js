d6bc94d108ab250237ac77fb3df345629e02f3bb
Paddy Byers <paddy.byers@gmail.com>
2013-01-03 15:51:08 +0000

    Add log statements; move payload->data

--- common/lib/client/channel.js
+++ common/lib/client/channel.js
@@ -1,5 +1,4 @@
 var Channel = (function() {
-	var noop = function() {};
 
 	/* public constructor */
 	function Channel(rest, name, options) {
@@ -11,20 +10,18 @@ var Channel = (function() {
 	Utils.inherits(Channel, EventEmitter);
 
 	Channel.prototype.presence = function(params, callback) {
-		Resource.get(this, '/channels/' + this.name + '/presence', params, callback);
+		Logger.logAction(Logger.LOG_MICRO, 'Channel.presence()', 'channel = ' + this.name);
+		Resource.get(this.rest, '/channels/' + this.name + '/presence', params, callback);
 	};
 
 	Channel.prototype.history = function(params, callback) {
-		Resource.get(this, '/channels/' + this.name + '/events', params, callback);
-	};
-
-	Channel.prototype.stats = function(params, callback) {
-		Resource.get(this, '/channels/' + this.name + '/stats', params, callback);
+		Logger.logAction(Logger.LOG_MICRO, 'Channel.history()', 'channel = ' + this.name);
+		Resource.get(this.rest, '/channels/' + this.name + '/history', params, callback);
 	};
 
 	Channel.prototype.publish = function(name, data, callback) {
-		Logger.logAction(Logger.LOG_MICRO, 'Channel.publish()', 'name = ' + name);
-		Resource.post(this.rest, '/channels/' + this.name + '/publish', {name:name, payload:data}, callback);
+		Logger.logAction(Logger.LOG_MICRO, 'Channel.publish()', 'channel = ' + this.name + '; name = ' + name);
+		Resource.post(this.rest, '/channels/' + this.name + '/publish', {name:name, data:data}, callback);
 	};
 
 	return Channel;
