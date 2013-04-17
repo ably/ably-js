031c927afe1369fe04a567adb557a65f25173223
Paddy Byers <paddy.byers@gmail.com>
2013-04-17 07:35:15 +0100

    Relocated transports that are browser-specific

---
+++ browser/lib/transport/jsonptransport.js
@@ -0,0 +1,98 @@
+var JSONPTransport = (function() {
+
+	/* public constructor */
+	function JSONPTransport(connectionManager, auth, params) {
+		params.binary = false;
+		CometTransport.call(this, connectionManager, auth, params);
+		Ably._ = {};
+	}
+	Utils.inherits(JSONPTransport, CometTransport);
+
+	JSONPTransport.isAvailable = function() { return true; };
+	ConnectionManager.httpTransports.jsonp = ConnectionManager.transports.jsonp = JSONPTransport;
+
+	/* connectivity check; since this has a hard-coded callback id,
+	 * we just make sure that we handle concurrent requests (but the
+	 * connectionmanager should ensure this doesn't happen anyway */
+	var checksInProgress = null;
+	JSONPTransport.checkConnectivity = function(callback) {
+		if(checksInProgress) {
+			checksInProgress.push(callback);
+			return;
+		}
+		checksInProgress = [callback];
+		new JSONPTransport.Request('http://live.cdn.ably-realtime.com/is-the-internet-up.js', null, null, false, 'isTheInternetUp', function(err, response) {
+			var result = !err && response;
+			for(var i = 0; i < checksInProgress.length; i++) checksInProgress[i](null, result);
+			checksInProgress = null;
+		});
+	};
+
+	JSONPTransport.tryConnect = function(connectionManager, auth, params, callback) {
+		var transport = new JSONPTransport(connectionManager, auth, params);
+		var errorCb = function(err) { callback(err); };
+		transport.on('error', errorCb);
+		transport.on('preconnect', function() {
+			Logger.logAction(Logger.LOG_MINOR, 'JSONPTransport.tryConnect()', 'viable transport ' + transport);
+			transport.off('error', errorCb);
+			callback(null, transport);
+		});
+		transport.connect();
+	};
+
+	JSONPTransport.prototype.toString = function() {
+		return 'JSONPTransport; uri=' + this.baseUri + '; isConnected=' + this.isConnected;
+	};
+
+	JSONPTransport.prototype.request = function(uri, params, body, expectToBlock, callback) {
+		return new JSONPTransport.Request(uri, params, body, expectToBlock, false, callback);
+	};
+
+	var requestId = 0;
+	JSONPTransport.Request = function(uri, params, body, expectToBlock, binary /* ignored */, callback) {
+		var _ = Ably._;
+		this.callback = callback;
+		var thisId = this.requestId = requestId++;
+
+		var timeout = expectToBlock ? Defaults.cometRecvTimeout : Defaults.cometSendTimeout;
+		var timer = this.timer = setTimeout(timeout, function() { self.abort(); });
+
+		params = params || {};
+		params.callback = 'Ably._._' + thisId;
+		if(body)
+			params.body = encodeURIComponent(body);
+		else
+			delete params.body;
+
+		var script = document.createElement('script');
+		script.async = true;
+		script.onerror = function() { self.abort(); };
+		script.src = CometTransport.paramStr(params, uri);
+
+		var self = this;
+		Ably._['_' + thisId] = function(message) {
+			clearTimeout(timer);
+			delete _['_' + thisId];
+			if(self.aborted)
+				return;
+			script.parentNode.removeChild(script);
+			callback(null, message);
+		};
+
+		var insertAt = document.getElementsByTagName('script')[0];
+	    insertAt.parentNode.insertBefore(script, insertAt);
+	    this.script = script;
+	};
+
+	JSONPTransport.Request.prototype.abort = function() {
+		/* No abort possible, but flag this request
+		 * so no action occurs if it does complete */
+		clearTimeout(this.timer);
+		this.aborted = true;
+		delete Ably._['_' + this.requestId];
+		this.callback(new Error('JSONPTransport: requestId ' + this.requestId + ' aborted'));
+	};
+	Http.Request = JSONPTransport.Request;
+
+	return JSONPTransport;
+})();
---
+++ browser/lib/transport/xhrtransport.js
@@ -0,0 +1,128 @@
+var XHRTransport = (function() {
+
+	var createXHR = function() {
+		var result = new XMLHttpRequest();
+		if ('withCredentials' in result)
+			return result;
+
+		if(typeof XDomainRequest !== "undefined")
+			/* Use IE-specific "CORS" code with XDR */
+			return new XDomainRequest();
+
+		return null;
+	};
+
+	/* public constructor */
+	function XHRTransport(connectionManager, auth, params) {
+		params.binary = params.binary && XHRTransport.binary;
+		CometTransport.call(this, connectionManager, auth, params);
+	}
+	Utils.inherits(XHRTransport, CometTransport);
+
+	var isAvailable;
+	XHRTransport.isAvailable = function() {
+		var xhr = createXHR();
+		if(!xhr)return false;
+//		XHRTransport.binary = (window.ArrayBuffer && xhr.responseType);
+		XHRTransport.binary = false;
+		return true;
+	};
+
+	XHRTransport.checkConnectivity = function(callback) {
+		new XHRTransport.Request('http://live.cdn.ably-realtime.com/is-the-internet-up.txt', null, null, false, function(err, responseText) {
+			callback(null, (!err && responseText == 'yes'));
+		});
+	};
+
+	XHRTransport.tryConnect = function(connectionManager, auth, params, callback) {
+		var transport = new XHRTransport(connectionManager, auth, params);
+		var errorCb = function(err) { callback(err); };
+		transport.on('error', errorCb);
+		transport.on('preconnect', function() {
+			Logger.logAction(Logger.LOG_MINOR, 'XHRTransport.tryConnect()', 'viable transport ' + transport);
+			transport.off('error', errorCb);
+			callback(null, transport);
+		});
+		transport.connect();
+	};
+
+	XHRTransport.prototype.request = function(uri, params, body, expectToBlock, callback) {
+		return new XHRTransport.Request(uri, params, body, expectToBlock, this.binary, callback);
+	};
+
+	XHRTransport.prototype.toString = function() {
+		return 'XHRTransport; uri=' + this.baseUri + '; isConnected=' + this.isConnected;
+	};
+
+	XHRTransport.Request = function(uri, params, body, expectToBlock, binary, callback) {
+		uri = CometTransport.paramStr(params, uri);
+		var successCode, method, err, timedout;
+		if(body) method = 'POST', successCode = 201;
+		else method = 'GET', successCode = 200;
+
+		var xhr = this.xhr = createXHR();
+		if(binary)
+			xhr.responseType = 'arraybuffer';
+
+		var timeout = expectToBlock ? Defaults.cometRecvTimeout : Defaults.cometSendTimeout;
+		var timer = setTimeout(timeout, function() { timedout = true; xhr.abort(); });
+		xhr.open(method, uri, true);
+		xhr.setRequestHeader('Accept', binary ? 'application/x-thrift' : 'application/json');
+		xhr.onerror = function(err) {
+			err = err;
+			err.code = 80000;
+			callback(err);
+		};
+		xhr.onabort = function() {
+			err = new Error(timedout ? 'Request timed out' : 'Request cancelled');
+			err.statusCode = 404;
+			err.code = 80000;
+			callback(err);
+		};
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState == 4) {
+				clearTimeout(timer);
+				if(err) {
+					callback(err);
+					return;
+				}
+				if(xhr.status == successCode) {
+					var response = null;
+					if(binary) {
+						if(xhr.response) {
+							response = new Buffer();
+							response.buf = xhr.response;
+							response.view = new DataView(response.buf);
+						}
+					} else {
+						response = xhr.responseText;
+					}
+					callback(null, response);
+					return;
+				}
+				if(xhr.status != 0) {
+					err = new Error('Unexpected response: statusCode = ' + xhr.status);
+					err.statusCode = xhr.status;
+					err.code = 80000;
+					err.statusText = xhr.statusText;
+					callback(err);
+					return;
+				}
+				/* statusCode is 0, so expect either an onerror or onabort callback */
+			}
+		};
+		xhr.send(body);
+	};
+
+	XHRTransport.Request.prototype.abort = function() {
+		if(this.xhr)
+			this.xhr.abort();
+	};
+
+	if(XHRTransport.isAvailable()) {
+		ConnectionManager.httpTransports.xhr = ConnectionManager.transports.xhr = XHRTransport;
+		Http.Request = XHRTransport.Request;
+	}
+
+	return XHRTransport;
+})();
