2cf94c21d780463b776c1b5736f1c60b250c8f60
Paddy Byers <paddy.byers@gmail.com>
2013-11-07 10:33:46 +0000

    Add rest host

--- test/browser-srv/framework/testvars.js
+++ test/browser-srv/framework/testvars.js
@@ -1,4 +1,5 @@
 /* put any test variables in here */
 exports.realtimeHost = 'staging-realtime.ably.io';
+exports.restHost = 'staging-rest.ably.io';
 exports.realtimePort = 80;
 exports.realtimeTlsPort = 443;
