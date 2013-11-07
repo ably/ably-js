f868274f329c71a2937f2c3170e8ca473f1e8833
Paddy Byers <paddy.byers@gmail.com>
2013-11-07 10:32:12 +0000

    Correct indentation only

--- test/browser/init.js
+++ test/browser/init.js
@@ -4,31 +4,31 @@
 var _init = {};
 var init = this.init = {};
 
-init.initbase0 = function(test) {
+init.initbase0 = function (test) {
 	test.expect(1);
 	try {
 		var timeout,
-				ably = sharedTests.realtimeConnection();
-		ably.connection.on('connected', function() {
+			ably = sharedTests.realtimeConnection();
+		ably.connection.on('connected', function () {
 			clearTimeout(timeout);
 			test.ok(true, 'Verify init with key');
 			test.done();
 			ably.close();
 		});
-		['failed', 'suspended'].forEach(function(state) {
-			ably.connection.on(state, function() {
+		['failed', 'suspended'].forEach(function (state) {
+			ably.connection.on(state, function () {
 				clearTimeout(timeout);
 				test.ok(false, 'Connection to server failed');
 				test.done();
 				ably.close();
 			});
 		});
-		timeout = setTimeout(function() {
-      test.ok(false, 'Timed out: Trying to connect took longer than expected');
-      test.done();
-      ably.close();
-    }, 10 * 1000);
-	} catch(e) {
+		timeout = setTimeout(function () {
+			test.ok(false, 'Timed out: Trying to connect took longer than expected');
+			test.done();
+			ably.close();
+		}, 10 * 1000);
+	} catch (e) {
 		test.ok(false, 'Init with key failed with exception: ' + e.stack);
 		test.done();
 	}
