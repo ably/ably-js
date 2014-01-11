acb8bed6bd5dbe06db20ee42ed87c8e119d85bc2
Paddy Byers <paddy.byers@gmail.com>
2014-01-11 22:15:53 +0000

    Regenerated

--- browser/lib/ably.js
+++ browser/lib/ably.js
@@ -8037,6 +8037,7 @@ var Channel = (function() {
 		this.rest = rest;
     	this.name = name;
 		this.cipher = null;
+		this.presence = new Presence();
 	}
 	Utils.inherits(Channel, EventEmitter);
 
@@ -8051,31 +8052,6 @@ var Channel = (function() {
 		} else {
 			callback(null, this.cipher = null);
 		}
-	};
-
-	Channel.prototype.presence = function(params, callback) {
-		Logger.logAction(Logger.LOG_MICRO, 'Channel.presence()', 'channel = ' + this.name);
-		/* params and callback are optional; see if params contains the callback */
-		if(callback === undefined) {
-			if(typeof(params) == 'function') {
-				callback = params;
-				params = null;
-			} else {
-				callback = noop;
-			}
-		}
-		var rest = this.rest, binary = !rest.options.useTextProtocol;
-		var headers = Utils.copy(Utils.defaultGetHeaders(binary));
-		if(rest.options.headers)
-			Utils.mixin(headers, rest.options.headers);
-		Resource.get(rest, '/channels/' + this.name + '/presence', headers, params, function(err, res) {
-			if(err) {
-				callback(err);
-				return;
-			}
-			if(binary) PresenceMessage.decodeTPresenceArray(res, callback);
-			else callback(null, res);
-		});
 	};
 
 	Channel.prototype.history = function(params, callback) {
@@ -8096,7 +8072,7 @@ var Channel = (function() {
 
 		if(rest.options.headers)
 			Utils.mixin(headers, rest.options.headers);
-		Resource.get(rest, '/channels/' + this.name + '/history', headers, params, function(err, res) {
+		Resource.get(rest, '/channels/' + this.name + '/messages', headers, params, function(err, res) {
 			if(err) {
 				callback(err);
 				return;
@@ -8128,7 +8104,76 @@ var Channel = (function() {
 		var headers = Utils.copy(Utils.defaultPostHeaders(binary));
 		if(rest.options.headers)
 			Utils.mixin(headers, rest.options.headers);
-		Resource.post(rest, '/channels/' + this.name + '/publish', requestBody, headers, null, callback);
+		Resource.post(rest, '/channels/' + this.name + '/messages', requestBody, headers, null, callback);
+	};
+
+	function Presence(channel) {
+		this.channel = channel;
+	}
+
+	Presence.prototype.get = function(params, callback) {
+		Logger.logAction(Logger.LOG_MICRO, 'Channel.presence.get()', 'channel = ' + this.name);
+		/* params and callback are optional; see if params contains the callback */
+		if(callback === undefined) {
+			if(typeof(params) == 'function') {
+				callback = params;
+				params = null;
+			} else {
+				callback = noop;
+			}
+		}
+		var channel = this.channel,
+			rest = channel.rest,
+			binary = !rest.options.useTextProtocol,
+			headers = Utils.copy(Utils.defaultGetHeaders(binary));
+
+		if(rest.options.headers)
+			Utils.mixin(headers, rest.options.headers);
+
+		Resource.get(rest, '/channels/' + channel.name + '/presence', headers, params, function(err, res) {
+			if(err) {
+				callback(err);
+				return;
+			}
+			if(binary) PresenceMessage.decodeTPresenceArray(res, callback);
+			else callback(null, res);
+		});
+	};
+
+	Presence.prototype.history = function(params, callback) {
+		Logger.logAction(Logger.LOG_MICRO, 'Channel.presence.history()', 'channel = ' + this.name);
+		/* params and callback are optional; see if params contains the callback */
+		if(callback === undefined) {
+			if(typeof(params) == 'function') {
+				callback = params;
+				params = null;
+			} else {
+				callback = noop;
+			}
+		}
+		var channel = this.channel,
+			rest = channel.rest,
+			binary = !rest.options.useTextProtocol,
+			headers = Utils.copy(Utils.defaultGetHeaders(binary));
+
+		if(rest.options.headers)
+			Utils.mixin(headers, rest.options.headers);
+
+		Resource.get(rest, '/channels/' + channel.name + '/presence/history', headers, params, function(err, res) {
+			if(err) {
+				callback(err);
+				return;
+			}
+			try {
+				var messages = Serialize.TMessageArray.decode(res, binary);
+				for(var i = 0; i < messages.length; i++) {
+					messages[i].data = Data.fromTData(messages[i].data);
+				}
+				callback(null, messages);
+			} catch(err) {
+				callback(err);
+			}
+		});
 	};
 
 	return Channel;
