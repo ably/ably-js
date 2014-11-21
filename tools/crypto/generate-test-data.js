5a9f28fef6515654d2308459cb510b92282f59e2
Paddy Byers <paddy.byers@gmail.com>
2014-11-21 09:53:54 +0000

    Updates for new data layout, and emit test data

--- tools/crypto/generate-test-data.js
+++ tools/crypto/generate-test-data.js
@@ -3,19 +3,30 @@
 /************************************************************
  * Usage
  *
- * generate-test-data [key=<hex-encoded key>] [data=<data>] [encoding=<encoding>] [iv=<hex-encoded iv>]
+ * generate-test-data [key=<hex-encoded key>] [keylen=<length of key to generate>] [data=<data>] [encoding=<encoding>] [iv=<hex-encoded iv>] [save=true|false] [verbose=true|false]
  ************************************************************/
 
 var crypto = require('crypto'),
+	msgpack = require('msgpack-js'),
 	util = require('util'),
 	ably = require('../../nodejs/rest'),
-	TType = require('../../nodejs/lib/protocol/clientmessage_types').TType,
 	hexy = require('hexy'),
-	DEFAULT_ALGORITHM = "aes",
+	DEFAULT_ALGORITHM = 'aes',
 	DEFAULT_KEYLENGTH = 128,  // bits
+	DEFAULT_MODE = 'cbc',
 	DEFAULT_BLOCKLENGTH = 16, // bytes
 	MAX_INT = Math.pow(2, 31) - 1,
 	MIN_INT = -Math.pow(2, 31);
+
+function mixin(target, src) {
+	for(var prop in src)
+		target[prop] = src[prop];
+	return target;
+}
+
+function copy(src) {
+	return mixin({}, src);
+}
 
 /**
  * Generate a buffer of secure random bytes of the given length
@@ -51,47 +62,8 @@ function padPlaintext(plaintext) {
 	return Buffer.concat([plaintext, padding]);
 }
 
-/**
- * Convert a given value to a TData (Thrift) representation
- * @param value
- * @returns {{}}
- */
-function toTData(value) {
-	var result = {};
-	switch(typeof(value)) {
-		case 'string':
-			result.type = TType.STRING;
-			result.stringData = value;
-			break;
-		case 'number':
-			if(value % 1 === 0 && value >= MIN_INT && value <= MAX_INT) {
-				result.type = TType.INT32;
-				result.i32Data = value;
-			} else {
-				result.type = TType.DOUBLE;
-				result.doubleData = value;
-			}
-			break;
-		case 'object':
-			if(Array.isArray(value)) {
-				result.type = TType.JSONARRAY;
-				result.stringData = JSON.stringify(value);
-			} else if(Buffer.isBuffer(value)) {
-				result.type = TType.BUFFER;
-				result.binaryData = value;
-			} else {
-				result.type = TType.JSONOBJECT;
-				result.stringData = JSON.stringify(value);
-			}
-			break;
-		default:
-			throw new Error('Unable to convert data value (' + String(value) + ')');
-	}
-	return result;
-}
-
 function hexdump(buffer) {
-	console.log(hexy.hexy(buffer)+ '(' + buffer.length + ' bytes)');
+	verboseOutput(hexy.hexy(buffer)+ '(' + buffer.length + ' bytes)');
 }
 
 /**
@@ -108,20 +80,43 @@ function usage(str, err) {
  *              Process arguments
  ************************************************/
 
-var key,
-	keylength,
+var algorithm = DEFAULT_ALGORITHM,
+	mode = DEFAULT_MODE,
+	keylength = DEFAULT_KEYLENGTH,
+	key,
+	iv,
 	data,
 	encoding,
-	iv;
+	save,
+	verbose;
 
 for(var i = 1; i < process.argv.length; i++) {
 	var optMatch = process.argv[i].match(/^\s*(\w+)\s*=\s*(.+)$/);
 	if(optMatch) {
 		switch(optMatch[1]) {
+			case 'algorithm':
+				algorithm = optMatch[2];
+				if(algorithm != 'aes')
+					usage('Unrecognised algorithm: ' + algorithm);
+				break;
+			case 'mode':
+				mode = optMatch[2];
+				if(mode != 'cbc')
+					usage('Unrecognised mode: ' + mode);
+				break;
+			case 'keylen':
+				keylength = Number(optMatch[2]);
+				break;
 			case 'key':
 				try {
 					var keyHex = optMatch[2].replace(/ /g, '');
 					key = new Buffer(keyHex, 'hex');
+				} catch(e) { usage(e.message); }
+				break;
+			case 'iv':
+				try {
+					var ivHex = optMatch[2].replace(/ /g, '');
+					iv = new Buffer(ivHex, 'hex');
 				} catch(e) { usage(e.message); }
 				break;
 			case 'data':
@@ -140,6 +135,12 @@ for(var i = 1; i < process.argv.length; i++) {
 					iv = new Buffer(ivHex, 'hex');
 				} catch(e) { usage(e.message); }
 				break;
+			case 'save':
+				save = (optMatch[2] == 'true');
+				break;
+			case 'verbose':
+				verbose = (optMatch[2] == 'true');
+				break;
 			default:
 				usage('Unrecognised option name: ' + optMatch[1]);
 		}
@@ -148,113 +149,160 @@ for(var i = 1; i < process.argv.length; i++) {
 
 /* generate key if none specified */
 if(!key) {
-	key = generateRandom(DEFAULT_KEYLENGTH / 8);
-	console.log('Generating key:');
+	key = generateRandom(keylength / 8);
+	verboseOutput('Generating key:');
 	hexdump(key);
-	console.log('\n');
+	verboseOutput('\n');
 }
-keylength = key.length * 8;
 
 /* generate iv if none specified */
 if(!iv) {
 	iv = generateRandom(DEFAULT_BLOCKLENGTH);
-	console.log('Generating iv:');
+	verboseOutput('Generating iv:');
 	hexdump(iv);
-	console.log('\n');
+	verboseOutput('\n');
 }
 
 /* use given data and encoding, or sample of data of various types */
+var items;
 if(data) {
 	if(encoding)
 		data = new Buffer(data, encoding);
-
-	try {
-		generate_test_data_for(data);
-	} catch(e) { usage(e.message); }
-
+	items = [data];
 } else {
-	[
-		12345,
-		1.2345,
+	items = [
 		'The quick brown fox jumped over the lazy dog',
 		new Buffer('000102030405060708090a0b0c0d0e0f', 'hex'),
-		{example:{json:'object'}},
+		{example: {json: 'Object'}},
 		['example', 'json', 'array']
-	].forEach(function(data) { generate_test_data_for(data); });
+	];
+}
+items = items.map(function(data) {
+	try {
+		return generate_test_data_for(data);
+	} catch(e) {
+		usage(e.message);
+		/* exits ... */
+	}
+});
+
+if(save) {
+	process.stdout.write(JSON.stringify({
+		algorithm: algorithm,
+		mode: mode,
+		keylength: keylength,
+		key: key.toString('base64'),
+		iv: iv.toString('base64'),
+		items: items
+	}, null, '\t'));
 }
 
+function verboseOutput(data) {
+	if(verbose) console.error(data);
+}
 /************************************************
  * Step by step encryption process for given data
  ************************************************/
 
 function generate_test_data_for(data) {
-	console.log('*******************************************************************');
-	console.log('Generating test data for: ' + util.inspect(data));
-	console.log('\n');
+	verboseOutput('*******************************************************************');
+	verboseOutput('Generating test data for: ' + util.inspect(data));
+	verboseOutput('\n');
 
-	/* unencrypted format (JSON) */
-	var unencrypted_json = {
+	/* unencrypted/unencoded Message */
+	var unencoded_message = {
 		name: 'example',
 		data: data
 	};
-	if(Buffer.isBuffer(data)) {
-		unencrypted_json.data = data.toString('base64');
-		unencrypted_json.encoding = 'base64';
+
+	verboseOutput('Unencoded message:');
+	verboseOutput(util.inspect(unencoded_message));
+	verboseOutput('\n');
+
+	/* encode */
+	var encoded_message = copy(unencoded_message);
+	var isString = (typeof(data) == 'string');
+	var isBuffer = Buffer.isBuffer(data);
+	var encoding = null;
+	if(!isString && !isBuffer) {
+		encoded_message.data = data = JSON.stringify(data);
+		encoded_message.encoding = encoding = 'json';
+		isString = true;
 	}
-	console.log('Unencrypted JSON message format:');
-	console.log(util.inspect(unencrypted_json));
-	console.log('\n');
 
-	/* unencrypted format (Thrift)*/
-	var unencrypted_thrift = toTData(data),
-		tdata_type = unencrypted_thrift.type;
+	verboseOutput('Encoded message:');
+	verboseOutput(util.inspect(encoded_message));
+	verboseOutput('\n');
 
-	console.log('Unencrypted Thrift message format:');
-	console.log('TData(' + util.inspect(unencrypted_thrift, 1) + ')');
-	console.log('\n');
+	/* JSON-encoded representation if not encrypted */
+	var encoded_json = copy(encoded_message);
+	if(isBuffer) {
+		encoded_json.data = data.toString('base64');
+		encoded_json.encoding = (encoding ? (encoding + '/') : '') + 'base64';
+	}
+	verboseOutput('Unencrypted JSON message format:');
+	verboseOutput(JSON.stringify(encoded_json));
+	verboseOutput('\n');
+
+	/* unencrypted format msgpack */
+	verboseOutput('Unencrypted msgpack message format:');
+	hexdump(msgpack.encode(encoded_message));
+	verboseOutput('\n');
 
 	/* plaintext without padding */
-	console.log('Plaintext before encryption (without padding):');
-	var plaintext = ably.Crypto.Data.asPlaintext(unencrypted_thrift);
+	verboseOutput('Plaintext before encryption (without padding):');
+	var plaintext = data;
+	if(isString) {
+		plaintext = new Buffer(plaintext);
+		encoding = (encoding ? (encoding + '/') : '') + 'utf-8';
+	}
 	hexdump(plaintext);
-	console.log('\n');
+	verboseOutput('\n');
 
 	/* plaintext including padding */
-	console.log('Plaintext before encryption (including padding):');
+	verboseOutput('Plaintext before encryption (including padding):');
 	var paddedPlaintext = padPlaintext(plaintext);
 	hexdump(paddedPlaintext);
-	console.log('\n');
+	verboseOutput('\n');
 
 	/* cipher output */
-	console.log('Raw cipher output:');
+	verboseOutput('Raw cipher output:');
 	var cipher = crypto.createCipheriv(DEFAULT_ALGORITHM + keylength, key, iv);
 	var cipherOut = cipher.update(paddedPlaintext);
 	hexdump(cipherOut);
-	console.log('\n');
+	verboseOutput('\n');
 
 	/* encrypted data */
-	console.log('Encrypted value, iv + cipher output:');
+	verboseOutput('Encrypted value, iv + cipher output:');
 	var encryptedData = Buffer.concat([iv, cipherOut]);
 	hexdump(encryptedData);
-	console.log('\n');
+	verboseOutput('\n');
+
+	/* encrypted message */
+	var encrypted_message = {
+		name: 'example',
+		data: encryptedData,
+		encoding: (encoding ? (encoding + '/') : '') + 'cipher+' + [algorithm, keylength, mode].join('-')
+	};
+	verboseOutput('Encrypted message format:');
+	verboseOutput(util.inspect(encrypted_message));
+	verboseOutput('\n');
 
 	/* encrypted format (JSON) */
-	var encrypted_json = {
-		name: 'example',
-		data: encryptedData.toString('base64'),
-		encoding: 'cipher+base64',
-		type: tdata_type
-	};
-	console.log('Encrypted JSON message format:');
-	console.log(util.inspect(encrypted_json));
-	console.log('\n');
+	var encrypted_json = copy(encrypted_message);
+	encrypted_json.data = encrypted_message.data.toString('base64');
+	encrypted_json.encoding = encrypted_message.encoding + '/base64'
+	verboseOutput('Encrypted JSON message format:');
+	verboseOutput(JSON.stringify(encrypted_json));
+	verboseOutput('\n');
 
-	/* encrypted format (Thrift)*/
-	var encrypted_thrift = {
-		type: tdata_type,
-		cipherData: encryptedData
+	/* encrypted format (msgpack)*/
+	verboseOutput('Encrypted msgpack message format:');
+	hexdump(msgpack.encode(encrypted_message));
+	verboseOutput('\n');
+
+	return {
+		encoded: encoded_json,
+		encrypted: encrypted_json
 	};
-	console.log('Encrypted Thrift message format:');
-	console.log('TData(' + util.inspect(encrypted_thrift) + ')');
-	console.log('\n');
 }
