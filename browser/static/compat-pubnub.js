4e934093dd2fde4dd034fe272597626c1b84b8f2
Paddy Byers <paddy.byers@gmail.com>
2014-12-06 16:40:05 +0000

    Regenerated

--- browser/static/compat-pubnub.js
+++ browser/static/compat-pubnub.js
@@ -234,7 +234,7 @@
 					var h = result[i];
 					if (!startTime)
 						startTime = endTime = h.timestamp;
-					presults.push(JSON.parse(h.data));
+					presults.push(h.data);
 				}
 				callback([presults, startTime, endTime]);
 			}
@@ -281,7 +281,7 @@
 	 */
 	PUBNUB.publish = function(args, callback) {
 		callback = callback || args.callback || noop;
-		var message = JSON.stringify(args.message);
+		var message = args.message;
 		var channel = args.channel;
 		var error = args.error || noop;
 		var timestamp = Date.now();
@@ -371,16 +371,7 @@
 
 		var subscr = function(channel, callback, args) {
 			if(subscriptions[channel]) return log('Already Connected');
-			var cb = function(message) {
-				var data;
-				try {
-					data = JSON.parse(message.data);
-				} catch(e) {
-					log('PUBNUB.subscribe: Error: '+e);
-					return;
-				}
-				callback(data);
-			};
+			var cb = function(message) { callback(message.data); };
 
 			// Create channel and register for message callbacks and presence events if necessary
 			var ablyChannel = getChannel(channel);
--- browser/static/compat-pusher.js
+++ browser/static/compat-pusher.js
@@ -463,7 +463,7 @@ var Utils = (function() {
 		var tlsorigin = options.tlshost || '';
 		var encrypted = options.encrypted || false;
 		var opts = {
-			key: applicationKey, tls: encrypted//, log: {level:4}
+			key: applicationKey, tls: encrypted, log: {level:4}
 		};
 		if (options.ablyClientId) opts.clientId = options.ablyClientId;
 		if (options.authEndpoint) opts.authUrl = options.authEndpoint;
@@ -620,7 +620,7 @@ var Utils = (function() {
 
 		this.channel.subscribe(function(message) {
 			log('PusherChannel::message callback: Event object '+JSON.stringify(this)+', message '+JSON.stringify(message));
-			self.channel.emit(message.name, JSON.parse(message.data));
+			self.channel.emit(message.name, message.data);
 		});
 		this.bindings = {};
 		this.bind_alls = [];
@@ -744,8 +744,7 @@ var Utils = (function() {
 	 * whether or not the trigger was successful.
 	 */
 	PusherChannel.prototype.trigger = function(event, data) {
-		data = JSON.stringify(data);
-		log('PusherChannel::trigger: Event '+event+', data '+ data);
+		log('PusherChannel::trigger: Event '+event+', data '+JSON.stringify(data));
 		if (!this.active) { log('PusherChannel::trigger: Inactive'); return true; }
 		this.channel.publish(event, data);
 		return true;
