98c1a48bcffd2a8070ce1bdcfc69240a243f9365
Paddy Byers <paddy.byers@gmail.com>
2014-12-07 14:34:17 +0000

    Support conditional inclusion of Crypto

--- browser/ably-epilogue.js
+++ browser/ably-epilogue.js
@@ -1,7 +1,7 @@
 window.Ably.Realtime = Realtime;
 Realtime.ConnectionManager = ConnectionManager;
 Realtime.BufferUtils = Rest.BufferUtils = BufferUtils;
-Realtime.Crypto = Rest.Crypto = Crypto;
+if(typeof(Crypto) !== 'undefined') Realtime.Crypto = Rest.Crypto = Crypto;
 Realtime.Message = Rest.Message = Message;
 Realtime.PresenceMessage = Rest.PresenceMessage = PresenceMessage;
 Realtime.ProtocolMessage = Rest.ProtocolMessage = ProtocolMessage;
