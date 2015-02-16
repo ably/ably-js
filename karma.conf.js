1b5224082edf554c4f4d5b1f1459e1b4f40354c3
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-02-16 17:49:11 +0000

    Set up sensible timeouts

--- karma.conf.js
+++ karma.conf.js
@@ -71,6 +71,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
+    browserNoActivityTimeout: 3600000,
 
 
     // Continuous Integration mode
