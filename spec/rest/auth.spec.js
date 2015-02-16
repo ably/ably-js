8775a6deb0f5853ed35c45d26d2b4c259c40a705
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-02-16 20:02:56 +0000

    First test against Ably.Rest library

--- spec/rest/auth.spec.js
+++ spec/rest/auth.spec.js
@@ -1,35 +1,51 @@
 "use strict";
 
-define(['ably', 'spec/common/testapp'], function(ably, testAppModule) {
-  var testApp;
+define(['ably', 'testapp'], function(Ably, testAppModule) {
+  describe('REST Auth', function() {
+    var testApp;
 
-  beforeAll(function(done) {
-    testAppModule.setup(function(err, newTestApp) {
-      if (err) {
-        throw "Could not set up Test App: " + JSON.stringify(err);
-      } else {
-        testApp = newTestApp;
-        done();
-      }
+    beforeAll(function(done) {
+      testAppModule.setup(function(err, newTestApp) {
+        if (err) {
+          throw "Could not set up Test App: " + JSON.stringify(err);
+        } else {
+          testApp = newTestApp;
+          done();
+        }
+      });
     });
-  });
 
-  afterAll(function(done) {
-    testAppModule.tearDown(testApp, function(err) {
-      if (err) {
-        throw "Could not tear down Test App: " + JSON.stringify(err);
-      } else {
-        testApp = null;
-        done();
-      }
+    afterAll(function(done) {
+      testAppModule.tearDown(testApp, function(err) {
+        if (err) {
+          throw "Could not tear down Test App: " + JSON.stringify(err);
+        } else {
+          testApp = null;
+          done();
+        }
+      });
     });
-  });
 
-  describe("REST Auth", function() {
-    it('should have a valid TestApp', function(done) {
-      expect(testApp).toBeDefined();
-      expect(testApp.appId).toBeDefined();
-      done();
+    var stripQualifier = function(qualifiedKeyId) {
+      return qualifiedKeyId.split('.')[1];
+    };
+
+    var AblyRest = function(options) {
+      var keyIndex = options.key || 0;
+      return new Ably.Rest({ key: testApp['key' + keyIndex + 'Str'], useBinaryProtocol: false, wsHost: 'sandbox-realtime.ably.io', host: 'sandbox-rest.ably.io' });
+    }
+
+    describe('Ably.Rest#time', function() {
+      it('obtains the server time', function(done) {
+        AblyRest({ key: 0 }).time(function(err, time) {
+          if(err) {
+            throw err;
+          }
+          var timeOffset = time - Date.now();
+          expect(Math.abs(timeOffset)).toBeLessThan(5000);
+          done();
+        });
+      }, 5000);
     });
   });
 });
