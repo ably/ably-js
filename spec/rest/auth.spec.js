b4702a3aba0b097ae99e90d9ed2c2c1a52f84061
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-02-17 04:53:47 +0000

    Sample Auth spec conversion

--- spec/rest/auth.spec.js
+++ spec/rest/auth.spec.js
@@ -7,17 +7,33 @@ define(['ably', 'testapp_module', 'client_module'], function(Ably, testAppHelper
   afterAll(testAppHelper.tearDown);
 
   describe('REST Auth', function() {
+    var currentTime; // set by first test
+
     describe('Ably.Rest#time', function() {
       it('obtains the server time', function(done) {
-        clientHelper.AblyRealtime({ key: 0 }).time(function(err, time) {
-          if(err) {
-            throw err;
-          }
+        clientHelper.AblyRest({ key: 0 }).time(function(err, time) {
+          if(err) { throw err; }
           var timeOffset = time - Date.now();
           expect(Math.abs(timeOffset)).toBeLessThan(5000);
+          currentTime = Math.floor(time/1000);
           done();
         });
       }, 5000);
+    });
+
+    describe('#requestToken', function() {
+      it('generates a valid new token', function(done) {
+        clientHelper.AblyRest({ key: 0 }).auth.requestToken(function(err, tokenDetails) {
+          if(err) { throw err; }
+
+          expect(tokenDetails.id).toBeDefined();
+          expect(tokenDetails.issued_at).toBeGreaterThan(currentTime - 1);
+          expect(tokenDetails.expires).toBeGreaterThan(tokenDetails.issued_at);
+          expect(tokenDetails.expires).toEqual(60*60 + tokenDetails.issued_at);
+          expect(tokenDetails.capability).toEqual({'*':['*']});
+          done();
+        });
+      });
     });
   });
 });
