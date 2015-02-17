905eccf7e6e4a3545b3c46a091b82efc68201092
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-02-17 20:18:29 +0000

    Add support for assert method
    Will help significantly when migrating across existing tests to the new suite as they use test.* assertions which are compatible with chai assert.* assertions.

--- spec/support/node_helper.js
+++ spec/support/node_helper.js
@@ -2,3 +2,14 @@ var reporters = require('jasmine-reporters');
 jasmine.getEnv().addReporter(new reporters.TerminalReporter({ verbosity: 3, color: true }));
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+
+var chai = require('chai');
+global.assert = chai.assert;
+
+var util = require('util');
+global.fail = function(error, message) {
+  console.error("Error! " + util.inspect(error));
+  if (error.stack) { console.error(error.stack); }
+  if (message) { console.error(message); };
+  assert.notOk(true, 'failed');
+}
