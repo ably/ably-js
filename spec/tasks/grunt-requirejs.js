b07f0bc556e88aea11f908cc3d755059a446bac3
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-02-26 14:45:34 +0000

    Allow nodeunit:webserver to live reload the specified tests
    Tests to run specified in karma.conf.js are now reloaded live from the web server which helps with testing

--- spec/tasks/grunt-requirejs.js
+++ spec/tasks/grunt-requirejs.js
@@ -2,33 +2,14 @@
 
 var fs = require('fs'),
     path = require('path'),
-    glob = require('glob'),
-    karmaConfigModule = require('../../karma.conf.js');
+    browserTestFiles = require('./lib/browser_test_files');
 
 module.exports = function (grunt) {
-  var requireFiles = {};
-
-  var config = {
-    set: function(karmaConf) {
-      karmaConf.files.forEach(function(fileSpec) {
-        var pattern = fileSpec.pattern;
-
-        if ((fileSpec.included === false) && pattern) {
-          glob.sync(path.resolve(__dirname, '../..', pattern)).forEach(function(file) {
-            requireFiles[path.relative(path.resolve(__dirname, '../..'), file)] = true;
-          });
-        }
-      });
-    }
-  };
-
-  karmaConfigModule(config);
-
   grunt.registerTask('requirejs',
     'Write out the RequireJS dependencies to the grunt-html-runner.js config file',
     function() {
       var body = "window.__karma__ = { base: '../' };\n";
-      body += "window.__karma__.files = " + JSON.stringify(requireFiles) + ";"
+      body += "window.__karma__.files = " + JSON.stringify(browserTestFiles()) + ";"
       fs.writeFileSync(path.resolve(__dirname, '../support', 'browser_file_list.js'), body);
     });
 };
---
+++ spec/tasks/lib/browser_test_files.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var path = require('path'),
+    glob = require('glob');
+
+module.exports = function () {
+  // Clear the Require cache for live updates of the Karma conf
+  for (var cacheKey in require.cache) {
+    if (/karma\.conf/.test(cacheKey)) {
+      delete require.cache[cacheKey];
+    }
+  }
+
+  var requireFiles = {},
+      karmaConfigModule = require('../../../karma.conf.js');
+
+  var config = {
+    set: function(karmaConf) {
+      karmaConf.files.forEach(function(fileSpec) {
+        var pattern = fileSpec.pattern;
+
+        if ((fileSpec.included === false) && pattern) {
+          glob.sync(path.resolve(__dirname, '../../..', pattern)).forEach(function(file) {
+            requireFiles[path.relative(path.resolve(__dirname, '../../..'), file)] = true;
+          });
+        }
+      });
+    }
+  };
+
+  karmaConfigModule(config);
+
+  return requireFiles;
+};
