66fcc8b46a07cd5968cc76804dddca89c287babf
Paddy Byers <paddy.byers@gmail.com>
2015-03-05 14:13:01 +0000

    Pass Ably object as this into IIFE

--- browser/ably-epilogue.js
+++ browser/ably-epilogue.js
@@ -1,9 +1,9 @@
-window.Ably.Realtime = Realtime;
-window.Ably.Rest = Rest;
+Ably.Realtime = Realtime;
+Ably.Rest = Rest;
 Realtime.ConnectionManager = ConnectionManager;
 Realtime.BufferUtils = Rest.BufferUtils = BufferUtils;
 if(typeof(Crypto) !== 'undefined') Realtime.Crypto = Rest.Crypto = Crypto;
 Realtime.Message = Rest.Message = Message;
 Realtime.PresenceMessage = Rest.PresenceMessage = PresenceMessage;
 Realtime.ProtocolMessage = Rest.ProtocolMessage = ProtocolMessage;
-})();
+}).call({});
