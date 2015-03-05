0900e5de2a20cacdfa50aeba419a6323598060c7
Paddy Byers <paddy.byers@gmail.com>
2015-03-05 16:11:40 +0000

    Conditionally include exports for ably.js

--- browser/ably-epilogue.js
+++ browser/ably-epilogue.js
@@ -1,9 +1,11 @@
-Ably.Realtime = Realtime;
-Ably.Rest = Rest;
-Realtime.ConnectionManager = ConnectionManager;
-Realtime.BufferUtils = Rest.BufferUtils = BufferUtils;
-if(typeof(Crypto) !== 'undefined') Realtime.Crypto = Rest.Crypto = Crypto;
-Realtime.Message = Rest.Message = Message;
-Realtime.PresenceMessage = Rest.PresenceMessage = PresenceMessage;
-Realtime.ProtocolMessage = Rest.ProtocolMessage = ProtocolMessage;
+if(typeof Realtime !== 'undefined') {
+	Ably.Rest = Rest;
+	Ably.Realtime = Realtime;
+	Realtime.ConnectionManager = ConnectionManager;
+	Realtime.BufferUtils = Rest.BufferUtils = BufferUtils;
+	if(typeof(Crypto) !== 'undefined') Realtime.Crypto = Rest.Crypto = Crypto;
+	Realtime.Message = Rest.Message = Message;
+	Realtime.PresenceMessage = Rest.PresenceMessage = PresenceMessage;
+	Realtime.ProtocolMessage = Rest.ProtocolMessage = ProtocolMessage;
+}
 }).call({});
