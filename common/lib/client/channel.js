c6b28e844e8313905c51e515e8487771b9c8b0ea
Simon Woolf <simon@ably.io>
2015-08-03 14:46:21 +0100

    Move Presence#history to rt presence object, have RestPresence call it
    So that presence#history is available to both realtime and rest channels

--- common/lib/client/channel.js
+++ common/lib/client/channel.js
@@ -119,28 +119,7 @@ var Channel = (function() {
 	};
 
 	RestPresence.prototype.history = function(params, callback) {
-		Logger.logAction(Logger.LOG_MICRO, 'Channel.presence.history()', 'channel = ' + this.channel.name);
-		/* params and callback are optional; see if params contains the callback */
-		if(callback === undefined) {
-			if(typeof(params) == 'function') {
-				callback = params;
-				params = null;
-			} else {
-				callback = noop;
-			}
-		}
-		var rest = this.channel.rest,
-			format = rest.options.useBinaryProtocol ? 'msgpack' : 'json',
-			envelope = Http.supportsLinkHeaders ? undefined : format,
-			headers = Utils.copy(Utils.defaultGetHeaders(format)),
-			options = this.channel.options;
-
-		if(rest.options.headers)
-			Utils.mixin(headers, rest.options.headers);
-
-		(new PaginatedResource(rest, this.basePath + '/history', headers, envelope, function(body, headers, unpacked) {
-			return PresenceMessage.fromResponseBody(body, options, !unpacked && format);
-		})).get(params, callback);
+		Presence.prototype.history.call(this, params, callback);
 	};
 
 	return Channel;
