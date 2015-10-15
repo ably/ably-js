e825e50a917980458c5b66b98bcf7f3eaf8b995a
Simon Woolf <simon@ably.io>
2015-10-15 11:56:32 +0100

    saucelabs-all: don’t need three tranches, can run up to 5 tests at once

--- test/bin/ci-saucelabs-all.sh
+++ test/bin/ci-saucelabs-all.sh
@@ -10,20 +10,15 @@ echo "\n$GREEN -> Installing all dependencies $NO_COLOUR"
 npm install
 npm install grunt-cli
 
-echo "\n$GREEN -> Running Karma test suite (Sauce Labs - part 1/3) $NO_COLOUR"
-$(npm bin)/grunt test:karma --browsers sl_chrome_42,sl_chrome_35,sl_firefox_37
+echo "\n$GREEN -> Running Karma test suite (Sauce Labs - part 1/2) $NO_COLOUR"
+$(npm bin)/grunt test:karma --browsers sl_chrome_42,sl_chrome_35,sl_firefox_37,sl_firefox_31
 karma_exitstatus1=$?
 
-echo "\n$GREEN -> Running Karma test suite (Sauce Labs - part 2/3) $NO_COLOUR"
-$(npm bin)/grunt test:karma --browsers sl_firefox_31,sl_ie_11
+echo "\n$GREEN -> Running Karma test suite (Sauce Labs - part 2/2) $NO_COLOUR"
+$(npm bin)/grunt test:karma --browsers sl_ios_safari_9_1,sl_ios_safari_8_4,sl_android_5_0,sl_ie_11
 karma_exitstatus2=$?
-
-echo "\n$GREEN -> Running Karma test suite (Sauce Labs - part 3/3) $NO_COLOUR"
-$(npm bin)/grunt test:karma --browsers sl_ios_safari_9_1,sl_ios_safari_8_4,sl_android_5_0
-karma_exitstatus3=$?
 
 if [[ $karma_exitstatus1 -ne 0 ]]; then exit $karma_exitstatus1; fi
 if [[ $karma_exitstatus2 -ne 0 ]]; then exit $karma_exitstatus2; fi
-if [[ $karma_exitstatus3 -ne 0 ]]; then exit $karma_exitstatus3; fi
 
 exit 0
