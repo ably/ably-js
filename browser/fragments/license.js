880fc92bc04769a30e6292427e571c92b34caca9
Matthew O'Riordan <matthew.oriordan@gmail.com>
2015-11-13 00:55:40 +0000

    Include License version & Github URL

--- browser/fragments/license.js
+++ browser/fragments/license.js
@@ -2,7 +2,10 @@
  * @license Copyright 2015, Ably
  *
  * Ably JavaScript Library v0.8.7
+ * https://github.com/ably/ably-js
+ *
+ * Ably Realtime Messaging
  * https://www.ably.io
  *
- * Released under the Apache licence.
+ * Released under the Apache Licence v2.0
  */
