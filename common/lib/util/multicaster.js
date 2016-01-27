47b963dbc5e9554e7251202685821f3bfdb22c17
Simon Woolf <simon@ably.io>
2016-01-27 15:27:02 +0000

    Multicaster: log an error if one of the callbacks throws an exception

--- common/lib/util/multicaster.js
+++ common/lib/util/multicaster.js
@@ -6,8 +6,13 @@ var Multicaster = (function() {
 		var handler = function() {
 			for(var i = 0; i < members.length; i++) {
 				var member = members[i];
-				try { member.apply(null, arguments); } catch(e){} };
-			};
+				try {
+					member.apply(null, arguments);
+				} catch(e){
+					Logger.logAction(Logger.LOG_ERROR, 'Multicaster multiple callback handler', 'Unexpected exception: ' + e + '; stack = ' + e.stack);
+				}
+			}
+		};
 
 		handler.push = function() {
 			Array.prototype.push.apply(members, arguments);
