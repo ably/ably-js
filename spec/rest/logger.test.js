27421ea5e51f7bed932f7673a0242c488d3a8c33
Alexandros Georgiou <alex.georgiou@gmail.com>
2016-01-28 14:23:01 +0200

    test(logger): Introducing logger tests and exposing common dir to express webserver
    Basic logger test skeleton with custom instantiation
    
    ensure test isolation
    
    more on testing custom logger
    
    More standard way for detecting browser in Logger instantiation code
    
    Minor fix to logger test so it doesn't crash in browser
    
    Exposing util to webserver, loading util/logger in webserver tests via script tag
    
    Better loading mechanism for node and webserver
    
    Fixed test for custom logger
    
    Polishing code before pull request
    
    Fix typo that prevented nodeunit tests from running
    
    Better tests, in style of tests in other language libraries
    
    Now testing that logger writes to stdout
    
    Better browser/non-browser stdout hooks
    
    Logger tests passing on node and browser
    
    Removing process.stdout hooks - intercepting console.log for both browser and node
    
    Adding TODO to remember to check Array.slice in IE
    
    style: Indentation of var definitions
    
    style: Better indentation and removing unneeded TODOs
    
    test: Using already existing 'isBrowser' boolean value
    
    Basic logger test skeleton with custom instantiation
    
    ensure test isolation
    
    more on testing custom logger
    
    More standard way for detecting browser in Logger instantiation code
    
    Minor fix to logger test so it doesn't crash in browser
    
    Exposing util to webserver, loading util/logger in webserver tests via script tag
    
    Better loading mechanism for node and webserver
    
    Fixed test for custom logger
    
    Polishing code before pull request
    
    test(logger): Hook into process.stdout, better testing loglevel
    
    Now testing that logger writes to stdout
    
    Better browser/non-browser stdout hooks
    
    Logger tests passing on node and browser
    
    Removing process.stdout hooks - intercepting console.log for both browser and node
    
    Adding TODO to remember to check Array.slice in IE
    
    style: Indentation of var definitions
    
    style: Better indentation and removing unneeded TODOs
    
    test: Using already existing 'isBrowser' boolean value
    
    Basic logger test skeleton with custom instantiation
    
    ensure test isolation
    
    more on testing custom logger
    
    More standard way for detecting browser in Logger instantiation code
    
    Minor fix to logger test so it doesn't crash in browser
    
    Exposing util to webserver, loading util/logger in webserver tests via script tag
    
    Better loading mechanism for node and webserver
    
    Fixed test for custom logger
    
    Polishing code before pull request
    
    Fix typo that prevented nodeunit tests from running
    
    Better tests, in style of tests in other language libraries
    
    Now testing that logger writes to stdout
    
    Better browser/non-browser stdout hooks
    
    Logger tests passing on node and browser
    
    test(logger): Removing process.stdout hooks, adding tests for custom logger
    
    Adding TODO to remember to check Array.slice in IE
    
    style: Indentation of var definitions
    
    test: Rest client uses custom logger specified in constructor
    
    test: Removing tls test that somehow got into this branch (oops)
    
    test(logger): Removed unneeded 'isBrowser' and improved comments
    
    test(logger): Exposing entire 'common' dir to express webserver

---
+++ spec/rest/logger.test.js
@@ -0,0 +1,155 @@
+"use strict";
+
+define(['shared_helper'], function(helper) {
+	var exports = {};
+
+	/* If we are running node, we mimic the loading mechanism used in /nodejs/realtime.js and /nodejs/rest.js
+	 * to get a reference to the logger. We do this because its implementation is not a module.
+	 * 
+	 * If we are in a browser, the logger is already loaded manually via a script tag in nodeunit.html.
+	 */
+
+	var logger;
+	
+	if (isBrowser) {
+		logger = Logger;
+	} else {
+
+		var fs = require('fs'),
+			path = require('path'),
+			vm = require('vm');
+
+		var context = vm.createContext({
+			require:require,
+			console:console,
+			process:process,
+			Buffer:Buffer,
+			setTimeout:setTimeout,
+			setInterval:setInterval,
+			clearTimeout:clearTimeout,
+			clearInterval:clearInterval,
+			global:global
+		});
+
+		var includeScript = function(name) {
+			var filename = path.resolve(__dirname, name);
+			return vm.runInContext(fs.readFileSync(filename, 'utf8'), context, filename);
+		};
+
+		includeScript('../../common/lib/util/logger.js');
+
+		logger = context.Logger;
+	};
+
+	/*
+	 * Check that the logger was instantiated correctly for testing.
+	 */
+	exports.logger_instantiate = function(test) {
+		test.expect(1);
+
+		test.equal(typeof(logger), 'function', 'Logger has been instantiated');
+
+		test.done();
+	};
+
+	/*
+	 * Check that the logger writes to console.log by default with no errors.
+	 */
+	exports.logger_writes_to_console = function(test) {
+		test.expect(2);
+
+		var oldLog = console.log,
+			messageOut = '';
+
+		console.log = function() {
+			var args = Array.prototype.slice.call(arguments);
+			messageOut += args.join(' ') + "\n";
+		};
+		
+		test.doesNotThrow(function() {
+			logger.logAction(logger.LOG_NONE,'logger_writes_to_stdout()','test message');
+		},'logger does not throw when called');
+
+		test.ok(messageOut.indexOf('test message') >= 0, 'Logger writes to stdout');
+
+		console.log = oldLog;
+
+		test.done();
+	}
+
+	/*
+	 * Check that a custom logger can be used. Check that the default logging level is logger.MAJOR.
+	 */
+	exports.logger_level_defaults_to_warn = function(test) {
+		test.expect(4);
+		
+		var messageOut = '',
+			customLogger = function(message) {
+				messageOut += message + "\n";
+			};
+		logger.setLog(undefined, customLogger);
+		
+		logger.logAction(logger.LOG_ERROR, 'Error level message');
+		logger.logAction(logger.LOG_MAJOR, 'Major level message');
+		logger.logAction(logger.LOG_MINOR, 'Minor level message');
+		logger.logAction(logger.LOG_MICRO, 'Micro level message');
+
+		test.ok(messageOut.indexOf('Error level message') >= 0, 'Logger writes at level ERROR by default');
+		test.ok(messageOut.indexOf('Major level message') >= 0, 'Logger writes at level MAJOR by default');
+		test.ok(messageOut.indexOf('Minor level message') < 0, 'Logger does not write at lever MINOR by default');
+		test.ok(messageOut.indexOf('Micro level message') < 0, 'Logger does not write at level MICRO by default');
+
+		test.done();
+	}
+
+	/*
+	 * Check that the logging level can be changed.
+	 */
+	exports.logger_level_change = function(test) {
+		test.expect(4);
+
+		var messageOut = '',
+			customLogger = function(message) {
+				messageOut += message + "\n";
+			};
+		logger.setLog(logger.LOG_MICRO, customLogger);
+
+		logger.logAction(logger.LOG_ERROR, 'Error level message');
+		logger.logAction(logger.LOG_MAJOR, 'Major level message');
+		logger.logAction(logger.LOG_MINOR, 'Minor level message');
+		logger.logAction(logger.LOG_MICRO, 'Micro level message');
+
+		test.ok(messageOut.indexOf('Error level message') >= 0, 'Logger writes at level ERROR by default');
+		test.ok(messageOut.indexOf('Major level message') >= 0, 'Logger writes at level MAJOR by default');
+		test.ok(messageOut.indexOf('Minor level message') >= 0, 'Logger does not write at lever MINOR by default');
+		test.ok(messageOut.indexOf('Micro level message') >= 0, 'Logger does not write at level MICRO by default');
+
+		test.done();
+	}
+
+	/*
+	 * Check that a custom logger can be specified in the constructor.
+	 */
+	exports.logger_rest_constructor = function(test) {
+		test.expect(2);
+
+		var messageOut = '',
+			customLogger = function(message) {
+				messageOut += message + "\n";
+			};
+
+		helper.setupApp(function() {
+			var rest = helper.AblyRest({
+				log: {
+					level: logger.LOG_MINOR,
+					handler: customLogger
+				}
+			});
+			test.ok(rest, 'Rest client instantiated');
+			test.ok(messageOut.indexOf('Ably: Rest(): started') >= 0, 'Rest client uses custom logger specified in constructor');
+			test.done();
+		});
+	}
+
+	return module.exports = helper.withTimeout(exports);
+});
