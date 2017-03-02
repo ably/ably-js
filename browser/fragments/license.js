fb175471d923d02514190d30ffc9145acf772d4b
Simon Woolf <simon@ably.io>
2017-03-02 21:05:42 +0000

    Regenerate and release version 0.9.0-beta.9

--- browser/fragments/license.js
+++ browser/fragments/license.js
@@ -1,7 +1,7 @@
 /**
  * @license Copyright 2017, Ably
  *
- * Ably JavaScript Library v0.9.0-beta.8
+ * Ably JavaScript Library v0.9.0-beta.9
  * https://github.com/ably/ably-js
  *
  * Ably Realtime Messaging
--- browser/static/ably.js
+++ browser/static/ably.js
@@ -1,7 +1,7 @@
 /**
  * @license Copyright 2017, Ably
  *
- * Ably JavaScript Library v0.9.0-beta.8
+ * Ably JavaScript Library v0.9.0-beta.9
  * https://github.com/ably/ably-js
  *
  * Ably Realtime Messaging
@@ -4087,7 +4087,7 @@ Defaults.TIMEOUTS = {
 };
 Defaults.httpMaxRetryCount = 3;
 
-Defaults.version          = '0.9.0-beta.8';
+Defaults.version          = '0.9.0-beta.9';
 Defaults.libstring        = 'js-' + Defaults.version;
 Defaults.apiVersion       = '1.0';
 
