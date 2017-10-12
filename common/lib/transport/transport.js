ably/ably-js

commit 6db805a43e3b63718c19385321148e98cc795513

have onActivity (nee setIdleTimeout) set the lastActivity time
else things like ws ping that just call that won't set that

common/lib/transport/transport.js
@@ -76,10 +76,7 @@ var Transport = (function() {
 		if (Logger.shouldLog(Logger.LOG_MICRO)) {
 			Logger.logAction(Logger.LOG_MICRO, 'Transport.onProtocolMessage()', 'received on ' + this.shortName + ': ' + ProtocolMessage.stringify(message));
 		}
-		this.lastActivity = this.connectionManager.lastActivity = Utils.now();
-		if(this.maxIdleInterval) {
-			this.setIdleTimer();
-		}
+		this.onActivity();
 
 		switch(message.action) {
 		case actions.HEARTBEAT:
@@ -138,7 +135,7 @@ var Transport = (function() {
 		var maxPromisedIdle = message.connectionDetails.maxIdleInterval;
 		if(maxPromisedIdle) {
 			this.maxIdleInterval = maxPromisedIdle + this.timeouts.realtimeRequestTimeout;
-			this.setIdleTimer();
+			this.onActivity();
 		}
 		/* else Realtime declines to guarantee any maximum idle interval - CD2h */
 	};
@@ -187,7 +184,10 @@ var Transport = (function() {
 		this.off();
 	};
 
-	Transport.prototype.setIdleTimer = function(timeout) {
+	Transport.prototype.onActivity = function(timeout) {
+		if(!this.maxIdleInterval) { return; }
+
+		this.lastActivity = this.connectionManager.lastActivity = Utils.now();
 		var self = this;
 		if(!this.idleTimer) {
 			this.idleTimer = setTimeout(function() {
@@ -205,7 +205,7 @@ var Transport = (function() {
 			Logger.logAction(Logger.LOG_ERROR, 'Transport.onIdleTimerExpire()', msg);
 			this.disconnect(new ErrorInfo(msg, 80003, 408));
 		} else {
-			this.setIdleTimer(timeRemaining + 10);
+			this.onActivity(timeRemaining + 10);
 		}
 	};
 

common/lib/transport/comettransport.js
@@ -109,9 +109,7 @@ var CometTransport = (function() {
 					err = err || new ErrorInfo('Request cancelled', 80000, 400);
 				}
 				self.recvRequest = null;
-				if(this.maxIdleInterval) {
-					this.setIdleTimer();
-				}
+				self.onActivity();
 				if(err) {
 					if(err.code) {
 						/* A protocol error received from realtime. TODO: once realtime
@@ -274,11 +272,9 @@ var CometTransport = (function() {
 		});
 		recvRequest.on('complete', function(err) {
 			self.recvRequest = null;
-			if(this.maxIdleInterval) {
-				/* A request completing must be considered activity, as realtime sends
-				 * heartbeats every 15s since a request began, not every 15s absolutely */
-				this.setIdleTimer();
-			}
+			/* A request completing must be considered activity, as realtime sends
+			 * heartbeats every 15s since a request began, not every 15s absolutely */
+			self.onActivity();
 			if(err) {
 				if(err.code) {
 					/* A protocol error received from realtime. TODO: once realtime

common/lib/transport/websockettransport.js
@@ -70,7 +70,7 @@ var WebSocketTransport = (function() {
 				if(wsConnection.on) {
 					/* node; browsers currently don't have a general eventemitter and can't detect
 					 * pings. Also, no need to reply with a pong explicitly, ws lib handles that */
-					wsConnection.on('ping', function() { self.setIdleTimer() });
+					wsConnection.on('ping', function() { self.onActivity(); });
 				}
 			} catch(e) {
 				Logger.logAction(Logger.LOG_ERROR, 'WebSocketTransport.connect()', 'Unexpected exception creating websocket: err = ' + (e.stack || e.message));

commit 00b8a39b09efb18a4a81e142e439c99e62e1252f

Fix idle timeout bug when timer extended due to +ve timeRemaning
Introduced by 6db805a

common/lib/transport/transport.js
@@ -184,15 +184,18 @@ var Transport = (function() {
 		this.off();
 	};
 
-	Transport.prototype.onActivity = function(timeout) {
+	Transport.prototype.onActivity = function() {
 		if(!this.maxIdleInterval) { return; }
-
 		this.lastActivity = this.connectionManager.lastActivity = Utils.now();
+		this.setIdleTimer(this.maxIdleInterval + 100);
+	};
+
+	Transport.prototype.setIdleTimer = function(timeout) {
 		var self = this;
 		if(!this.idleTimer) {
 			this.idleTimer = setTimeout(function() {
 				self.onIdleTimerExpire();
-			}, timeout || this.maxIdleInterval);
+			}, timeout);
 		}
 	};
 
@@ -205,7 +208,7 @@ var Transport = (function() {
 			Logger.logAction(Logger.LOG_ERROR, 'Transport.onIdleTimerExpire()', msg);
 			this.disconnect(new ErrorInfo(msg, 80003, 408));
 		} else {
-			this.onActivity(timeRemaining + 10);
+			this.setIdleTimer(timeRemaining + 100);
 		}
 	};
 