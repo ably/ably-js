8020c5407fde686a563c3dfb361ac1764059102e
Simon Woolf <simon@ably.io>
2018-01-26 16:00:19 +0100

    Fix crash if someone tries to throw undefined in a listener

--- common/lib/util/eventemitter.js
+++ common/lib/util/eventemitter.js
@@ -13,7 +13,7 @@ var EventEmitter = (function() {
 		try {
 			listener.apply(eventThis, args);
 		} catch(e) {
-			Logger.logAction(Logger.LOG_ERROR, 'EventEmitter.emit()', 'Unexpected listener exception: ' + e + '; stack = ' + e.stack);
+			Logger.logAction(Logger.LOG_ERROR, 'EventEmitter.emit()', 'Unexpected listener exception: ' + e + '; stack = ' + (e && e.stack));
 		}
 	}
 
