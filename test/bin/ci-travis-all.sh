225469106a06a98fb7135dd6432c6d5e962caf03
Cesare Rocchi <cesarerocchi@gmail.com>
2018-02-07 17:44:24 +0100

    Add batches of broswer tests

--- test/bin/ci-travis-all.sh
+++ test/bin/ci-travis-all.sh
@@ -1,2 +1,4 @@
 grunt test:nodeunit
-grunt test:karma --browsers bs_ie11_win81,bs_chrome_sierra
+grunt test:karma --browsers bs_firefox_sierra,bs_chrome_sierra,bs_ie11_win81
+grunt test:karma --browsers bs_ie10_win81,bs_ie9_win7,bs_ie16_win10
+grunt test:karma --browsers bs_safari_10_3,bs_safari_11
