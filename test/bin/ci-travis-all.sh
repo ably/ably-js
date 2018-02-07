cd705eb295288a4e3b57a65405d0bf2366163a4d
Cesare Rocchi <cesarerocchi@gmail.com>
2018-02-07 18:34:05 +0100

    Try parallel builds

--- test/bin/ci-travis-all.sh
+++ test/bin/ci-travis-all.sh
@@ -1,4 +1,4 @@
 grunt test:nodeunit
-grunt test:karma --browsers bs_firefox_sierra,bs_chrome_sierra,bs_ie11_win81
-grunt test:karma --browsers bs_ie10_win81,bs_ie9_win7,bs_ie16_win10
-grunt test:karma --browsers bs_safari_10_3,bs_safari_11
+grunt test:karma --browsers $B1
+grunt test:karma --browsers $B2
+grunt test:karma --browsers $B3
