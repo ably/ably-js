5e2726a29040e1645152d5bf44b02991d1acfed6
Cesare Rocchi <cesarerocchi@gmail.com>
2018-02-08 14:31:33 +0100

    Try removing iOS10 tests

--- test/bin/ci-travis-all.sh
+++ test/bin/ci-travis-all.sh
@@ -1,3 +1,3 @@
 grunt test:nodeunit
-grunt test:karma --browsers bs_firefox_sierra,bs_chrome_sierra,bs_ie11_win81,bs_ie10_win81
-grunt test:karma --browsers bs_ie9_win7,bs_ie16_win10,bs_safari_10_3,bs_safari_11
+grunt test:karma --browsers bs_firefox_sierra,bs_chrome_sierra,bs_ie11_win81,bs_ie10_win81,bs_ie9_win7
+grunt test:karma --browsers bs_ie9_win7,bs_ie16_win10,bs_safari_11
