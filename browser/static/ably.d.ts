e3be9adff00a56cb3db8e987702c5293145eabea
Matthew O'Riordan <matthew.oriordan@gmail.com>
2018-03-23 20:04:15 +0000

    DefinitelyTyped is deprecated

--- browser/static/ably.d.ts
+++ browser/static/ably.d.ts
@@ -1,7 +1,6 @@
 // Type definitions for Ably Realtime and Rest client library 1.0
 // Project: https://www.ably.io/
 // Definitions by: Ably <https://github.com/ably/>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 declare namespace ablyLib {
   namespace ChannelState {
