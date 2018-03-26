2b9844a60c905daba343d2970a614156eb591407
Cesare Rocchi <cesarerocchi@gmail.com>
2018-03-26 22:20:52 +0200

    Run only IE11 tests

--- test/bin/ci-travis-all.sh
+++ test/bin/ci-travis-all.sh
@@ -1,3 +1,2 @@
 grunt concat
-grunt test:karma --browsers bs_firefox_sierra,bs_chrome_sierra,bs_ie11_win81,bs_ie10_win81,bs_ie9_win7
-grunt test:karma --browsers bs_ie8_win7,bs_ie16_win10,bs_safari_11_iOS,bs_safari_high_sierra,bs_android_6
+grunt test:karma --browsers bs_ie11_win81
