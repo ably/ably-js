9fd89dfd59a971acaa86ccf7b86fba23b361ed02
Simon Woolf <simon@ably.io>
2018-12-14 14:52:47 +0100

    Bring Stats structs up to date

--- common/lib/types/stats.js
+++ common/lib/types/stats.js
@@ -40,18 +40,45 @@ var Stats = (function() {
 		this.push = new MessageTypes(values && values.push);
 		this.sharedQueue = new MessageTypes(values && values.sharedQueue);
 		this.externalQueue = new MessageTypes(values && values.externalQueue);
+		this.httpEvent = new MessageTypes(values && values.httpEvent);
+		this.push = new MessageTypes(values && values.push);
 		this.all = new MessageTypes(values && values.all);
 	}
 
-	function Stats(values) {
+	function MessageDirections(values) {
 		this.all           = new MessageTypes(values && values.all);
 		this.inbound       = new MessageTraffic(values && values.inbound);
 		this.outbound      = new MessageTraffic(values && values.outbound);
+	}
+
+	function XchgMessages(values) {
+		this.all           = new MessageTypes(values && values.all);
+		this.producerPaid  = new MessageDirections(values && values.producerPaid);
+		this.consumerPaid  = new MessageDirections(values && values.consumerPaid);
+	}
+
+	function PushStats(values) {
+		this.messages = (values && values.messages) || 0;
+		var notifications = value && values.notifications;
+		this.notifications = {
+			invalid: notifications.invalid || 0,
+			attempted: notifications.attempted || 0,
+			successful: notifications.successful || 0,
+			failed: notifications.failed || 0
+		};
+		this.directPublishes = (values && values.directPublishes) || 0;
+	}
+
+	function Stats(values) {
+		MessageDirections.call(this, values);
 		this.persisted     = new MessageTypes(values && values.persisted);
 		this.connections   = new ConnectionTypes(values && values.connections);
 		this.channels      = new ResourceCount(values && values.channels);
 		this.apiRequests   = new RequestCount(values && values.apiRequests);
 		this.tokenRequests = new RequestCount(values && values.tokenRequests);
+		this.xchgProducer  = new XchgMessages(values && values.xchgProducer);
+		this.xchgConsumer  = new XchgMessages(values && values.xchgConsumer);
+		this.push          = new PushStats(values && values.pushStats);
 		this.inProgress    = (values && values.inProgress) || undefined;
 		this.unit          = (values && values.unit) || undefined;
 		this.intervalId    = (values && values.intervalId) || undefined;
