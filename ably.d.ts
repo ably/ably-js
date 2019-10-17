5d0b192d43396e21531ffde244486031c35b99af
Tsviatko Yovtchev <tsviatko@codemerx.com>
2019-10-17 14:05:01 +0300

    Refactorings as per PR comments

--- ably.d.ts
+++ ably.d.ts
@@ -236,7 +236,7 @@ declare namespace Types {
 		ttl?: number;
 	}
 
-	type ChannelParams = Map<string, string>;
+	type ChannelParams = { [key: string]: string };
 
 	type ChannelModes = Array<string>;
 
