1c3ffb9ea6bbfd9991d3667d56deffd54374f70c
Tsviatko Yovtchev <tsviatko@codemerx.com>
2019-12-18 15:30:14 +0200

    Fixing tests

--- spec/common/globals/named_dependencies.js
+++ spec/common/globals/named_dependencies.js
@@ -5,7 +5,7 @@ define(function() {
 		'ably':              { browser: 'browser/static/ably',                node: 'nodejs/index' },
 		'ably.noencryption': { browser: 'browser/static/ably.noencryption' },
 		'browser-base64':    { browser: 'browser/lib/util/base64',            node: 'skip' },
-		'vcdiff-decoder':    { browser: 'node_modules/@ably/vcdiff-decoder/dist/vcdiff-decoder' },
+		'vcdiff-decoder':    { browser: 'node_modules/@ably/vcdiff-decoder/dist/vcdiff-decoder',  node:'node_modules/@ably/vcdiff-decoder'},
 		
 		// test modules
 		'globals':           { browser: 'spec/common/globals/environment',    node: 'spec/common/globals/environment' },
