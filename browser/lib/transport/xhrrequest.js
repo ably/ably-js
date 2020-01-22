ably/ably-js

commit 1f511f48ac1c32e9183eb60f29b00c714e40a0b4

Rest.request: handle msgpack and errors with contentful bodies

browser/lib/transport/xhrrequest.js
@@ -211,7 +211,7 @@ var XHRRequest = (function() {
 			if(!err) {
 				err = new ErrorInfo('Error response received from server: ' + statusCode + ' body was: ' + Utils.inspect(responseBody), null, statusCode);
 			}
-			self.complete(err);
+			self.complete(err, responseBody, headers, unpacked, statusCode);
 		}
 
 		function onProgress() {

common/lib/client/paginatedresource.js
@@ -44,8 +44,16 @@ var PaginatedResource = (function() {
 		});
 	};
 
+	function returnErrOnly(err, body, useHPR) {
+		/* If using httpPaginatedResponse, errors from Ably are returned as part of
+		 * the HPR, only do callback(err) for network errors etc. which don't
+		 * return a body and/or have no ably-originated error code (non-numeric
+		 * error codes originate from node) */
+		return !(useHPR && (body || typeof err.code === 'number'));
+	}
+
 	PaginatedResource.prototype.handlePage = function(err, body, headers, unpacked, statusCode, callback) {
-		if(err) {
+		if(err && returnErrOnly(err, body, this.useHttpPaginatedResponse)) {
 			Logger.logAction(Logger.LOG_ERROR, 'PaginatedResource.handlePage()', 'Unexpected error getting resource: err = ' + Utils.inspectError(err));
 			callback(err);
 			return;
@@ -54,7 +62,9 @@ var PaginatedResource = (function() {
 		try {
 			items = this.bodyHandler(body, headers, unpacked);
 		} catch(e) {
-			callback(e);
+			/* If we got an error, the failure to parse the body is almost certainly
+			 * due to that, so cb with that in preference to the parse error */
+			callback(err || e);
 			return;
 		}
 
@@ -63,7 +73,7 @@ var PaginatedResource = (function() {
 		}
 
 		if(this.useHttpPaginatedResponse) {
-			callback(null, new HttpPaginatedResponse(this, items, headers, statusCode, relParams));
+			callback(null, new HttpPaginatedResponse(this, items, headers, statusCode, relParams, err));
 		} else {
 			callback(null, new PaginatedResult(this, items, relParams));
 		}
@@ -100,15 +110,13 @@ var PaginatedResource = (function() {
 		});
 	};
 
-	function HttpPaginatedResponse(resource, items, headers, statusCode, relParams) {
+	function HttpPaginatedResponse(resource, items, headers, statusCode, relParams, err) {
 		PaginatedResult.call(this, resource, items, relParams);
 		this.statusCode = statusCode;
 		this.success = statusCode < 300 && statusCode >= 200;
 		this.headers = headers;
-		/* Note: we don't populate errorCode or errorMessage: for consistency with
-		 * the way the rest of the js library works, error data is passed as
-		 * ErrorInfos to the err argument of the callback; an  HttpPaginatedResponse
-		 * is only ever passed to the callback if there was no error */
+		this.errorCode = err && err.code;
+		this.errorMessage = err && err.message;
 	}
 	Utils.inherits(HttpPaginatedResponse, PaginatedResult);
 

common/lib/client/resource.js
@@ -23,7 +23,7 @@ var Resource = (function() {
 
 	function unenvelope(callback, format) {
 		return function(err, body, headers, unpacked, statusCode) {
-			if(err) {
+			if(err && !body) {
 				callback(err);
 				return;
 			}
@@ -49,16 +49,16 @@ var Resource = (function() {
 
 			if(statusCode < 200 || statusCode >= 300) {
 				/* handle wrapped errors */
-				var err = response && response.error;
+				var wrappedErr = (response && response.error) || err;
 				if(!err) {
 					err = new Error(String(res));
 					err.statusCode = statusCode;
 				}
-				callback(err);
+				callback(wrappedErr, response, headers, true, statusCode);
 				return;
 			}
 
-			callback(null, response, headers, true, statusCode);
+			callback(err, response, headers, true, statusCode);
 		};
 	}
 

common/lib/client/rest.js
@@ -1,5 +1,6 @@
 var Rest = (function() {
 	var noop = function() {};
+	var msgpack = Platform.msgpack;
 
 	function Rest(options) {
 		if(!(this instanceof Rest)){
@@ -58,7 +59,8 @@ var Rest = (function() {
 			}
 		}
 		var headers = Utils.copy(Utils.defaultGetHeaders()),
-			envelope = Http.supportsLinkHeaders ? undefined : 'json';
+			format = this.options.useBinaryProtocol ? 'msgpack' : 'json',
+			envelope = Http.supportsLinkHeaders ? undefined : format;
 
 		if(this.options.headers)
 			Utils.mixin(headers, this.options.headers);
@@ -105,14 +107,17 @@ var Rest = (function() {
 	};
 
 	Rest.prototype.request = function(method, path, params, body, customHeaders, callback) {
-		var format = this.options.useBinaryProtocol ? 'msgpack' : 'json',
+		var useBinary = this.options.useBinaryProtocol,
+			encoder = useBinary ? msgpack.encode: JSON.stringify,
+			decoder = useBinary ? msgpack.decode : JSON.parse,
+			format = useBinary ? 'msgpack' : 'json',
 			method = method.toLowerCase(),
-			envelope = Http.supportsLinkHeaders ? undefined : 'json',
+			envelope = Http.supportsLinkHeaders ? undefined : format,
 			params = params || {},
-			headers = Utils.copy(method == 'get' ? Utils.defaultGetHeaders() : Utils.defaultPostHeaders(format));
+			headers = Utils.copy(method == 'get' ? Utils.defaultGetHeaders(format) : Utils.defaultPostHeaders(format));
 
 		if(typeof body !== 'string') {
-			body = JSON.stringify(body);
+			body = encoder(body);
 		}
 		if(this.options.headers) {
 			Utils.mixin(headers, this.options.headers);
@@ -121,7 +126,7 @@ var Rest = (function() {
 			Utils.mixin(headers, customHeaders);
 		}
 		var paginatedResource = new PaginatedResource(this, path, headers, envelope, function(resbody, headers, unpacked) {
-			return Utils.ensureArray(unpacked ? resbody : JSON.parse(resbody));
+			return Utils.ensureArray(unpacked ? resbody : decoder(resbody));
 		}, /* useHttpPaginatedResponse: */ true);
 
 		switch(method) {

nodejs/lib/util/http.js
@@ -1,6 +1,7 @@
 "use strict";
 this.Http = (function() {
 	var request = require('request');
+	var msgpack = Platform.msgpack;
 	var noop = function() {};
 
 	/***************************************************
@@ -26,7 +27,13 @@ this.Http = (function() {
 			}
 			var statusCode = response.statusCode, headers = response.headers;
 			if(statusCode >= 300) {
-				if(headers['content-type'] == 'application/json') body = JSON.parse(body);
+				switch(headers['content-type']) {
+					case 'application/json':
+						body = JSON.parse(body);
+						break;
+					case 'application/x-msgpack':
+						body = msgpack.decode(body);
+				}
 				var error = body.error || {
 					statusCode: statusCode,
 					code: headers['x-ably-errorcode'],

spec/realtime/encoding.test.js
@@ -97,7 +97,7 @@ define(['ably', 'shared_helper', 'async'], function(Ably, helper, async) {
 				return;
 			}
 			test.expect(testData.messages.length * 5);
-			var realtime = helper.AblyRealtime(),
+			var realtime = helper.AblyRealtime({useBinaryProtocol: false}),
 				binaryrealtime = helper.AblyRealtime({useBinaryProtocol: true}),
 				channelName = 'message_encoding',
 				channelPath = '/channels/' + channelName + '/messages',

spec/rest/request.test.js
@@ -4,7 +4,8 @@ define(['ably', 'shared_helper', 'async'], function(Ably, helper, async) {
 	var rest,
 		exports = {},
 		_exports = {},
-		utils = helper.Utils;
+		utils = helper.Utils,
+		restTestOnJsonMsgpack = helper.restTestOnJsonMsgpack;
 
 	exports.setuptime = function(test) {
 		test.expect(1);
@@ -21,8 +22,7 @@ define(['ably', 'shared_helper', 'async'], function(Ably, helper, async) {
 		});
 	};
 
-	exports.request_time = function(test) {
-		test.expect(5);
+	restTestOnJsonMsgpack(exports, 'request_time', function(test, rest) {
 		rest.request('get', '/time', null, null, null, function(err, res) {
 			test.ok(!err, err && helper.displayError(err));
 			test.equal(res.statusCode, 200, 'Check statusCode');
@@ -31,25 +31,50 @@ define(['ably', 'shared_helper', 'async'], function(Ably, helper, async) {
 			test.equal(res.items.length, 1, 'Check array was of length 1');
 			test.done();
 		});
-	};
+	});
 
-	exports.request_404 = function(test) {
-		test.expect(2);
+	restTestOnJsonMsgpack(exports, 'request_404', function(test, rest) {
 		/* NB: can't just use /invalid or something as the CORS preflight will
 		 * fail. Need something superficially a valid path but where the actual
 		 * request fails */
 		rest.request('get', '/keys/ablyjs.test/requestToken', null, null, null, function(err, res) {
-			test.equal(err.statusCode, 404, 'Check statusCode');
-			test.equal(err.code, 40400, 'Check code');
+			test.equal(err, null, 'Check that we do not get an error response for a failure that returns an actual ably error code');
+			test.equal(res.success, false, 'Check res.success is false for a failure');
+			test.equal(res.statusCode, 404, 'Check HPR.statusCode is 404');
+			test.equal(res.errorCode, 40400, 'Check HPR.errorCode is 40400');
+			test.ok(res.errorMessage, 'Check have an HPR.errorMessage');
+			test.done();
+		});
+	});
+
+	restTestOnJsonMsgpack(exports, 'request_404', function(test, rest) {
+		/* NB: can't just use /invalid or something as the CORS preflight will
+		 * fail. Need something superficially a valid path but where the actual
+		 * request fails */
+		rest.request('get', '/keys/ablyjs.test/requestToken', null, null, null, function(err, res) {
+			test.equal(err, null, 'Check that we do not get an error response for a failure that returns an actual ably error code');
+			test.equal(res.success, false, 'Check res.success is false for a failure');
+			test.equal(res.statusCode, 404, 'Check HPR.statusCode is 404');
+			test.equal(res.errorCode, 40400, 'Check HPR.errorCode is 40400');
+			test.ok(res.errorMessage, 'Check have an HPR.errorMessage');
+			test.done();
+		});
+	});
+
+	/* With a network issue, should get an actual err, not an HttpPaginatedResponse with error members */
+	exports.request_network_error = function(test) {
+		rest = helper.AblyRest({restHost: helper.unroutableAddress})
+		rest.request('get', '/time', null, null, null, function(err, res) {
+			test.ok(err, 'Check get an err');
+			test.ok(!res, 'Check do not get a res');
 			test.done();
 		});
 	};
 
 	/* Use the request feature to publish, then retrieve (one at a time), some messages */
-	exports.request_post_get_messages = function(test) {
+	restTestOnJsonMsgpack(exports, 'request_post_get_messages', function(test, rest, channelName) {
 		test.expect(23);
-		var channelName = 'request_post_get_messages',
-			channelPath = '/channels/' + channelName + '/messages',
+		var channelPath = '/channels/' + channelName + '/messages',
 			msgone = {name: 'faye', data: 'whittaker'},
 			msgtwo = {name: 'martin', data: 'reed'};
 
@@ -106,7 +131,48 @@ define(['ably', 'shared_helper', 'async'], function(Ably, helper, async) {
 		], function() {
 			test.done();
 		})
-	}
+	});
+
+	restTestOnJsonMsgpack(exports, 'request_batch_api_success', function(test, rest, name) {
+		var body = {channels: [name + '1', name + '2'], messages: {data: 'foo'}};
+
+		rest.request("POST", "/messages", {}, body, {}, function(err, res) {
+			test.equal(err, null, 'Check that we do not get an error response for a success');
+			test.equal(res.success, true, 'Check res.success is true for a success');
+			test.equal(res.statusCode, 201, 'Check res.statusCode is 201 for a success');
+			test.equal(res.errorCode, null, 'Check res.errorCode is null for a success');
+			test.equal(res.errorMessage, null, 'Check res.errorMessage is null for a success');
+
+			test.ok(!res.items[0].batchResponse, 'Check no batchResponse, since items is now just a flat array of channel responses');
+			test.equal(res.items.length, 2, 'Verify batched response includes response for each channel');
+			test.ok(!res.items[0].error, 'Verify channel1 response is not an error');
+			test.equal(res.items[0].channel, name + '1', 'Verify channel1 response includes correct channel');
+			test.ok(!res.items[1].error, 'Verify channel2 response is not an error');
+			test.equal(res.items[1].channel, name + '2', 'Verify channel2 response includes correct channel');
+			test.done();
+		});
+	});
+
+	restTestOnJsonMsgpack(exports, 'request_batch_api_partial_success', function(test, rest, name) {
+		var body = {channels: [name, '[invalid', ''], messages: {data: 'foo'}};
+
+		rest.request("POST", "/messages", {}, body, {}, function(err, res) {
+			test.equal(err, null, 'Check that we do not get an error response for a partial success');
+			test.equal(res.success, false, 'Check res.success is false for a partial failure');
+			test.equal(res.statusCode, 400, 'Check HPR.statusCode is 400 for a partial failure');
+			test.equal(res.errorCode, 40020, 'Check HPR.errorCode is 40020 for a partial failure');
+			test.ok(res.errorMessage, 'Check have an HPR.errorMessage');
+
+			var response = res.items[0];
+			test.equal(response.error.code, 40020, 'Verify response has an errorCode');
+			test.equal(response.batchResponse.length, 3, 'Verify batched response includes response for each channel');
+			test.equal(response.batchResponse[0].channel, name, 'Verify channel1 response includes correct channel');
+			test.ok(!response.batchResponse[0].error, 'Verify first channel response is not an error');
+			test.equal(response.batchResponse[1].error.code, 40010, 'Verify [invalid response includes an error with the right code');
+			test.equal(response.batchResponse[2].error.code, 40010, 'Verify empty channel response includes an error with the right code');
+			test.done();
+		});
+	});
 
 	return module.exports = helper.withTimeout(exports);
 });

commit d08caed2d2f65a7ca468b8658a4be9c151ff6d8a

CometTransport.sendRequest: fix interpretation of non-ProtocolMessage errors
Bug introduced by 1f511f48a, which made xhrrequest emit both the full
response body even in the case of an error response in order to populate
the httpPaginatedResponse. The comettransport code assumed that at most
one of err and body was set, and checked the latter first.

browser/lib/transport/xhrrequest.js
@@ -80,8 +80,9 @@ var XHRRequest = (function() {
 	XHRRequest.prototype.complete = function(err, body, headers, unpacked, statusCode) {
 		if(!this.requestComplete) {
 			this.requestComplete = true;
-			if(body)
+			if(!err && body) {
 				this.emit('data', body);
+			}
 			this.emit('complete', err, body, headers, unpacked, statusCode);
 			this.dispose();
 		}

common/lib/transport/comettransport.js
@@ -216,18 +216,25 @@ var CometTransport = (function() {
 			if(err) Logger.logAction(Logger.LOG_ERROR, 'CometTransport.sendItems()', 'on complete: err = ' + Utils.inspectError(err));
 			self.sendRequest = null;
 
-			/* the results of the request usually get handled as protocol responses instead of send errors */
+			/* the result of the request, even if a nack, is usually a protocol response
+			 * contained in the data. An err is anomolous, and indicates some issue with the
+			 * network,transport, or connection */
+			if(err) {
+				if(err.code) {
+					/* A protocol error received from realtime. TODO: once realtime
+					 * consistendly sends errors wrapped in protocol messages, should be
+					 * able to remove this */
+					self.onData(protocolMessageFromRawError(err));
+				} else {
+					/* A network/xhr error. Don't bother wrapping in a protocol message,
+					 * just disconnect the transport */
+					self.disconnect(err);
+				}
+				return;
+			}
+
 			if(data) {
 				self.onData(data);
-			} else if(err && err.code) {
-				/* A protocol error received from realtime. TODO: once realtime
-				 * consistendly sends errors wrapped in protocol messages, should be
-				 * able to remove this */
-				self.onData(protocolMessageFromRawError(err));
-			} else {
-				/* A network/xhr error. Don't bother wrapping in a protocol message,
-				 * just disconnect the transport */
-				self.disconnect(err);
 			}
 
 			if(self.pendingItems) {