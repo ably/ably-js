9fdc5b98f8193d0d5b84b12025f054370b09abf5
Simon Woolf <simon@ably.io>
2020-06-08 18:47:00 +0100

    Test: remove obselete comments

--- spec/rest/message.test.js
+++ spec/rest/message.test.js
@@ -148,8 +148,6 @@ define(['ably', 'shared_helper', 'async'], function(Ably, helper, async) {
 	};
 
 	/* Check ids are correctly sent */
-	// TODO enable when idempotent publishing works on sandbox
-	// until then, test with ABLY_ENV=idempotent-dev
 	exports.idempotent_rest_publishing = function(test) {
 		test.expect(2);
 		var rest = helper.AblyRest({idempotentRestPublishing: false, useBinaryProtocol: false}),
