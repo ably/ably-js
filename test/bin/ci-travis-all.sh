ca50214daac8fa0844d60ec7e231faa6f7d148c8
Owen Pearson <owen.pearson@ably.com>
2020-11-16 11:17:57 +0000

    remove unused bash scripts

--- test/bin/ci-travis-all.sh
+++
@@ -1,3 +0,0 @@
-./node_modules/.bin/grunt concat
-./node_modules/.bin/grunt test:karma --browsers bs_firefox_sierra,bs_chrome_sierra,bs_ie11_win10,bs_ie10_win81,bs_ie9_win7
-./node_modules/.bin/grunt test:karma --browsers bs_ie8_win7,bs_ie16_win10,bs_safari_11_iOS,bs_safari_high_sierra,bs_android_6
