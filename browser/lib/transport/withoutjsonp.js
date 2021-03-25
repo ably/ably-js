abeda9a6ee7a31cd71cf8029514f36939e13a25b
Owen Pearson <owen.pearson@ably.com>
2021-03-25 10:44:21 +0000

    add comment explaining the withoutjsonp file

--- browser/lib/transport/withoutjsonp.js
+++ browser/lib/transport/withoutjsonp.js
@@ -1,3 +1,6 @@
+/**
+ * This file exists for React Native and Nativescript in order to exclude the unsupported JSONP transport from these platforms.
+ */
 import XHRPollingTransport from './xhrpollingtransport';
 import XHRStreamingTransport from './xhrstreamingtransport';
 
