0a1da2d683b7cfafdf9af1085d4ba04b1d6d436d
Owen Pearson <owen.pearson@ably.com>
2021-04-12 11:20:39 +0100

    Regenerate and release version 1.2.9

--- browser/fragments/license.js
+++ browser/fragments/license.js
@@ -1,6 +1,6 @@
 module.exports = `@license Copyright 2021, Ably
 
-Ably JavaScript Library v1.2.8
+Ably JavaScript Library v1.2.9
 https://github.com/ably/ably-js
 
 Ably Realtime Messaging
--- common/lib/util/defaults.js
+++ common/lib/util/defaults.js
@@ -35,7 +35,7 @@ Defaults.errorReportingHeaders = {
 	"Content-Type": "application/json"
 };
 
-Defaults.version          = '1.2.8';
+Defaults.version          = '1.2.9';
 Defaults.libstring        = Platform.libver + '-' + Defaults.version;
 Defaults.apiVersion       = '1.2';
 
