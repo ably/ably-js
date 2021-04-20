9e5c68e99a6ba0366c69d0b92f80010755970a87
Owen Pearson <owen.pearson@ably.com>
2021-04-20 17:31:44 +0100

    improvements and comments for the newly introduced env vars

--- spec/support/runPlaywrightTests.js
+++ spec/support/runPlaywrightTests.js
@@ -7,6 +7,12 @@ const serverProcess = require('child_process').fork(path.resolve(__dirname, '..'
 const port = process.env.PORT || 3000;
 const host = 'localhost';
 
+const browserEnv = process.env.PLAYWRIGHT_BROWSER;
+
+if (!['chromium', 'firefox', 'webkit'].includes(browserEnv)) {
+    throw new Error(`PLAYWRIGHT_BROWSER environment variable must be either 'chromium', 'webkit' or 'firefox' (currently ${browserEnv})`);
+}
+
 const runTests = async (browserType) => {
     const browser = await browserType.launch();
     const page = await browser.newPage();
@@ -43,9 +49,11 @@ const runTests = async (browserType) => {
 
 (async () => {
     try {
-        if (process.env.PLAYWRIGHT_BROWSER) {
-            await runTests(playwright[process.env.PLAYWRIGHT_BROWSER]);
+        if (browserEnv) {
+            // If the PLAYWRIGHT_BROWSER env var is set, only run tests in the specified browser...
+            await runTests(playwright[browserEnv]);
         } else {
+            // ...otherwise run all the browsers
             await runTests(playwright.chromium);
             await runTests(playwright.webkit);
             await runTests(playwright.firefox);
