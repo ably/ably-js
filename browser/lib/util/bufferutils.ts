fa0cb44e59bdf58ff258cfaf91ea65c60c003d7d
Owen Pearson <owen.pearson@ably.com>
2021-05-14 01:23:09 +0100

    Convert bufferutils files to TypeScript

--- browser/lib/util/bufferutils.ts
+++ browser/lib/util/bufferutils.ts
@@ -3,33 +3,37 @@ import { parse as parseUtf8, stringify as stringifyUtf8 } from 'crypto-js/build/
 import { parse as parseBase64, stringify as stringifyBase64 } from 'crypto-js/build/enc-base64';
 import WordArray from 'crypto-js/build/lib-typedarrays';
 import Platform from 'platform';
+import { TypedArray } from '../../../common/types/IPlatform';
 
-var BufferUtils = (function() {
-	var ArrayBuffer = Platform.ArrayBuffer;
-	var atob = Platform.atob;
-	var TextEncoder = Platform.TextEncoder;
-	var TextDecoder = Platform.TextDecoder;
-	var base64CharSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-	var hexCharSet = '0123456789abcdef';
+/* Most BufferUtils methods that return a binary object return an ArrayBuffer
+ * if supported, else a CryptoJS WordArray. The exception is toBuffer, which
+ * returns a Uint8Array (and won't work on browsers too old to support it) */
+module BufferUtils {
+	const ArrayBuffer = Platform.ArrayBuffer;
+	const atob = Platform.atob;
+	const TextEncoder = Platform.TextEncoder;
+	const TextDecoder = Platform.TextDecoder;
+	export const base64CharSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+	export const hexCharSet = '0123456789abcdef';
 
-	function isWordArray(ob) { return ob !== null && ob !== undefined && ob.sigBytes !== undefined; }
-	function isArrayBuffer(ob) { return ob !== null && ob !== undefined && ob.constructor === ArrayBuffer; }
-	function isTypedArray(ob) { return ArrayBuffer && ArrayBuffer.isView && ArrayBuffer.isView(ob); }
+	function isWordArray(ob: unknown): ob is WordArray { return ob !== null && ob !== undefined && (ob as WordArray).sigBytes !== undefined; }
+	function isArrayBuffer(ob: unknown): ob is ArrayBuffer { return ob !== null && ob !== undefined && (ob as ArrayBuffer).constructor === ArrayBuffer; }
+	function isTypedArray(ob: unknown): ob is TypedArray { return !!ArrayBuffer && ArrayBuffer.isView && ArrayBuffer.isView(ob); }
 
 	// https://gist.githubusercontent.com/jonleighton/958841/raw/f200e30dfe95212c0165ccf1ae000ca51e9de803/gistfile1.js
-	function uint8ViewToBase64(bytes) {
-		var base64    = ''
-		var encodings = base64CharSet;
+	function uint8ViewToBase64(bytes: Uint8Array) {
+		let base64      = ''
+		const encodings = base64CharSet;
 
-		var byteLength    = bytes.byteLength
-		var byteRemainder = byteLength % 3
-		var mainLength    = byteLength - byteRemainder
+		const byteLength    = bytes.byteLength;
+		const byteRemainder = byteLength % 3;
+		const mainLength    = byteLength - byteRemainder;
 
-		var a, b, c, d
-		var chunk
+		let a, b, c, d;
+		let chunk;
 
 		// Main loop deals with bytes in chunks of 3
-		for (var i = 0; i < mainLength; i = i + 3) {
+		for (let i = 0; i < mainLength; i = i + 3) {
 			// Combine the three bytes into a single integer
 			chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
 
@@ -68,39 +72,33 @@ var BufferUtils = (function() {
 		return base64
 	}
 
-	function base64ToArrayBuffer(base64) {
-		var binary_string =  atob(base64);
-		var len = binary_string.length;
-		var bytes = new Uint8Array( len );
-		for (var i = 0; i < len; i++)        {
-			var ascii = binary_string.charCodeAt(i);
+	function base64ToArrayBuffer(base64: string) {
+		const binary_string =  atob?.(base64) as string; // this will always be defined in browser so it's safe to cast
+		const len = binary_string.length;
+		const bytes = new Uint8Array( len );
+		for (let i = 0; i < len; i++)        {
+			const ascii = binary_string.charCodeAt(i);
 			bytes[i] = ascii;
 		}
 		return bytes.buffer;
 	}
 
-	/* Most BufferUtils methods that return a binary object return an ArrayBuffer
-	 * if supported, else a CryptoJS WordArray. The exception is toBuffer, which
-	 * returns a Uint8Array (and won't work on browsers too old to support it) */
 	function BufferUtils() {}
 
-	BufferUtils.base64CharSet = base64CharSet;
-	BufferUtils.hexCharSet = hexCharSet;
-
-	var isBuffer = BufferUtils.isBuffer = function(buf) { return isArrayBuffer(buf) || isWordArray(buf) || isTypedArray(buf); };
+	export function isBuffer(buf: unknown): buf is (ArrayBuffer | WordArray | TypedArray) { return isArrayBuffer(buf) || isWordArray(buf) || isTypedArray(buf); };
 
 	/* In browsers, returns a Uint8Array */
-	var toBuffer = BufferUtils.toBuffer = function(buf) {
+	export function toBuffer(buf: WordArray | TypedArray | ArrayBuffer) {
 		if(!ArrayBuffer) {
 			throw new Error("Can't convert to Buffer: browser does not support the necessary types");
 		}
 
 		if(isArrayBuffer(buf)) {
-			return new Uint8Array(buf);
+			return new Uint8Array(buf as ArrayBuffer);
 		}
 
 		if(isTypedArray(buf)) {
-			return new Uint8Array(buf.buffer);
+			return new Uint8Array((buf as TypedArray).buffer);
 		}
 
 		if(isWordArray(buf)) {
@@ -119,45 +117,44 @@ var BufferUtils = (function() {
 		throw new Error("BufferUtils.toBuffer expected an arraybuffer, typed array, or CryptoJS wordarray");
 	};
 
-	BufferUtils.toArrayBuffer = function(buf) {
+	export function toArrayBuffer(buf: ArrayBuffer | WordArray) {
 		if(isArrayBuffer(buf)) {
 			return buf;
 		}
 		return toBuffer(buf).buffer;
 	};
 
-	BufferUtils.toWordArray = function(buf) {
+	export function toWordArray(buf: TypedArray | WordArray | number[] | ArrayBuffer) {
 		if(isTypedArray(buf)) {
 			buf = buf.buffer;
 		}
-		return isWordArray(buf) ? buf : WordArray.create(buf);
+		return isWordArray(buf) ? buf : WordArray.create(buf as number[]);
 	};
 
-	BufferUtils.base64Encode = function(buf) {
+	export function base64Encode(buf: WordArray | ArrayBuffer) {
 		if(isWordArray(buf)) {
 			return stringifyBase64(buf);
 		}
 		return uint8ViewToBase64(toBuffer(buf));
 	};
 
-	BufferUtils.base64Decode = function(str) {
+	export function base64Decode(str: string) {
 		if(ArrayBuffer && atob) {
 			return base64ToArrayBuffer(str);
 		}
 		return parseBase64(str);
 	};
 
-	BufferUtils.hexEncode = function(buf) {
-		buf = BufferUtils.toWordArray(buf);
-		return stringifyHex(buf);
+	export function hexEncode(buf: ArrayBuffer) {
+		return stringifyHex(toWordArray(buf));
 	};
 
-	BufferUtils.hexDecode = function(string) {
+	export function hexDecode(string: string) {
 		var wordArray = parseHex(string);
-		return ArrayBuffer ? BufferUtils.toArrayBuffer(wordArray) : wordArray;
+		return ArrayBuffer ? toArrayBuffer(wordArray) : wordArray;
 	};
 
-	BufferUtils.utf8Encode = function(string) {
+	export function utf8Encode(string: string) {
 		if(TextEncoder) {
 			return (new TextEncoder()).encode(string).buffer;
 		}
@@ -169,35 +166,36 @@ var BufferUtils = (function() {
 	 * can take (in particular allowing strings, which are just interpreted as
 	 * binary); here we ensure that the input is actually a buffer since trying
 	 * to utf8-decode a string to another string is almost certainly a mistake */
-	BufferUtils.utf8Decode = function(buf) {
+	export function utf8Decode(buf: ArrayBuffer | WordArray) {
 		if(!isBuffer(buf)) {
 			throw new Error("Expected input of utf8decode to be an arraybuffer, typed array, or CryptoJS wordarray");
 		}
 		if(TextDecoder && !isWordArray(buf)) {
 			return (new TextDecoder()).decode(buf);
 		}
-		buf = BufferUtils.toWordArray(buf);
+		buf = toWordArray(buf);
 		return stringifyUtf8(buf);
 	};
 
-	BufferUtils.bufferCompare = function(buf1, buf2) {
+	export function bufferCompare(buf1: TypedArray, buf2: TypedArray) {
 		if(!buf1) return -1;
 		if(!buf2) return 1;
-		buf1 = BufferUtils.toWordArray(buf1);
-		buf2 = BufferUtils.toWordArray(buf2);
-		buf1.clamp(); buf2.clamp();
+		const wordArray1 = toWordArray(buf1);
+		const wordArray2 = toWordArray(buf2);
+		wordArray1.clamp(); wordArray2.clamp();
 
-		var cmp = buf1.sigBytes - buf2.sigBytes;
+		var cmp = wordArray1.sigBytes - wordArray2.sigBytes;
 		if(cmp != 0) return cmp;
-		buf1 = buf1.words; buf2 = buf2.words;
-		for(var i = 0; i < buf1.length; i++) {
-			cmp = buf1[i] - buf2[i];
+		const words1 = wordArray1.words;
+        const words2 = wordArray2.words;
+		for(var i = 0; i < words1.length; i++) {
+			cmp = words1[i] - words2[i];
 			if(cmp != 0) return cmp;
 		}
 		return 0;
 	};
 
-	BufferUtils.byteLength = function(buf) {
+	export function byteLength(buf: ArrayBuffer | TypedArray | WordArray) {
 		if(isArrayBuffer(buf) || isTypedArray(buf)) {
 			return buf.byteLength
 		} else if(isWordArray(buf)) {
@@ -206,11 +204,9 @@ var BufferUtils = (function() {
 	};
 
 	/* Returns ArrayBuffer on browser and Buffer on Node.js */
-	BufferUtils.typedArrayToBuffer = function(typedArray) {
+	export function typedArrayToBuffer(typedArray: TypedArray) {
 		return typedArray.buffer;
 	};
-
-	return BufferUtils;
-})();
+};
 
 export default BufferUtils;
