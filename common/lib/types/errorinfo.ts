74307d0902ef3049e69b3bfdf7f953e9cdfe8e24
Owen Pearson <owen.pearson@ably.com>
2021-09-16 17:10:45 +0100

    convert errorinfo to ts

--- common/lib/types/errorinfo.ts
+++ common/lib/types/errorinfo.ts
@@ -1,28 +1,32 @@
-import Utils from '../util/utils';
+import inspectError from '../util/inspectError';
 
-var ErrorInfo = (function() {
+export default class ErrorInfo {
+	message?: string;
+	code?: number;
+	statusCode?: number;
+	cause?: string | Error;
+	href?: string;
 
-	function ErrorInfo(message, code, statusCode, cause) {
+	constructor(message?: string, code?: number, statusCode?: number, cause?: string | Error) {
 		this.message = message;
 		this.code = code;
 		this.statusCode = statusCode;
 		this.cause = cause;
-		this.href = undefined;
 	}
 
-	ErrorInfo.prototype.toString = function() {
-		var result = '[' + this.constructor.name;
+	toString(): string {
+		let result = '[' + this.constructor.name;
 		if(this.message) result += ': ' + this.message;
 		if(this.statusCode) result += '; statusCode=' + this.statusCode;
 		if(this.code) result += '; code=' + this.code;
-		if(this.cause) result += '; cause=' + Utils.inspectError(this.cause);
+		if(this.cause) result += '; cause=' + inspectError(this.cause);
 		if(this.href && !(this.message && this.message.indexOf('help.ably.io') > -1)) result += '; see ' + this.href + ' ';
 		result += ']';
 		return result;
-	};
+	}
 
-	ErrorInfo.fromValues = function(values) {
-		var result = Utils.mixin(new ErrorInfo(), values);
+	static fromValues(values: Error | ErrorInfo): ErrorInfo {
+		const result = Object.assign(new ErrorInfo(), values);
 		if (values instanceof Error) {
 			/* Error.message is not enumerable, so mixin loses the message */
 			result.message = values.message;
@@ -31,9 +35,5 @@ var ErrorInfo = (function() {
 			result.href = 'https://help.ably.io/error/' + result.code;
 		}
 		return result;
-	};
-
-	return ErrorInfo;
-})();
-
-export default ErrorInfo;
+	}
+}
