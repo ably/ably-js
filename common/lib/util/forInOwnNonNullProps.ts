76d440d23fa129fa61e4e8e44321a87a7d3432c5
Owen Pearson <owen.pearson@ably.com>
2021-09-21 17:44:04 +0100

    minor improvements to forInOwnNonNullProps

--- common/lib/util/forInOwnNonNullProps.ts
+++ common/lib/util/forInOwnNonNullProps.ts
@@ -1,5 +1,5 @@
-export default function(ob: {[key: string]: unknown}, fn: Function) {
-	for (var prop in ob) {
+export default function(ob: Record<string, unknown>, fn: Function) {
+	for (let prop in ob) {
 		if (Object.prototype.hasOwnProperty.call(ob, prop) && ob[prop]) {
 			fn(prop);
 		}
