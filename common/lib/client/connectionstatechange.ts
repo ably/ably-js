c296f25583da67a9c7575cf6dcb0f7dff9640a60
Owen Pearson <owen.pearson@ably.com>
2021-10-14 16:40:24 +0100

    Convert ConnectionStateChange to TypeScript

--- common/lib/client/connectionstatechange.ts
+++ common/lib/client/connectionstatechange.ts
@@ -1,14 +1,17 @@
-var ConnectionStateChange = (function() {
+import ErrorInfo from "../types/errorinfo";
 
-	/* public constructor */
-	function ConnectionStateChange(previous, current, retryIn, reason) {
+class ConnectionStateChange {
+	previous?: string;
+	current?: string;
+	retryIn?: number;
+	reason?: string | Error | ErrorInfo;
+
+	constructor(previous?: string, current?: string, retryIn?: number | null, reason?: string | Error | ErrorInfo) {
 		this.previous = previous;
 		this.current = current;
 		if(retryIn) this.retryIn = retryIn;
 		if(reason) this.reason = reason;
 	}
-
-	return ConnectionStateChange;
-})();
+}
 
 export default ConnectionStateChange;
