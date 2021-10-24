a5c423ba616483d8e3075886dc89f602fe691d66
Owen Pearson <owen.pearson@ably.com>
2021-10-24 09:15:17 +0100

    Add JSDoc annotations to Rest and Realtime classes

--- ably.d.ts
+++ ably.d.ts
@@ -733,6 +733,19 @@ declare namespace Types {
 	}
 }
 
-export declare class Rest extends Types.RestCallbacks {}
+/**
+ * The Ably REST client offers a simple stateless API to interact directly with Ably’s REST API.
+ * 
+ * The REST library is typically used server-side to issue tokens, publish messages, and retrieve message history. If you are building a client-side application, you may want to consider using our stateful Ably Realtime client libraries.
+ */
+ export declare class Rest extends Types.RestCallbacks {}
 
-export declare class Realtime extends Types.RealtimeCallbacks {}
+ /**
+	* The Ably Realtime client establishes and maintains a persistent connection to Ably and provides methods to publish and subscribe to messages over a low latency realtime connection.
+	* 
+	* The Realtime client extends the REST client and as such provides the functionality available in the REST client in addition to Realtime-specific features.
+	* 
+	* @extends Rest
+	*/
+ export declare class Realtime extends Types.RealtimeCallbacks {}
+ 
