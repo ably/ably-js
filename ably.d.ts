1c00c76f05c8bc74db378731aa3a54ba8706f5ed
Owen Pearson <owen.pearson@ably.com>
2021-10-24 12:52:50 +0100

    Add JSDoc annotations for TokenDetails

--- ably.d.ts
+++ ably.d.ts
@@ -478,11 +478,29 @@ declare namespace Types {
 		webhook: StatsMessageTypes;
 	}
 
+	/**
+	 * The details of Ably Token string and its associated metadata.
+	 */
 	interface TokenDetails {
+		/**
+		 * The capability associated with this Ably Token. The capability is a a JSON stringified canonicalized representation of the resource paths and associated operations. [Read more about authentication and capabilities](https://ably.com/documentation/core-features/authentication/#capabilities-explained).
+		 */
 		capability: string;
+		/**
+		 * The client ID, if any, bound to this Ably Token. If a client ID is included, then the Ably Token authenticates its bearer as that client ID, and the Ably Token may only be used to perform operations on behalf of that client ID. The client is then considered to be an [identified client](https://ably.com/documentation/core-features/authentication#identified-clients).
+		 */
 		clientId?: string;
+		/**
+		 * The time (in milliseconds since the epoch) at which this token expires.
+		 */
 		expires: number;
+		/**
+		 * The time (in milliseconds since the epoch) at which this token was issued.
+		 */
 		issued: number;
+		/**
+		 * The [Ably Token](https://ably.com/documentation/core-features/authentication#ably-tokens) itself. A typical Ably Token string may appear like `xVLyHw.A-pwh7wicf3afTfgiw4k2Ku33kcnSA7z6y8FjuYpe3QaNRTEo4`.
+		 */
 		token: string;
 	}
 
