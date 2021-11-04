15e20a57576f494979c6e5090adecaef57889065
Owen Pearson <owen.pearson@ably.com>
2021-11-04 14:47:59 +0000

    Add error case for MessageQueue.completeMessages when MessageQueue is empty

--- common/lib/transport/messagequeue.ts
+++ common/lib/transport/messagequeue.ts
@@ -47,6 +47,9 @@ class MessageQueue extends EventEmitter {
 		Logger.logAction(Logger.LOG_MICRO, 'MessageQueue.completeMessages()', 'serial = ' + serial + '; count = ' + count);
 		err = err || null;
 		const messages = this.messages;
+		if (messages.length === 0) {
+			throw new Error('MessageQueue.completeMessages(): completeMessages called on any empty MessageQueue');
+		}
 		const first = messages[0];
 		if(first) {
 			const startSerial = first.message.msgSerial as number;
