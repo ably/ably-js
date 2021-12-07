63f5af406934ae5fd6dfb7a6bf8a2a8587b1dd88
Owen Pearson <owen.pearson@ably.com>
2021-12-07 16:23:17 +0000

    Convert ChannelStateChange to TypeScript

--- common/lib/client/channelstatechange.ts
+++ common/lib/client/channelstatechange.ts
@@ -1,14 +1,17 @@
-var ChannelStateChange = (function() {
+import ErrorInfo from "../types/errorinfo";
 
-	/* public constructor */
-	function ChannelStateChange(previous, current, resumed, reason) {
+class ChannelStateChange {
+	previous: string;
+	current: string;
+	resumed?: boolean;
+	reason?: string | Error | ErrorInfo;
+
+	constructor(previous: string, current: string, resumed?: boolean, reason?: string | Error | ErrorInfo | null) {
 		this.previous = previous;
 		this.current = current;
 		if(current === 'attached') this.resumed = resumed;
 		if(reason) this.reason = reason;
 	}
-
-	return ChannelStateChange;
-})();
+}
 
 export default ChannelStateChange;
