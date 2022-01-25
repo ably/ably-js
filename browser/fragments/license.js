ffc121abd51c19ddecf5c09464b98b06b36c5f72
Owen Pearson <owen.pearson@ably.com>
2022-01-25 17:15:18 +0000

    Use range of years for license.js

--- browser/fragments/license.js
+++ browser/fragments/license.js
@@ -1,6 +1,6 @@
 const { version } = require('../../package.json');
 
-module.exports = `@license Copyright 2022, Ably
+module.exports = `@license Copyright 2015-2022, Ably
 
 Ably JavaScript Library v${version}
 https://github.com/ably/ably-js
