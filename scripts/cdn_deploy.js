bc4976250fd0de89932d1abe894cb41b57db2ac0
Peter Maguire <peter.maguire@ably.com>
2022-02-17 10:57:53 +0000

    resolve formatting issues

--- scripts/cdn_deploy.js
+++ scripts/cdn_deploy.js
@@ -4,11 +4,11 @@ const AWS = require('aws-sdk');
 const Git = require('nodegit');
 const fs = require('fs');
 const path = require('path');
-const argv =  require('minimist')(process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2));
 const S3_DEFAULT_BUCKET = "cdn.ably.io";
 const S3_DEFAULT_ROOT = "lib";
 
-async function run(){
+async function run() {
 
 	let config = {
 		bucket: S3_DEFAULT_BUCKET,
@@ -20,37 +20,36 @@ async function run(){
 		excludeDirs: "node_modules,.git",
 		fileRegex: "^(?!\\.).*\\.(map|js|html)$",
 		...argv,
-	}
+	};
 
-	if(!config.s3Key || !config.s3Secret)
+	if (!config.s3Key || !config.s3Secret)
 		throw new Error(`Missing S3 credentials, provide either --s3Key and --s3Secret or environment variables AWS_ACCESS_KEY and AWS_SECRET_ACCESS_KEY`);
 
 	config.path = path.resolve(config.path);
-	config.includeDirs = config.includeDirs.split(",").map((dir)=>path.resolve(dir));
-	config.excludeDirs = config.excludeDirs.split(",").map((dir)=>path.resolve(dir));
+	config.includeDirs = config.includeDirs.split(",").map((dir) => path.resolve(dir));
+	config.excludeDirs = config.excludeDirs.split(",").map((dir) => path.resolve(dir));
 
 	const s3 = new AWS.S3({
 		region: "REGION",
 		accessKeyId: config.s3Key,
 		secretAccessKey: config.s3Secret,
 		endpoint: config.endpoint,
-	})
+	});
 
 	let repo = await Git.Repository.open(config.path);
-	if(!config.tag) {
+	if (!config.tag) {
 		let refs = await repo.getReferences();
 		console.log("Available tags:");
-		refs.filter((r)=>r.isTag()).forEach((r)=>console.log(`${r.name().substring(r.name().indexOf("tags/")+5)}`))
+		refs.filter((r) => r.isTag()).forEach((r) => console.log(`${r.name().substring(r.name().indexOf("tags/") + 5)}`));
 		return throw new Error("You must supply a tag with --tag or skip this with --force")
 	}
 
 	let ref = await repo.getReference(config.tag);
 
 	if (!ref.isTag())
-		return throw new Error(`Reference '${config.tag}' is a branch not a tag, please select a versioned release to deploy.`)
+		return throw new Error(`Reference '${config.tag}' is a branch not a tag, please select a versioned release to deploy.`);
 
 	await repo.checkoutRef(ref);
-
 
 	console.log(`Starting Ably Javascript S3 library deployment for version ${await repo.getCurrentBranch()}`);
 	console.log("Bucket:", config.bucket);
@@ -67,54 +66,51 @@ async function run(){
 	try {
 		for (let file of files) {
 			console.log(`Uploading ${file}...`);
-			for(let version of versions) {
+			for (let version of versions) {
 				const relativePath = path.relative(config.path, file);
 				const ext = path.extname(relativePath);
 				await upload(s3, {
 					Body: fs.readFileSync(file),
-					Key: path.join(config.root, `${relativePath.substring(0, relativePath.length-ext.length)}-${version}${ext}`),
+					Key: path.join(config.root, `${relativePath.substring(0, relativePath.length - ext.length)}-${version}${ext}`),
 					Bucket: config.bucket,
-				})
+				});
 			}
 		}
 		console.log("Success!");
-	}catch(e){
+	} catch (e) {
 		throw new Error(e);
 	}
 }
 
-function upload(s3, upload){
-	return new Promise((resolve, reject)=>{
-		s3.upload(upload, (err, body)=>{
-			if(err)reject(err);
+function upload(s3, upload) {
+	return new Promise((resolve, reject) => {
+		s3.upload(upload, (err, body) => {
+			if (err) reject(err);
 			else resolve(body);
 		})
 	})
 }
 
-
-run();
-
-function recursiveFile(includes, excludes, regex){
-	return includes.flatMap(scanDir(excludes)).filter((file)=>file && new RegExp(regex, "gi").test(path.basename(file)))
+function recursiveFile(includes, excludes, regex) {
+	return includes.flatMap(scanDir(excludes)).filter((file) => file && new RegExp(regex, "gi").test(path.basename(file)));
 }
 
-function scanDir(excludes){
-	return (dir)=>{
-		if(excludes?.includes(dir))return null;
-		return fs.readdirSync(dir).flatMap((file)=>{
+function scanDir(excludes) {
+	return (dir) => {
+		if (excludes?.includes(dir)) return null;
+		return fs.readdirSync(dir).flatMap((file) => {
 			const filePath = path.join(dir, file);
 			const stat = fs.statSync(filePath);
-			if(stat.isDirectory()) return scanDir(excludes)(filePath);
+			if (stat.isDirectory()) return scanDir(excludes)(filePath);
 			return filePath;
 		})
 	}
 }
 
-
-
-
-function getVersions(fullVersion){
+function getVersions(fullVersion) {
 	const split = fullVersion.split(".");
-	return split.map((v, i)=>split.slice(0,i+1).join("."))
+	return split.map((v, i) => split.slice(0, i + 1).join("."));
 }
+
+
+run();
