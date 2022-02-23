566f2c699e29497ddc30b57299b19a755976cef8
Owen Pearson <owen.pearson@ably.com>
2022-02-23 23:30:44 +0000

    Add doc annotations to connection classes

--- ably.d.ts
+++ ably.d.ts
@@ -1567,22 +1567,54 @@ declare namespace Types {
 	}
 
 	class ConnectionBase extends EventEmitter<connectionEventCallback, ConnectionStateChange, ConnectionEvent, ConnectionState> {
+		/**
+		 * When a connection failure occurs this property contains the ErrorInfo.
+		 */
 		errorReason: ErrorInfo;
+		/**
+		 * A unique public identifier String for this connection, used to identify this member in presence events and messages.
+		 */
 		id: string;
+		/**
+		 * A unique private connection key String used to recover or resume a connection, assigned by Ably. When recovering a connection explicitly, the recoveryKey is used in the recover client options as it contains both the key and the last message serial.
+		 * 
+		 * This private connection key can also be used by other REST clients to publish on behalf of this client. See the [publishing over REST on behalf of a realtime client documentation](https://ably.com/documentation/rest/channels#publish-on-behalf) for more info.
+		 */
 		key: string;
+		/**
+		 * The recovery key String can be used by another client to recover this connection’s state in the recover client options property. See [connection state recover options](https://ably.com/documentation/realtime/connection#connection-state-recover-options) for more information.
+		 */
 		recoveryKey: string;
+		/**
+		 * The serial number Integer of the last message to be received on this connection, used automatically by the library when recovering or resuming a connection. When recovering a connection explicitly, the recoveryKey is used in the recover client options as it contains both the key and the last message serial.
+		 */
 		serial: number;
+		/**
+		 * The current state of this Connection. See [Connection states](https://ably.com/documentation/realtime/connection#connection-states) for more information.
+		 */
 		readonly state: ConnectionState;
+		/**
+		 * Causes the connection to close, entering the closing state. Once closed, the library will not attempt to re-establish the connection without an explicit call to `connect`.
+		 */
 		close(): void;
+		/**
+		 * Explicitly calling connect is unnecessary unless the `ClientOptions` attribute `autoConnect` is false. Unless already connected or connecting, this method causes the connection to open, entering the connecting state.
+		 */
 		connect(): void;
 	}
 
 	class ConnectionCallbacks extends ConnectionBase {
+		/**
+		 * When connected, sends a heartbeat ping to the Ably server and executes the callback with any error and the response time in milliseconds when a heartbeat ping request is echoed from the server. This can be useful for measuring true round-trip latency to the connected Ably server.
+		 */
 		ping(callback?: Types.StandardCallback<number>): void;
 		whenState(targetState: ConnectionState, callback: connectionEventCallback): void;
 	}
 
 	class ConnectionPromise extends ConnectionBase {
+		/**
+		 * When connected, sends a heartbeat ping to the Ably server and executes the callback with any error and the response time in milliseconds when a heartbeat ping request is echoed from the server. This can be useful for measuring true round-trip latency to the connected Ably server.
+		 */
 		ping(): Promise<number>;
 		whenState(targetState: ConnectionState): Promise<ConnectionStateChange>;
 	}
