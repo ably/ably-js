8f1f3a2f3b1a648cb3cbb6cc54bcded2cfbb8f6e
Owen Pearson <owen.pearson@ably.com>
2022-03-11 12:07:14 +0000

    Remove comma-dangle eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,6 @@ module.exports = {
   ],
   rules: {
     "no-undef": "error",
-    // comma-dangle used for browser compatibility for browsers that don't support trailing commas
-    "comma-dangle": ["error", "never"],
     "eol-last": "error",
     // security/detect-object-injection just gives a lot of false positives
     // see https://github.com/nodesecurity/eslint-plugin-security/issues/21
