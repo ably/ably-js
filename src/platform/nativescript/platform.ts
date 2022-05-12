20c396cb2b82cd2e1b4b5cf6fb25a8bfd16bd54a
Peter Maguire <peter.maguire@ably.com>
2022-05-12 14:28:35 +0100

    fix nativescript

--- src/platform/nativescript/platform.ts
+++ src/platform/nativescript/platform.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-undef */
 import msgpack from '../web/lib/util/msgpack';
+import { IPlatform } from "../../common/types/IPlatform";
+import '@nativescript/types';
 require('nativescript-websockets');
 
-var randomBytes;
+var randomBytes: (size: number)=>string;
 if (global.android) {
   randomBytes = function (size) {
     var sr = new java.security.SecureRandom();
@@ -13,34 +15,35 @@ if (global.android) {
 } else {
   randomBytes = function (size) {
     var bytes = NSMutableData.dataWithLength(size);
+    //@ts-ignore
     SecRandomCopyBytes(kSecRandomDefault, size, bytes.mutableBytes());
     return bytes.base64EncodedStringWithOptions(0);
   };
 }
 
-var Platform = {
+const Platform: IPlatform = {
   agent: 'nativescript',
   logTimestamps: true,
   noUpgrade: false,
   binaryType: 'arraybuffer',
   WebSocket: WebSocket,
-  xhrSupported: XMLHttpRequest,
+  xhrSupported: !!XMLHttpRequest,
   allowComet: true,
   jsonpSupported: false,
   streamingSupported: false,
   useProtocolHeartbeats: true,
   createHmac: null,
   msgpack: msgpack,
-  supportsBinary: typeof TextDecoder !== 'undefined' && TextDecoder,
+  supportsBinary: typeof TextDecoder !== 'undefined' && !!TextDecoder,
   preferBinary: false,
   ArrayBuffer: ArrayBuffer,
   atob: null,
-  nextTick: function (f) {
+  nextTick: (f: ()=>void) => {
     setTimeout(f, 0);
   },
   addEventListener: null,
   inspect: JSON.stringify,
-  stringByteSize: function (str) {
+  stringByteSize: function (str: string) {
     /* str.length will be an underestimate for non-ascii strings. But if we're
      * in a browser too old to support TextDecoder, not much we can do. Better
      * to underestimate, so if we do go over-size, the server will reject the
@@ -50,7 +53,7 @@ var Platform = {
   TextEncoder: global.TextEncoder,
   TextDecoder: global.TextDecoder,
   Promise: global.Promise,
-  getRandomValues: function (arr, callback) {
+  getRandomValues: function (arr: any, callback: any) {
     var bytes = randomBytes(arr.length);
     for (var i = 0; i < arr.length; i++) {
       arr[i] = bytes[i];
