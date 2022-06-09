8e9ffac1cbfc09d0797d8e0ad9575536bd2ea09d
Owen Pearson <owen.pearson@ably.com>
2022-06-09 16:20:16 +0100

    Use optional chaining to check for `console.log` existence

--- src/common/lib/util/logger.ts
+++ src/common/lib/util/logger.ts
@@ -47,9 +47,7 @@ const getDefaultLoggers = (): [Function, Function] => {
   /* Can't just check for console && console.log; fails in IE <=9 */
   if (
     (typeof Window === 'undefined' && typeof WorkerGlobalScope === 'undefined') /* node */ ||
-    (globalOrWindow.console &&
-      globalOrWindow.console.log &&
-      typeof globalOrWindow.console.log.apply === 'function') /* sensible browsers */
+    typeof globalOrWindow?.console?.log?.apply === 'function' /* sensible browsers */
   ) {
     consoleLogger = function (...args: unknown[]) {
       console.log.apply(console, args);
@@ -59,7 +57,7 @@ const getDefaultLoggers = (): [Function, Function] => {
           console.warn.apply(console, args);
         }
       : consoleLogger;
-  } else if (globalOrWindow.console && (globalOrWindow.console.log as unknown)) {
+  } else if (globalOrWindow?.console.log as unknown) {
     /* IE <= 9 with the console open -- console.log does not
      * inherit from Function, so has no apply method */
     consoleLogger = errorLogger = function () {
