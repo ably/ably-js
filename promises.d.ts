7cf5adf768fa88fa78b8e5c9e44d372768181051
Lawrence Forooghian <lawrence@forooghian.com>
2022-06-30 17:05:56 -0300

    Add docstrings to promises.d.ts’s exports
    Copied from ably.d.ts.

--- promises.d.ts
+++ promises.d.ts
@@ -1,6 +1,22 @@
 import Ably = require('./ably');
 
+/**
+ * The Ably REST client offers a simple stateless API to interact directly with Ably’s REST API.
+ *
+ * The REST library is typically used server-side to issue tokens, publish messages, and retrieve message history. If you are building a client-side application, you may want to consider using our stateful Ably Realtime client libraries.
+ */
 export declare class Rest extends Ably.Rest.Promise {}
+/**
+ * The Ably Realtime client establishes and maintains a persistent connection to Ably and provides methods to publish and subscribe to messages over a low latency realtime connection.
+ *
+ *
+ *
+ * The Realtime client extends the REST client and as such provides the functionality available in the REST client in addition to Realtime-specific features.
+ *
+ *
+ *
+ * @augments Rest
+ */
 export declare class Realtime extends Ably.Realtime.Promise {}
 
 // Re-export the Types namespace.
