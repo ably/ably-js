713f8265e05bb47014f41006b268909d8d4e2377
Lawrence Forooghian <lawrence@forooghian.com>
2022-06-30 17:01:29 -0300

    Change mechanism used for re-exporting Types namespace
    This is groundwork for generating separate documentation for the
    Promise-based version of the library (that which a user gets when they
    import `ably/promises`). I’ll explain why we want to do this in a
    subsequent commit message.
    
    In addition to ably.d.ts, we have a separate file promises.d.ts, which
    is the TypeScript declarations file used when a user imports
    `ably/promises`.
    
    This file is responsible for re-exporting the `Types` namespace from
    ably.d.ts, as well as setting the top-level `Rest` and `Realtime`
    exports to be the promise-based version of these classes.
    
    Our current approach for this file has explicit re-exports for each of
    the types in the Types namespace. As well as the currently explained
    problem of this file needing to be manually kept in sync with ably.d.ts,
    another problem is that none of these re-exports have any documentation
    comments, meaning that:
    
    1. TypeDoc documentation generated from promises.d.ts will not have any
       descriptions.
    2. A user who imports `ably/promises` will not get any useful
       IntelliSense documentation in their code editor.
    
    So, I’ve tried a different approach for re-exporting the Types
    namespace. To be very honest, I don’t really understand why this
    approach works, but it seems to. I pretty much arrived at it by trial
    and error. I saw the documentation on namespace aliases
    (https://www.typescriptlang.org/docs/handbook/namespaces.html#aliases)
    and wondered what would happen if I just tried to export that object.
    
    I also tried export = { ...Types, <the Rest and Realtime types> } but
    that introduced a weird namespace named 'export = ' in my TypeDoc docs
    🤷.
    
    I did my best to check that this change doesn’t affect the experience of
    using the library. That is, I tested it locally by `npm install <path to
    my local ably-js repo>` in a new repo, checking that `tsc` works for
    compiling a file that imports `ably/promises`, and checking that
    IntelliSense works in Visual Studio Code.

--- promises.d.ts
+++ promises.d.ts
@@ -3,48 +3,6 @@ import Ably = require('./ably');
 export declare class Realtime extends Ably.Realtime.Promise {}
 export declare class Rest extends Ably.Rest.Promise {}
 
-/* Typescript currently has no way of reexporting an existing namespace other than `export
- * = ...`, which wouldn't allow modifying the Rest & Realtime members. So need to
- * export its members individually :/ */
-export declare namespace Types {
-  type ChannelState = Ably.Types.ChannelState;
-  type ChannelEvent = Ably.Types.ChannelEvent;
-  type ConnectionState = Ably.Types.ConnectionState;
-  type ConnectionEvent = Ably.Types.ConnectionEvent;
-  type PresenceAction = Ably.Types.PresenceAction;
-  type StatsIntervalGranularity = Ably.Types.StatsIntervalGranularity;
-  type HTTPMethods = Ably.Types.HTTPMethods;
-  type Transport = Ably.Types.Transport;
-  type ClientOptions = Ably.Types.ClientOptions;
-  type AuthOptions = Ably.Types.AuthOptions;
-  type CapabilityOp = Ably.Types.CapabilityOp;
-  type TokenParams = Ably.Types.TokenParams;
-  type CipherParams = Ably.Types.CipherParams;
-  type ErrorInfo = Ably.Types.ErrorInfo;
-  type StatsMessageCount = Ably.Types.StatsMessageCount;
-  type StatsMessageTypes = Ably.Types.StatsMessageTypes;
-  type StatsRequestCount = Ably.Types.StatsRequestCount;
-  type StatsResourceCount = Ably.Types.StatsResourceCount;
-  type StatsConnectionTypes = Ably.Types.StatsConnectionTypes;
-  type StatsMessageTraffic = Ably.Types.StatsMessageTraffic;
-  type TokenDetails = Ably.Types.TokenDetails;
-  type TokenRequest = Ably.Types.TokenRequest;
-  type ChannelOptions = Ably.Types.ChannelOptions;
-  type RestHistoryParams = Ably.Types.RestHistoryParams;
-  type RestPresenceParams = Ably.Types.RestPresenceParams;
-  type RealtimePresenceParams = Ably.Types.RealtimePresenceParams;
-  type RealtimeHistoryParams = Ably.Types.RealtimeHistoryParams;
-  type LogInfo = Ably.Types.LogInfo;
-  type ChannelStateChange = Ably.Types.ChannelStateChange;
-  type ConnectionStateChange = Ably.Types.ConnectionStateChange;
-  type DeviceDetails = Ably.Types.DeviceDetails;
-  type PushChannelSubscription = Ably.Types.PushChannelSubscription;
-  type DevicePushState = 'ACTIVE' | 'FAILING' | 'FAILED';
-  type DevicePushDetails = Ably.Types.DevicePushDetails;
-  type DeviceRegistrationParams = Ably.Types.DeviceRegistrationParams;
-  type PushChannelSubscriptionParams = Ably.Types.PushChannelSubscriptionParams;
-  type PushChannelsParams = Ably.Types.PushChannelsParams;
-  type Crypto = Ably.Types.Crypto;
-  type Message = Ably.Types.Message;
-  type PresenceMessage = Ably.Types.PresenceMessage;
-}
+// Re-export the Types namespace.
+import Types = Ably.Types;
+export { Types };
