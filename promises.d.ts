78b3c1208be7e25b1589e16fe60e8a34ee3500e2
Lawrence Forooghian <lawrence@forooghian.com>
2022-06-30 17:05:10 -0300

    Switch order of Realtime and Rest in promises.d.ts
    For consistency with ably.d.ts.

--- promises.d.ts
+++ promises.d.ts
@@ -1,7 +1,7 @@
 import Ably = require('./ably');
 
-export declare class Realtime extends Ably.Realtime.Promise {}
 export declare class Rest extends Ably.Rest.Promise {}
+export declare class Realtime extends Ably.Realtime.Promise {}
 
 // Re-export the Types namespace.
 import Types = Ably.Types;
