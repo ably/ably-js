2247ce2a32982853808eedac2e30b2480380951d
Lawrence Forooghian <lawrence@forooghian.com>
2022-07-06 13:07:32 -0300

    Add canonical docstring for class CipherParams
    This is taken verbatim from the repo
    ably/canonical-api-reference-prototyping at commit cf9891c.

--- ably.d.ts
+++ ably.d.ts
@@ -738,6 +738,10 @@ declare namespace Types {
   }
 
   /**
+   * BEGIN CANONICAL DOCSTRING
+   * Sets the properties to configure encryption for a [`RestChannel`]{@link RestChannel} or [`RealtimeChannel`]{@link RealtimeChannel} object.
+   * END CANONICAL DOCSTRING
+   *
    * BEGIN LEGACY DOCSTRING
    * An object containing configuration options for a channel cipher, including algorithm, mode, key length and key. ably-js currently supports AES with CBC, PKCS#7 with a default key length of 256 bits, and AES128.
    * END LEGACY DOCSTRING
