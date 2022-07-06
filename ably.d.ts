657ee531623627b57c240e1d79d0fba307a49380
Lawrence Forooghian <lawrence@forooghian.com>
2022-07-06 14:02:44 -0300

    Add canonical docstring for RealtimePresence#history
    This is taken verbatim from the repo
    ably/canonical-api-reference-prototyping at commit cf9891c.

--- ably.d.ts
+++ ably.d.ts
@@ -2794,6 +2794,23 @@ declare namespace Types {
      */
     get(params?: RealtimePresenceParams, callback?: realtimePresenceGetCallback): void;
     /**
+     * BEGIN CANONICAL DOCSTRING
+     * Retrieves a [`PaginatedResult`]{@link PaginatedResult} object, containing an array of historical [`PresenceMessage`]{@link PresenceMessage} objects for the channel. If the channel is configured to persist messages, then presence messages can be retrieved from history for up to 72 hours in the past. If not, presence messages can only be retrieved from history for up to two minutes in the past.
+     *
+     * @param start - The time from which messages are retrieved, specified as milliseconds since the Unix epoch.
+     * @param end - The time until messages are retrieved, specified as milliseconds since the Unix epoch.
+     * @param direction - The order for which messages are returned in. Valid values are `backwards` which orders messages from most recent to oldest, or `forwards` which orders messages from oldest to most recent. The default is `backwards`.
+     * BEGIN PARAM CANONICAL API DEFAULT
+     * api-default .Backwards
+     * END PARAM CANONICAL API DEFAULT
+     * @param limit - An upper limit on the number of messages returned. The default is 100, and the maximum is 1000.
+     * BEGIN PARAM CANONICAL API DEFAULT
+     * api-default 100
+     * END PARAM CANONICAL API DEFAULT
+
+     * @returns A [`PaginatedResult`]{@link PaginatedResult} object containing an array of [`PresenceMessage`]{@link PresenceMessage} objects.
+     * END CANONICAL DOCSTRING
+     *
      * BEGIN LEGACY DOCSTRING
      * Gets a paginated set of historical presence message events for this channel. If the channel is configured to persist messages to disk, then the presence message event history will typically be available for 24 – 72 hours. If not, presence message events are only retained in memory by the Ably service for two minutes.
      * END LEGACY DOCSTRING
@@ -2916,6 +2933,23 @@ declare namespace Types {
      */
     get(params?: RealtimePresenceParams): Promise<PresenceMessage[]>;
     /**
+     * BEGIN CANONICAL DOCSTRING
+     * Retrieves a [`PaginatedResult`]{@link PaginatedResult} object, containing an array of historical [`PresenceMessage`]{@link PresenceMessage} objects for the channel. If the channel is configured to persist messages, then presence messages can be retrieved from history for up to 72 hours in the past. If not, presence messages can only be retrieved from history for up to two minutes in the past.
+     *
+     * @param start - The time from which messages are retrieved, specified as milliseconds since the Unix epoch.
+     * @param end - The time until messages are retrieved, specified as milliseconds since the Unix epoch.
+     * @param direction - The order for which messages are returned in. Valid values are `backwards` which orders messages from most recent to oldest, or `forwards` which orders messages from oldest to most recent. The default is `backwards`.
+     * BEGIN PARAM CANONICAL API DEFAULT
+     * api-default .Backwards
+     * END PARAM CANONICAL API DEFAULT
+     * @param limit - An upper limit on the number of messages returned. The default is 100, and the maximum is 1000.
+     * BEGIN PARAM CANONICAL API DEFAULT
+     * api-default 100
+     * END PARAM CANONICAL API DEFAULT
+
+     * @returns A [`PaginatedResult`]{@link PaginatedResult} object containing an array of [`PresenceMessage`]{@link PresenceMessage} objects.
+     * END CANONICAL DOCSTRING
+     *
      * BEGIN LEGACY DOCSTRING
      * Gets a paginated set of historical presence message events for this channel. If the channel is configured to persist messages to disk, then the presence message event history will typically be available for 24 – 72 hours. If not, presence message events are only retained in memory by the Ably service for two minutes.
      * END LEGACY DOCSTRING
