c52c9eb1251858b9611c051321808facaa0cd533
Lawrence Forooghian <lawrence@forooghian.com>
2022-07-11 17:12:49 -0300

    Add canonical docstring for ConnectionStateChange#reason
    This is taken verbatim from the repo
    ably/canonical-api-reference-prototyping at commit cf9891c.

--- ably.d.ts
+++ ably.d.ts
@@ -1818,6 +1818,10 @@ declare namespace Types {
      */
     previous: ConnectionState;
     /**
+     * BEGIN CANONICAL DOCSTRING
+     * An [`ErrorInfo`]{@link ErrorInfo} object containing any information relating to the transition.
+     * END CANONICAL DOCSTRING
+     *
      * BEGIN LEGACY DOCSTRING
      * An ErrorInfo containing any information relating to the transition.
      * END LEGACY DOCSTRING
