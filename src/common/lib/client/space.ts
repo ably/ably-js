d29417ceb8078f2b16241d1bfa36e8c89541e14f
Peter Maguire <peter.maguire@ably.com>
2023-01-19 16:38:02 +0000

    space API get functionality and tests

--- src/common/lib/client/space.ts
+++ src/common/lib/client/space.ts
@@ -1,8 +1,29 @@
+import Realtime from "./realtime";
+import RealtimeChannel from "./realtimechannel";
 
 
 class Spaces {
+
+  spaces: Record<string, Space>;
+  private realtime: Realtime;
+
+  constructor(realtime: Realtime) {
+    this.spaces = {};
+    this.realtime = realtime;
+  }
+
   get(name: string, options: SpaceOptions): Space {
-    return new Space(name, options);
+    if(name.length === 0){
+      throw new Error("Spaces must have a non-empty name");
+    }
+    // TODO: updating options
+    if(this.spaces[name])
+      return this.spaces[name];
+
+    // make a space
+    let space = new Space(name, options, this.realtime);
+    this.spaces[name] = space;
+    return space;
   }
 }
 
@@ -11,10 +32,16 @@ class Space {
 
   name: string;
   private options: SpaceOptions;
+  private realtime: Realtime;
+  private channel: RealtimeChannel;
 
-  constructor(name: string, options: SpaceOptions){
+  constructor(name: string, options: SpaceOptions, realtime: Realtime){
     this.name = name;
     this.options = options;
+    this.realtime = realtime;
+
+    // The channel name prefix here should be unique to avoid conflicts with non-space channels
+    this.channel = this.realtime.channels.get(`_ably_space_${name}`);
   }
 
 }
--- test/realtime/space.test.js
+++ test/realtime/space.test.js
@@ -1,17 +1,58 @@
-define(['ably', 'shared_helper', 'async', 'chai'], function (Ably, helper, async, chai) {
+define(['ably', 'shared_helper', 'async', 'chai'], function (Ably, helper, async, { expect }) {
   describe('realtime/space', ()=>{
 
+    before(function (done) {
+      helper.setupApp(function (err) {
+        if (err) {
+          done(err);
+        }
+        done();
+      });
+    });
+
     describe('get', ()=>{
-      it('should create a space if it does not exist', ()=>{
+
+      it.only('should create a space if it does not exist', (done)=>{
+        let realtime, err;
+        try {
+          realtime = helper.AblyRealtime();
+          expect(realtime.spaces.spaces["test_space"]).to.equal(undefined);
+          let space = realtime.spaces.get("test_space", {});
+          expect(realtime.spaces.spaces["test_space"]).to.equal(space);
+
+        }catch(e){
+          err = e;
+        }finally{
+          helper.closeAndFinish(done, realtime, err);
+        }
 
       });
 
-      it('should return an existing space', ()=>{
-
+      it('should return an existing space', (done)=>{
+        let realtime, err;
+        try {
+          realtime = helper.AblyRealtime();
+          let space1 = realtime.spaces.get("test_space", {});
+          let space2 = realtime.spaces.get("test_space", {});
+          expect(space1).to.equal(space2);
+        }catch(e){
+          err = e;
+        }finally{
+          helper.closeAndFinish(done, realtime, err);
+        }
       });
 
-      it('should throw an error when getting a space with no name', ()=>{
+      it('should throw an error when getting a space with no name', (done)=>{
+        let realtime, err;
+        try {
+          realtime = helper.AblyRealtime();
+          expect(realtime.spaces.get("", {})).to.not.be.ok;
 
+        }catch(e){
+          err = e;
+        }finally{
+          helper.closeAndFinish(done, realtime, err);
+        }
       });
 
       it('should validate the channel name', ()=>{
