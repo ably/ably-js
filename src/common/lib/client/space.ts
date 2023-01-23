13d6608fa9e4550c42dc428e45ba44c679163429
Dominik Piatek <dominik.piatek@ably.io>
2023-01-23 15:24:20 +0000

    Formatting fixes

--- src/common/lib/client/space.ts
+++ src/common/lib/client/space.ts
@@ -1,13 +1,11 @@
-import Realtime from "./realtime";
-import RealtimeChannel from "./realtimechannel";
-import ErrorInfo from "../types/errorinfo";
-import PresenceMessage from "../types/presencemessage";
-import { Types } from "../../../../ably";
+import Realtime from './realtime';
+import RealtimeChannel from './realtimechannel';
+import ErrorInfo from '../types/errorinfo';
+import PresenceMessage from '../types/presencemessage';
+import { Types } from '../../../../ably';
 import errorCallback = Types.errorCallback;
 
-
 class Spaces {
-
   spaces: Record<string, Space>;
   private realtime: Realtime;
 
@@ -17,29 +15,25 @@ class Spaces {
   }
 
   get(name: string, options: SpaceOptions): Space {
-    if(typeof name !== "string" || name.length === 0){
-      throw new Error("Spaces must have a non-empty name");
+    if (typeof name !== 'string' || name.length === 0) {
+      throw new Error('Spaces must have a non-empty name');
     }
-    // TODO: updating options
-    if(this.spaces[name])
-      return this.spaces[name];
 
-    // make a space
+    if (this.spaces[name]) return this.spaces[name];
+
     let space = new Space(name, options, this.realtime);
     this.spaces[name] = space;
     return space;
   }
 }
 
-
 class Space {
-
   name: string;
   private options: SpaceOptions;
   private realtime: Realtime;
   private channel: RealtimeChannel;
 
-  constructor(name: string, options: SpaceOptions, realtime: Realtime){
+  constructor(name: string, options: SpaceOptions, realtime: Realtime) {
     this.name = name;
     this.options = options;
     this.realtime = realtime;
@@ -48,28 +42,33 @@ class Space {
     this.channel = this.realtime.channels.get(`_ably_space_${name}`);
   }
 
-  enter(data: any, callback: errorCallback){
-    if(!data || typeof data !== "object")
-      return callback({message: "Data must be a JSON serializable Object", code:40000, statusCode: 400});
+  enter(data: any, callback: errorCallback) {
+    if (!data || typeof data !== 'object') {
+      return callback({ message: 'Data must be a JSON serializable Object', code: 40000, statusCode: 400 });
+    }
+
     this.channel.presence.enter(data, callback);
   }
 
-  leave(callback: errorCallback){
+  leave(callback: errorCallback) {
     this.channel.presence.leave(undefined, callback);
   }
 
-  members(callback: (err: ErrorInfo | undefined, members: unknown[])=>void){
+  members(callback: (err: ErrorInfo | undefined, members: unknown[]) => void) {
     let members = this.channel.presence.members.list({});
-    callback(undefined, members.map((value: PresenceMessage)=>({
-      id: value.id,
-      data: value.data,
-    })));
+
+    callback(
+      undefined,
+      members.map((value: PresenceMessage) => ({
+        id: value.id,
+        data: value.data,
+      }))
+    );
   }
 }
 
 type SpaceOptions = {
   data?: any;
-}
-
+};
 
 export default Spaces;
--- test/realtime/space.test.js
+++ test/realtime/space.test.js
@@ -1,6 +1,5 @@
 define(['ably', 'shared_helper', 'async', 'chai'], function (Ably, helper, async, { expect }) {
-  describe('realtime/space', ()=>{
-
+  describe.only('realtime/space', () => {
     before(function (done) {
       helper.setupApp(function (err) {
         if (err) {
@@ -10,93 +9,90 @@ define(['ably', 'shared_helper', 'async', 'chai'], function (Ably, helper, async
       });
     });
 
-    describe('get', ()=>{
-
-      it('should create a space if it does not exist', (done)=>{
+    describe('get', () => {
+      it('should create a space if it does not exist', (done) => {
         let realtime, err;
         try {
           realtime = helper.AblyRealtime();
-          expect(realtime.spaces.spaces["test_space"]).to.equal(undefined);
-          let space = realtime.spaces.get("test_space", {});
-          expect(realtime.spaces.spaces["test_space"]).to.equal(space);
-
-        }catch(e){
+          expect(realtime.spaces.spaces['test_space']).to.equal(undefined);
+          let space = realtime.spaces.get('test_space', {});
+          expect(realtime.spaces.spaces['test_space']).to.equal(space);
+        } catch (e) {
           err = e;
-        }finally{
+        } finally {
           helper.closeAndFinish(done, realtime, err);
         }
       });
 
-      it('should return an existing space', (done)=>{
+      it('should return an existing space', (done) => {
         let realtime, err;
         try {
           realtime = helper.AblyRealtime();
-          let space1 = realtime.spaces.get("test_space", {});
-          let space2 = realtime.spaces.get("test_space", {});
+          let space1 = realtime.spaces.get('test_space', {});
+          let space2 = realtime.spaces.get('test_space', {});
           expect(space1).to.equal(space2);
-        }catch(e){
+        } catch (e) {
           err = e;
-        }finally{
+        } finally {
           helper.closeAndFinish(done, realtime, err);
         }
       });
 
-      it('should throw an error when getting a space with no name', (done)=>{
+      it('should throw an error when getting a space with no name', (done) => {
         let realtime, err;
         try {
           realtime = helper.AblyRealtime();
-          expect(()=>realtime.spaces.get("", {})).to.throw();
-        }catch(e){
+          expect(() => realtime.spaces.get('', {})).to.throw();
+        } catch (e) {
           err = e;
-        }finally{
+        } finally {
           helper.closeAndFinish(done, realtime, err);
         }
       });
 
-      it('should validate the space name', (done)=>{
+      it('should validate the space name', (done) => {
         let realtime, err;
         try {
           realtime = helper.AblyRealtime();
-          expect(()=>realtime.spaces.get(false, {})).to.throw();
-          expect(()=>realtime.spaces.get({}, {})).to.throw();
-          expect(()=>realtime.spaces.get(null, {})).to.throw();
-
-        }catch(e){
+          expect(() => realtime.spaces.get(false, {})).to.throw();
+          expect(() => realtime.spaces.get({}, {})).to.throw();
+          expect(() => realtime.spaces.get(null, {})).to.throw();
+        } catch (e) {
           err = e;
-        }finally{
+        } finally {
           helper.closeAndFinish(done, realtime, err);
         }
       });
     });
 
-    describe('enter', ()=>{
-      it('should successfully enter the space with the provided data', (done)=>{
+    describe('enter', () => {
+      it('should successfully enter the space with the provided data', (done) => {
         let realtime, err;
         try {
           realtime = helper.AblyRealtime();
-          let space = realtime.spaces.get("test_space", {});
+          let space = realtime.spaces.get('test_space', {});
 
-          realtime.channels.get("_ably_space_test_space").presence.subscribe("enter", (d)=>{
-            expect(d.data.name).to.equal("Example");
+          realtime.channels.get('_ably_space_test_space').presence.subscribe('enter', (d) => {
+            expect(d.data.name).to.equal('Example');
           });
 
-          space.enter({name: "Example"}, (err)=>{
+          space.enter({ name: 'Example' }, (err) => {
             expect(err).to.equal(undefined);
           });
-        }catch(e){
+        } catch (e) {
           err = e;
-        }finally{
+        } finally {
           helper.closeAndFinish(done, realtime, err);
         }
       });
 
-      it.only('should fail if invalid data is passed', (done)=>{
+      it('should fail if invalid data is passed', (done) => {
         let realtime, err;
         try {
           realtime = helper.AblyRealtime();
-          let space = realtime.spaces.get("test_space", {});
+          let space = realtime.spaces.get('test_space', {});
 
-          let callback = (err)=>{
+          let callback = (err) => {
             expect(err.code).to.equal(40000);
           };
 
@@ -104,26 +100,20 @@ define(['ably', 'shared_helper', 'async', 'chai'], function (Ably, helper, async
           space.enter(null, callback);
           space.enter(false, callback);
           space.enter(true, callback);
-        }catch(e){
+        } catch (e) {
           err = e;
-        }finally{
+        } finally {
           helper.closeAndFinish(done, realtime, err);
         }
       });
 
-      it('should fail you try and enter a space that you are already in', ()=>{
-
-      });
+      it('should fail you try and enter a space that you are already in', () => {});
     });
 
-    describe('leave', ()=>{
-      it('should successfully leave the space', ()=>{
+    describe('leave', () => {
+      it('should successfully leave the space', () => {});
 
-      });
-
-      it('should fail if you leave a space that you have not entered', ()=>{
-
-      });
-    })
-  })
+      it('should fail if you leave a space that you have not entered', () => {});
+    });
+  });
 });
