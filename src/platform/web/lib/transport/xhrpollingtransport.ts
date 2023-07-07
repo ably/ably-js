2fd7cdb5f2918585c2cf75df6c83b65daaec9974
Owen Pearson <owen.pearson@ably.com>
2023-07-07 15:57:56 +0100

    refactor: convert xhrpollingtransport.ts to typescript

--- src/platform/web/lib/transport/xhrpollingtransport.ts
+++ src/platform/web/lib/transport/xhrpollingtransport.ts
@@ -1,35 +1,42 @@
-import * as Utils from '../../../../common/lib/util/utils';
 import Platform from '../../../../common/platform';
 import CometTransport from '../../../../common/lib/transport/comettransport';
 import XHRRequest from './xhrrequest';
+import ConnectionManager, { TransportParams } from 'common/lib/transport/connectionmanager';
+import Auth from 'common/lib/client/auth';
+import { RequestParams } from 'common/types/http';
 
-var XHRPollingTransport = function (connectionManager) {
-  var shortName = 'xhr_polling';
-
-  function XHRPollingTransport(connectionManager, auth, params) {
+var shortName = 'xhr_polling';
+class XHRPollingTransport extends CometTransport {
+  shortName = shortName;
+  constructor(connectionManager: ConnectionManager, auth: Auth, params: TransportParams) {
+    super(connectionManager, auth, params);
     params.stream = false;
-    CometTransport.call(this, connectionManager, auth, params);
     this.shortName = shortName;
   }
-  Utils.inherits(XHRPollingTransport, CometTransport);
 
-  XHRPollingTransport.isAvailable = function () {
+  static isAvailable() {
     return Platform.Config.xhrSupported && Platform.Config.allowComet;
-  };
-
-  XHRPollingTransport.prototype.toString = function () {
-    return 'XHRPollingTransport; uri=' + this.baseUri + '; isConnected=' + this.isConnected;
-  };
-
-  XHRPollingTransport.prototype.createRequest = function (uri, headers, params, body, requestMode) {
-    return XHRRequest.createRequest(uri, headers, params, body, requestMode, this.timeouts);
-  };
-
-  if (typeof connectionManager !== 'undefined' && XHRPollingTransport.isAvailable()) {
-    connectionManager.supportedTransports[shortName] = XHRPollingTransport;
   }
 
-  return XHRPollingTransport;
-};
+  toString() {
+    return 'XHRPollingTransport; uri=' + this.baseUri + '; isConnected=' + this.isConnected;
+  }
 
-export default XHRPollingTransport;
+  createRequest(
+    uri: string,
+    headers: Record<string, string>,
+    params: RequestParams,
+    body: unknown,
+    requestMode: number
+  ) {
+    return XHRRequest.createRequest(uri, headers, params, body, requestMode, this.timeouts);
+  }
+}
+
+function initialiseTransport(connectionManager: any): typeof XHRPollingTransport {
+  if (XHRPollingTransport.isAvailable()) connectionManager.supportedTransports[shortName] = XHRPollingTransport;
+
+  return XHRPollingTransport;
+}
+
+export default initialiseTransport;
