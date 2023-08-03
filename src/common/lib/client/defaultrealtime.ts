805304f4e3520945a68806e3e2dc0f850f944f1e
Lawrence Forooghian <lawrence@forooghian.com>
2023-08-03 05:33:29 -0300

    Move remaining static properties from BaseRealtime to DefaultRealtime
    These properties are only used by the tests and are not part of the
    public API. Let’s move them out of BaseRealtime to make it more
    tree-shakable.

--- src/common/lib/client/defaultrealtime.ts
+++ src/common/lib/client/defaultrealtime.ts
@@ -1,6 +1,9 @@
 import BaseRealtime from './baserealtime';
 import ClientOptions from '../../types/ClientOptions';
 import { allCommonModules } from './modulesmap';
+import * as Utils from '../util/utils';
+import ConnectionManager from '../transport/connectionmanager';
+import ProtocolMessage from '../types/protocolmessage';
 
 /**
  `DefaultRealtime` is the class that the non tree-shakable version of the SDK exports as `Realtime`. It ensures that this version of the SDK includes all of the functionality which is optionally available in the tree-shakable version.
@@ -9,4 +12,8 @@ export class DefaultRealtime extends BaseRealtime {
   constructor(options: ClientOptions) {
     super(options, allCommonModules);
   }
+
+  static Utils = Utils;
+  static ConnectionManager = ConnectionManager;
+  static ProtocolMessage = ProtocolMessage;
 }
