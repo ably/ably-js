9b946275f8b20432fe815fb951ee147a18c6ad62
Lawrence Forooghian <lawrence@forooghian.com>
2023-08-17 10:56:47 -0300

    CI: Perform a check of whether tree-shaking is working
    Emit an error if adding a tree-shakable module does not increase the
    bundle size (this means that the module is not being tree-shaken
    correctly).

--- scripts/moduleReport.js
+++ scripts/moduleReport.js
@@ -27,12 +27,30 @@ function getImportSize(modules) {
   return result.metafile.outputs[outfile].bytes;
 }
 
+const errors = [];
+
 ['BaseRest', 'BaseRealtime'].forEach((baseClient) => {
+  const baseClientSize = getImportSize([baseClient]);
+
   // First display the size of the base client
-  console.log(`${baseClient}: ${formatBytes(getImportSize([baseClient]))}`);
+  console.log(`${baseClient}: ${formatBytes(baseClientSize)}`);
 
   // Then display the size of each module together with the base client
   moduleNames.forEach((moduleName) => {
-    console.log(`${baseClient} + ${moduleName}: ${formatBytes(getImportSize([baseClient, moduleName]))}`);
+    const size = getImportSize([baseClient, moduleName]);
+    console.log(`${baseClient} + ${moduleName}: ${formatBytes(size)}`);
+
+    if (!(baseClientSize < size)) {
+      // Emit an error if adding the module does not increase the bundle size
+      // (this means that the module is not being tree-shaken correctly).
+      errors.push(new Error(`Adding ${moduleName} to ${baseClient} does not increase the bundle size.`));
+    }
   });
 });
+
+if (errors.length > 0) {
+  for (const error of errors) {
+    console.log(error.message);
+  }
+  process.exit(1);
+}
