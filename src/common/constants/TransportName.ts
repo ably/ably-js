17fce5575ba52f8ef658a28d95e62ba0451a89de
Lawrence Forooghian <lawrence@forooghian.com>
2023-08-24 15:20:40 -0300

    Fix inflection of TransportNames enum name

--- src/common/constants/TransportName.ts
+++ src/common/constants/TransportName.ts
@@ -1,8 +1,8 @@
-enum TransportNames {
+enum TransportName {
   WebSocket = 'web_socket',
   Comet = 'comet',
   XhrStreaming = 'xhr_streaming',
   XhrPolling = 'xhr_polling',
 }
 
-export default TransportNames;
+export default TransportName;
--- src/platform/nodejs/lib/util/defaults.ts
+++ src/platform/nodejs/lib/util/defaults.ts
@@ -1,14 +1,14 @@
 import IDefaults from '../../../../common/types/IDefaults';
-import TransportNames from '../../../../common/constants/TransportNames';
+import TransportName from '../../../../common/constants/TransportName';
 
 const Defaults: IDefaults = {
   connectivityCheckUrl: 'https://internet-up.ably-realtime.com/is-the-internet-up.txt',
   /* Note: order matters here: the base transport is the leftmost one in the
    * intersection of baseTransportOrder and the transports clientOption that's supported. */
-  defaultTransports: [TransportNames.WebSocket],
-  baseTransportOrder: [TransportNames.Comet, TransportNames.WebSocket],
-  transportPreferenceOrder: [TransportNames.Comet, TransportNames.WebSocket],
-  upgradeTransports: [TransportNames.WebSocket],
+  defaultTransports: [TransportName.WebSocket],
+  baseTransportOrder: [TransportName.Comet, TransportName.WebSocket],
+  transportPreferenceOrder: [TransportName.Comet, TransportName.WebSocket],
+  upgradeTransports: [TransportName.WebSocket],
   restAgentOptions: { maxSockets: 40, keepAlive: true },
 };
 
