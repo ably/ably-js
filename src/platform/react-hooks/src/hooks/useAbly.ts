7af5a4b07721da42c42b46983fd89d2aa5ed3a03
Owen Pearson <owenp@Owens-MacBook-Pro-2.local>
2023-11-20 12:18:26 +0000

    fix: use 'ably' as import path from react-hooks
    The import paths here only worked before by accident - tsc would compile
    then to `from '../../../ably'` which in most cases would resolve to
    `node_modules/ably` however in certain environments (pnpm) this would
    not be the case and therefore the types would not be imported correctly.
    I've updated the tsconfig compilerOptions.paths field so that these are
    resolved correctly for development, and when consumed as a library these
    will just import directly from the 'ably' module.

--- src/platform/react-hooks/src/hooks/useAbly.ts
+++ src/platform/react-hooks/src/hooks/useAbly.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getContext } from '../AblyProvider.js';
-import * as API from '../../../../../callbacks.js';
+import * as API from 'ably';
 
 export function useAbly(id = 'default'): API.Types.RealtimePromise {
   const client = React.useContext(getContext(id)) as API.Types.RealtimePromise;
