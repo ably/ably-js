dd7c83ea910c580a4ee20f72cf19babd59351b76
Lawrence Forooghian <lawrence@forooghian.com>
2023-12-13 17:45:17 -0300

    Emit JUnit report from tests
    The test reports emitted by mocha-junit-reporter have <testcase> tags
    with slightly weird `name` and `classname` properties, e.g.
    
    <testsuite name="rest/defaults">
        <testcase name="rest/defaults Init with given environment" classname="Init with given environment">
        <testcase/>
    </testsuite>
    
    This looks a bit odd in the observability server web UI, which displays
    the testcase.name and testcase.classname properties. I don’t know
    whether it’d be better to do some pre-upload manipulation of the ably-js
    reports, or to change the fields displayed in the web UI. Not going to
    do anything about it now; it’s usable enough and I don’t want to mess up
    the display of the ably-cocoa test reports.
    
    Co-authored-by: Owen Pearson <owen.pearson@ably.com>

---
+++ test/support/mocha_reporter.js
@@ -0,0 +1,20 @@
+const Mocha = require('mocha');
+const MochaJUnitReporter = require('./mocha_junit_reporter/build/node');
+const path = require('path');
+const jUnitDirectoryPath = require('./junit_directory_path');
+
+/**
+ * Logs test results to the console (by extending the default `Spec` reporter) and also emits a JUnit XML file.
+ */
+class Reporter extends Mocha.reporters.Spec {
+  jUnitReporter;
+
+  constructor(runner, options) {
+    super(runner, options);
+    const jUnitFileName = `node-${process.version.split('.')[0]}.junit`;
+    const jUnitFilePath = path.join(jUnitDirectoryPath, jUnitFileName);
+    this.jUnitReporter = new MochaJUnitReporter(runner, { reporterOptions: { mochaFile: jUnitFilePath } });
+  }
+}
+
+module.exports = Reporter;
