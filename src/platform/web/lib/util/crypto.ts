ably/ably-js

commit e14f1bf7b8996c74f91f5c0486a1d734b0c4cbe6

Make CipherParams.key an ArrayBuffer on web instead of a WordArray
Preparation for #1292 (using Web Crypto API for encrypting and
decrypting).

src/platform/web/lib/util/crypto.ts
@@ -1,5 +1,4 @@
 import WordArray from 'crypto-js/build/lib-typedarrays';
-import { parse as parseBase64 } from 'crypto-js/build/enc-base64';
 import CryptoJS from 'crypto-js/build';
 import Logger from '../../../../common/lib/util/logger';
 import ErrorInfo from 'common/lib/types/errorinfo';
@@ -26,20 +25,15 @@ var CryptoFactory = function (config: IPlatformConfig, bufferUtils: typeof Buffe
   var DEFAULT_BLOCKLENGTH = 16; // bytes
   var DEFAULT_BLOCKLENGTH_WORDS = 4; // 32-bit words
   var UINT32_SUP = 0x100000000;
-  var INT32_SUP = 0x80000000;
 
   /**
    * Internal: generate an array of secure random words corresponding to the given length of bytes
    * @param bytes
    * @param callback
    */
-  var generateRandom: (byteLength: number, callback: (error: Error | null, result: WordArray | null) => void) => void;
+  var generateRandom: (byteLength: number, callback: (error: Error | null, result: ArrayBuffer | null) => void) => void;
   if (config.getRandomArrayBuffer) {
-    generateRandom = (byteLength, callback) => {
-      config.getRandomArrayBuffer!(byteLength, (error, result) => {
-        callback(error, result ? bufferUtils.toWordArray(result) : null);
-      });
-    };
+    generateRandom = config.getRandomArrayBuffer;
   } else if (typeof Uint32Array !== 'undefined' && config.getRandomValues) {
     var blockRandomArray = new Uint32Array(DEFAULT_BLOCKLENGTH_WORDS);
     generateRandom = function (bytes, callback) {
@@ -47,7 +41,7 @@ var CryptoFactory = function (config: IPlatformConfig, bufferUtils: typeof Buffe
         nativeArray = words == DEFAULT_BLOCKLENGTH_WORDS ? blockRandomArray : new Uint32Array(words);
       config.getRandomValues!(nativeArray, function (err) {
         if (typeof callback !== 'undefined') {
-          callback(err, bufferUtils.toWordArray(nativeArray));
+          callback(err, bufferUtils.toArrayBuffer(nativeArray));
         }
       });
     };
@@ -59,16 +53,12 @@ var CryptoFactory = function (config: IPlatformConfig, bufferUtils: typeof Buffe
         'Warning: the browser you are using does not support secure cryptographically secure randomness generation; falling back to insecure Math.random()'
       );
       var words = bytes / 4,
-        array = new Array(words);
+        array = new Uint32Array(words);
       for (var i = 0; i < words; i++) {
-        /* cryptojs wordarrays use signed ints. When WordArray.create is fed a
-         * Uint32Array unsigned are converted to signed automatically, but when
-         * fed a normal array they aren't, so need to do so ourselves by
-         * subtracting INT32_SUP */
-        array[i] = Math.floor(Math.random() * UINT32_SUP) - INT32_SUP;
+        array[i] = Math.floor(Math.random() * UINT32_SUP);
       }
 
-      callback(null, WordArray.create(array));
+      callback(null, bufferUtils.toArrayBuffer(array));
     };
   }
 
@@ -149,9 +139,9 @@ var CryptoFactory = function (config: IPlatformConfig, bufferUtils: typeof Buffe
     algorithm: string;
     keyLength: number;
     mode: string;
-    key: WordArray;
+    key: ArrayBuffer;
 
-    constructor(algorithm: string, keyLength: number, mode: string, key: WordArray) {
+    constructor(algorithm: string, keyLength: number, mode: string, key: ArrayBuffer) {
       this.algorithm = algorithm;
       this.keyLength = keyLength;
       this.mode = mode;
@@ -190,20 +180,22 @@ var CryptoFactory = function (config: IPlatformConfig, bufferUtils: typeof Buffe
      * AES), mode (defaults to 'cbc')
      */
     static getDefaultParams(params: API.Types.CipherParamOptions) {
-      var key: WordArray;
+      var key: ArrayBuffer;
 
       if (!params.key) {
         throw new Error('Crypto.getDefaultParams: a key is required');
       }
 
       if (typeof params.key === 'string') {
-        key = parseBase64(normaliseBase64(params.key));
+        key = bufferUtils.toArrayBuffer(bufferUtils.base64Decode(normaliseBase64(params.key)));
+      } else if (params.key instanceof ArrayBuffer) {
+        key = params.key;
       } else {
-        key = bufferUtils.toWordArray(params.key); // Expect key to be an Array, ArrayBuffer, or WordArray at this point
+        key = bufferUtils.toArrayBuffer(params.key);
       }
 
       var algorithm = params.algorithm || DEFAULT_ALGORITHM;
-      var keyLength = key.words.length * (4 * 8);
+      var keyLength = key.byteLength * 8;
       var mode = params.mode || DEFAULT_MODE;
       var cipherParams = new CipherParams(algorithm, keyLength, mode, key);
 
@@ -309,8 +301,9 @@ var CryptoFactory = function (config: IPlatformConfig, bufferUtils: typeof Buffe
               callback(err, null);
               return;
             }
-            self.encryptCipher = CryptoJS.algo[self.cjsAlgorithm].createEncryptor(self.key, { iv: iv! });
-            self.iv = iv;
+            const ivWordArray = bufferUtils.toWordArray(iv!);
+            self.encryptCipher = CryptoJS.algo[self.cjsAlgorithm].createEncryptor(self.key, { iv: ivWordArray });
+            self.iv = ivWordArray;
             then();
           });
         }
@@ -352,7 +345,8 @@ var CryptoFactory = function (config: IPlatformConfig, bufferUtils: typeof Buffe
           callback(err, null);
           return;
         }
-        callback(null, self.encryptCipher!.process(randomBlock!));
+        const randomBlockWordArray = bufferUtils.toWordArray(randomBlock!);
+        callback(null, self.encryptCipher!.process(randomBlockWordArray));
       });
     }
   }

ably.d.ts
@@ -2904,11 +2904,11 @@ declare namespace Types {
    */
   type CipherKeyParam = ArrayBuffer | Uint8Array | string; // if string must be base64-encoded
   /**
-   * Typed differently depending on platform. (`WordArray` in browser, `Buffer` in node)
+   * Typed differently depending on platform. (`ArrayBuffer` in browser, `Buffer` in node)
    *
    * @internal
    */
-  type CipherKey = unknown; // WordArray on browsers, Buffer on node, using unknown as
+  type CipherKey = unknown; // ArrayBuffer on browsers, Buffer on node, using unknown as
   // user should not be interacting with it - output of getDefaultParams should be used opaquely
 
   /**

test/realtime/crypto.test.js
@@ -124,8 +124,8 @@ define(['ably', 'shared_helper', 'async', 'chai'], function (Ably, helper, async
           return;
         }
         try {
-          /* .length for a nodejs buffer, .sigbytes for a browser CryptoJS WordArray */
-          expect(key.length || key.sigBytes).to.equal(8, 'generated key is the correct length');
+          /* .length for a nodejs buffer, .byteLength for a browser ArrayBuffer */
+          expect(key.length || key.byteLength).to.equal(8, 'generated key is the correct length');
           done();
         } catch (err) {
           done(err);
@@ -141,7 +141,7 @@ define(['ably', 'shared_helper', 'async', 'chai'], function (Ably, helper, async
           return;
         }
         try {
-          expect(key.length || key.sigBytes).to.equal(32, 'generated key is the default length');
+          expect(key.length || key.byteLength).to.equal(32, 'generated key is the default length');
           done();
         } catch (err) {
           done(err);

commit fccd4ac950858634ccb7e0120ad5520261be63b8

Fixes same array in memory was used for `generateRandom` in web crypto
I'm not entirely sure why `generateRandom` was reusing the same
Uint32Array array stored in `blockRandomArray` variable between calls.
It may be the relic of the past, or a bug that was only discovered
recently, since we stopped copying this array for some operations.

This was causing race condition errors in crypto browser tests,
specifically with `multiple_send_*` tests. They generated cipher key
using `Crypto.generateRandomKey()` and thus shared the same array in
memory which was used for iv generation on each message encryption.
This way, cipher key was actually changed in memory after each
`CBCCipher.encrypt` call, and if two messages were encrypted back to
back, then next call to `CBCCipher.decrypt` would fail due to incorrect
cipher key.

This regression was introduced in e14f1bf, when we switched from using
WordArray and `BufferUtils.toWordArray`, which was copying the array, to
ArrayBuffer and `BufferUtils.toArrayBuffer`, which is returning the same
array in memory.

Resolves #1557

src/platform/web/lib/util/crypto.ts
@@ -33,8 +33,8 @@ var createCryptoClass = function (config: IPlatformConfig, bufferUtils: typeof B
   if (config.getRandomArrayBuffer) {
     generateRandom = config.getRandomArrayBuffer;
   } else if (typeof Uint32Array !== 'undefined' && config.getRandomValues) {
-    var blockRandomArray = new Uint32Array(DEFAULT_BLOCKLENGTH_WORDS);
     generateRandom = function (bytes, callback) {
+      var blockRandomArray = new Uint32Array(DEFAULT_BLOCKLENGTH_WORDS);
       var words = bytes / 4,
         nativeArray = words == DEFAULT_BLOCKLENGTH_WORDS ? blockRandomArray : new Uint32Array(words);
       config.getRandomValues!(nativeArray, function (err) {