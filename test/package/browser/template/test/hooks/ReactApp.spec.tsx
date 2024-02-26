4cb3b00dd347cd29c2fdbe8ae3f0c7e4efc16fe4
Andrew Bulat <andrii.bulat@gmail.com>
2024-02-26 22:40:12 +0000

    Add `console` and `pageerror` listeners to react-hooks package test
    By default playwright doesn't output console messages and errors
    happened during rendering React components to stdout of node.js test
    process. This means we're getting a generic "Test timeout of 10000ms
    exceeded." error and nothing else when running react-hooks package test
    and there is a rendering error [1].
    
    This commit adds listeners for 'console' and 'pageerror' events on
    playwright page, so we can output actual errors to node.js stdout. So we
    also get error messages like here [2].
    
    [1] https://github.com/ably/ably-js/actions/runs/8056450454/job/22005521704?pr=1639
    [2] https://github.com/ably/ably-js/actions/runs/8056604349/job/22005998807?pr=1639

--- test/package/browser/template/test/hooks/ReactApp.spec.tsx
+++ test/package/browser/template/test/hooks/ReactApp.spec.tsx
@@ -6,6 +6,16 @@ test.describe('NPM package', () => {
   for (const scenario of [{ name: 'react export' }]) {
     test.describe(scenario.name, () => {
       test('can be imported and provides access to Ably functionality', async ({ mount, page }) => {
+        page.on('console', (message) => {
+          if (['error', 'warning'].includes(message.type())) {
+            console.log(`Console ${message.type()}:`, message);
+          }
+        });
+
+        page.on('pageerror', (err) => {
+          console.log('Uncaught exception:', err);
+        });
+
         const pageResultPromise = new Promise<void>((resolve, reject) => {
           page.exposeFunction('onResult', (error: Error | null) => {
             if (error) {
@@ -17,7 +27,7 @@ test.describe('NPM package', () => {
         });
 
         const component = await mount(<App />);
-        await pageResultPromise;
+        await expect(pageResultPromise).resolves.not.toThrow();
         await expect(component).toContainText('Ably NPM package test (react export)');
       });
     });
