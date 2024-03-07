30668049ebc34ee08dce5e6e234e4ca355d051ed
owenpearson <owen.pearson@ably.com>
2024-03-07 15:13:59 +0000

    build: cleanup unused grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,6 @@ var stripLogsPlugin = require('./grunt/esbuild/strip-logs').default;
 var kexec = require('kexec');
 
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-webpack');
 
   var dirs = {
@@ -22,13 +20,6 @@ module.exports = function (grunt) {
     static: 'build',
     dest: 'build',
   };
-
-  function compilerSpec(src, dest) {
-    return {
-      src: src,
-      dest: dest || src.replace(/\.js/, '.min.js'),
-    };
-  }
 
   async function execExternalPromises(cmd) {
     grunt.log.ok('Executing ' + cmd);
@@ -56,26 +47,10 @@ module.exports = function (grunt) {
 
   var gruntConfig = {
     dirs: dirs,
-    pkgVersion: grunt.file.readJSON('package.json').version,
     webpack: {
       all: Object.values(webpackConfig),
       node: [webpackConfig.node],
       browser: [webpackConfig.browser, webpackConfig.browserMin, webpackConfig.mochaJUnitReporterBrowser],
-    },
-  };
-
-  gruntConfig.bump = {
-    options: {
-      files: ['package.json', 'README.md'],
-      globalReplace: true,
-      commit: true,
-      commitMessage: 'Regenerate and release version %VERSION%',
-      commitFiles: [], // Add files manually as can't add new files with a commit flag
-      createTag: true,
-      tagName: '%VERSION%',
-      tagMessage: 'Version %VERSION%',
-      push: false,
-      prereleaseName: 'beta',
     },
   };
 
@@ -162,59 +137,6 @@ module.exports = function (grunt) {
     'checkGitSubmodules',
     'mocha:webserver',
   ]);
-
-  grunt.registerTask('release:refresh-pkgVersion', 'Refreshes GruntConfig.pkgVersion', function () {
-    grunt.config('pkgVersion', grunt.file.readJSON('package.json').version);
-    grunt.log.ok('pkgVersion updated');
-  });
-
-  grunt.registerTask('release:git-add-generated', 'Adds generated files to the git staging area', function () {
-    var done = this.async();
-    var generatedFiles = [
-      gruntConfig.dirs.common + '/lib/util/defaults.js',
-      gruntConfig.dirs.fragments + '/license.js',
-      'package.json',
-      'package-lock.json',
-      'README.md',
-      'test/support/browser_file_list.js',
-    ];
-    var cmd = 'git add -A ' + generatedFiles.join(' ');
-    grunt.log.ok('Executing ' + cmd);
-
-    require('child_process').exec(cmd, function (err, stdout, stderr) {
-      if (err) {
-        grunt.fatal('git add . -A failed with ' + stderr);
-      }
-      done();
-    });
-  });
-
-  grunt.registerTask('release:git-push', 'Pushes to git', execExternal('git push origin main --follow-tags'));
-
-  grunt.registerTask('release:ably-deploy', 'Deploys to ably CDN', function () {
-    var version = grunt.file.readJSON('package.json').version,
-      cmd = 'node scripts/cdn_deploy.js --skipCheckout --tag ' + version;
-    console.log('Publishing version ' + version + ' of the library to the CDN');
-    execExternal(cmd).call(this);
-  });
-
-  grunt.registerTask('release:deploy', 'Pushes a new release to github and then deploys to the Ably CDN', function () {
-    grunt.task.run(['release:git-push', 'release:ably-deploy']);
-  });
-
-  grunt.registerTask(
-    'release',
-    'Increments the version, regenerates, and makes a tagged commit. Run as "grunt release:type", where "type" is "major", "minor", "patch", "prepatch", etc.)',
-    function (versionType) {
-      grunt.task.run([
-        'bump-only:' + versionType,
-        'release:refresh-pkgVersion',
-        'all',
-        'release:git-add-generated',
-        'bump-commit',
-      ]);
-    },
-  );
 
   (function () {
     const baseDir = path.join(__dirname, 'test', 'package', 'browser');
