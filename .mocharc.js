7dd2adcc96b516ec57b6ddae963976a4043d949a
owenpearson <owen.pearson@ably.com>
2024-03-11 18:18:03 +0000

    test: dynamically add mocha config specs if no specs passed via args

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,18 @@
-module.exports = {
+const config = {
   require: ['source-map-support/register', 'test/support/modules_helper.js', 'test/support/test_helper.js'],
   reporter: 'test/support/mocha_reporter.js',
-  spec: ['test/realtime/*.test.js', 'test/rest/*.test.js'],
 };
+
+// mocha has a ridiculous issue (https://github.com/mochajs/mocha/issues/4100) that command line
+// specs don't override config specs; they are merged instead, so you can't run a single test file
+// if you've defined specs in your config. therefore we work around it by only adding specs to the
+// config if none are passed as arguments
+if (!process.argv.slice(2).some(isTestFile)) {
+  config.spec = ['test/realtime/*.test.js', 'test/rest/*.test.js'];
+}
+
+function isTestFile(arg) {
+  return arg.match(/\.test.js$/);
+}
+
+module.exports = config;
