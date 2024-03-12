2454a287ceebcad3008b3024509b26c1c1804657
Andrew Bulat <andrii.bulat@gmail.com>
2024-03-12 21:00:10 +0000

    Refactor onMount/onUnmount functions in `usePresence` hook

--- src/platform/react-hooks/src/hooks/usePresence.ts
+++ src/platform/react-hooks/src/hooks/usePresence.ts
@@ -1,5 +1,5 @@
 import type * as Ably from 'ably';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { ChannelParameters } from '../AblyReactHooks.js';
 import { useAbly } from './useAbly.js';
 import { useChannelInstance } from './useChannelInstance.js';
@@ -26,27 +26,37 @@ export function usePresence<T = any>(
   const ably = useAbly(params.ablyId);
   const { channel } = useChannelInstance(params.ablyId, params.channelName);
   const { connectionError, channelError } = useStateErrors(params);
+  // we can't simply add messageOrPresenceObject to dependency list in our useCallback/useEffect hooks,
+  // since it will most likely cause an infinite loop of updates in cases when user calls this hook
+  // with an object literal instead of a state or memoized object.
+  // to prevent this from happening we store messageOrPresenceObject in a ref, and use that instead.
+  // note that it still prevents us from automatically re-entering presence with new messageOrPresenceObject if it changes.
+  // one of the options to fix this, is to use deep equals to check if the object has actually changed. see https://github.com/ably/ably-js/issues/1688.
+  const messageOrPresenceObjectRef = useRef(messageOrPresenceObject);
 
-  const onMount = async () => {
-    await channel.presence.enter(messageOrPresenceObject);
-  };
+  useEffect(() => {
+    messageOrPresenceObjectRef.current = messageOrPresenceObject;
+  }, [messageOrPresenceObject]);
 
-  const onUnmount = () => {
+  const onMount = useCallback(async () => {
+    await channel.presence.enter(messageOrPresenceObjectRef.current);
+  }, [channel.presence]);
+
+  const onUnmount = useCallback(() => {
     // if connection is in one of inactive states, leave call will produce exception
     if (channel.state === 'attached' && !INACTIVE_CONNECTION_STATES.includes(ably.connection.state)) {
       channel.presence.leave();
     }
-  };
+  }, [channel, ably.connection.state]);
 
-  const useEffectHook = () => {
-    if (!skip) onMount();
+  useEffect(() => {
+    if (skip) return;
+
+    onMount();
     return () => {
       onUnmount();
     };
-  };
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(useEffectHook, [skip]);
+  }, [skip, onMount, onUnmount]);
 
   const updateStatus = useCallback(
     (messageOrPresenceObject: T) => {
