ably/ably-js

commit e8c3012f7321cd080ab1ed7fb68e0ddea9151ec2

Use ClientOptions.plugins for modular variant
This updates the modular variant of the SDK to receive its functionality
via the ClientOptions.plugins mechanism which we restored in 5f56c45.
The aim of this change is to make sure that there is still only a single
mechanism for injecting functionality into the SDK, as there was
pre-5f56c45.

As part of this change, I’ve also replaced the use of the term “module”,
when used to refer to a piece of injectable functionality, with the term
“plugin”. The term “module” already has a defined meaning in ECMAScript
(see [1]; a module is a file, basically), so we don’t want to overload
it.

This new usage of ClientOptions.plugins (i.e. plugins unique to a given
client library) is allowed for by spec point PC4, which I added in
commit 79f9bd0 in [2] (awaiting approval at time of writing).

Resolves #1662.

[1] https://tc39.es/ecma262/multipage/ecmascript-language-scripts-and-modules.html#prod-Module
[2] https://github.com/ably/specification/pull/184

src/common/lib/client/baseclient.ts
@@ -9,10 +9,9 @@ import ClientOptions, { NormalisedClientOptions } from '../../types/ClientOption
 import * as API from '../../../../ably';
 
 import Platform from '../../platform';
-import { ModulesMap } from './modulesmap';
 import { Rest } from './rest';
 import { IUntypedCryptoStatic } from 'common/types/ICryptoStatic';
-import { throwMissingModuleError } from '../util/utils';
+import { throwMissingPluginError } from '../util/utils';
 import { MsgPack } from 'common/types/msgpack';
 import { HTTPRequestImplementations } from 'platform/web/lib/http/http';
 import { FilteredSubscriptions } from './filteredsubscriptions';
@@ -43,28 +42,27 @@ class BaseClient {
   readonly _Crypto: IUntypedCryptoStatic | null;
   readonly _MsgPack: MsgPack | null;
   // Extra HTTP request implementations available to this client, in addition to those in web’s Http.bundledRequestImplementations
-  readonly _additionalHTTPRequestImplementations: HTTPRequestImplementations;
+  readonly _additionalHTTPRequestImplementations: HTTPRequestImplementations | null;
   private readonly __FilteredSubscriptions: typeof FilteredSubscriptions | null;
 
-  constructor(options: ClientOptions | string, modules: ModulesMap) {
-    this._additionalHTTPRequestImplementations = modules;
+  constructor(options: ClientOptions) {
+    this._additionalHTTPRequestImplementations = options.plugins ?? null;
 
     if (!options) {
       const msg = 'no options provided';
       Logger.logAction(Logger.LOG_ERROR, 'BaseClient()', msg);
       throw new Error(msg);
     }
-    const optionsObj = Defaults.objectifyOptions(options);
 
-    Logger.setLog(optionsObj.logLevel, optionsObj.logHandler);
+    Logger.setLog(options.logLevel, options.logHandler);
     Logger.logAction(
       Logger.LOG_MICRO,
       'BaseClient()',
       'initialized with clientOptions ' + Platform.Config.inspect(options),
     );
 
-    this._MsgPack = modules.MsgPack ?? null;
-    const normalOptions = (this.options = Defaults.normaliseOptions(optionsObj, this._MsgPack));
+    this._MsgPack = options.plugins?.MsgPack ?? null;
+    const normalOptions = (this.options = Defaults.normaliseOptions(options, this._MsgPack));
 
     /* process options */
     if (normalOptions.key) {
@@ -97,21 +95,21 @@ class BaseClient {
     this.http = new Http(this);
     this.auth = new Auth(this, normalOptions);
 
-    this._rest = modules.Rest ? new modules.Rest(this) : null;
-    this._Crypto = modules.Crypto ?? null;
-    this.__FilteredSubscriptions = modules.MessageInteractions ?? null;
+    this._rest = options.plugins?.Rest ? new options.plugins.Rest(this) : null;
+    this._Crypto = options.plugins?.Crypto ?? null;
+    this.__FilteredSubscriptions = options.plugins?.MessageInteractions ?? null;
   }
 
   get rest(): Rest {
     if (!this._rest) {
-      throwMissingModuleError('Rest');
+      throwMissingPluginError('Rest');
     }
     return this._rest;
   }
 
   get _FilteredSubscriptions(): typeof FilteredSubscriptions {
     if (!this.__FilteredSubscriptions) {
-      throwMissingModuleError('MessageInteractions');
+      throwMissingPluginError('MessageInteractions');
     }
     return this.__FilteredSubscriptions;
   }

src/common/lib/client/baserealtime.ts
@@ -9,41 +9,42 @@ import ProtocolMessage from '../types/protocolmessage';
 import { ChannelOptions } from '../../types/channel';
 import ClientOptions from '../../types/ClientOptions';
 import * as API from '../../../../ably';
-import { ModulesMap, RealtimePresenceModule } from './modulesmap';
+import { ModularPlugins, RealtimePresencePlugin } from './modularplugins';
 import { TransportNames } from 'common/constants/TransportName';
 import { TransportImplementations } from 'common/platform';
+import Defaults from '../util/defaults';
 
 /**
  `BaseRealtime` is an export of the tree-shakable version of the SDK, and acts as the base class for the `DefaultRealtime` class exported by the non tree-shakable version.
  */
 class BaseRealtime extends BaseClient {
-  readonly _RealtimePresence: RealtimePresenceModule | null;
+  readonly _RealtimePresence: RealtimePresencePlugin | null;
   // Extra transport implementations available to this client, in addition to those in Platform.Transports.bundledImplementations
   readonly _additionalTransportImplementations: TransportImplementations;
   _channels: any;
   connection: Connection;
 
-  constructor(options: ClientOptions | string, modules: ModulesMap) {
-    super(options, modules);
+  constructor(options: ClientOptions | string) {
+    super(Defaults.objectifyOptions(options));
     Logger.logAction(Logger.LOG_MINOR, 'Realtime()', '');
-    this._additionalTransportImplementations = BaseRealtime.transportImplementationsFromModules(modules);
-    this._RealtimePresence = modules.RealtimePresence ?? null;
+    this._additionalTransportImplementations = BaseRealtime.transportImplementationsFromPlugins(this.options.plugins);
+    this._RealtimePresence = this.options.plugins?.RealtimePresence ?? null;
     this.connection = new Connection(this, this.options);
     this._channels = new Channels(this);
     if (this.options.autoConnect !== false) this.connect();
   }
 
-  private static transportImplementationsFromModules(modules: ModulesMap) {
+  private static transportImplementationsFromPlugins(plugins?: ModularPlugins) {
     const transports: TransportImplementations = {};
 
-    if (modules.WebSocketTransport) {
-      transports[TransportNames.WebSocket] = modules.WebSocketTransport;
+    if (plugins?.WebSocketTransport) {
+      transports[TransportNames.WebSocket] = plugins.WebSocketTransport;
     }
-    if (modules.XHRStreaming) {
-      transports[TransportNames.XhrStreaming] = modules.XHRStreaming;
+    if (plugins?.XHRStreaming) {
+      transports[TransportNames.XhrStreaming] = plugins.XHRStreaming;
     }
-    if (modules.XHRPolling) {
-      transports[TransportNames.XhrPolling] = modules.XHRPolling;
+    if (plugins?.XHRPolling) {
+      transports[TransportNames.XhrPolling] = plugins.XHRPolling;
     }
 
     return transports;

src/common/lib/client/baserest.ts
@@ -1,15 +1,15 @@
 import BaseClient from './baseclient';
 import ClientOptions from '../../types/ClientOptions';
-import { ModulesMap } from './modulesmap';
 import { Rest } from './rest';
+import Defaults from '../util/defaults';
 
 /**
  `BaseRest` is an export of the tree-shakable version of the SDK, and acts as the base class for the `DefaultRest` class exported by the non tree-shakable version.
 
- It always includes the `Rest` module.
+ It always includes the `Rest` plugin.
  */
 export class BaseRest extends BaseClient {
-  constructor(options: ClientOptions | string, modules: ModulesMap) {
-    super(options, { Rest, ...modules });
+  constructor(options: ClientOptions | string) {
+    super(Defaults.objectifyOptions(options, { Rest }));
   }
 }

src/common/lib/client/defaultrealtime.ts
@@ -1,6 +1,6 @@
 import BaseRealtime from './baserealtime';
 import ClientOptions from '../../types/ClientOptions';
-import { allCommonModules } from './modulesmap';
+import { allCommonModularPlugins } from './modularplugins';
 import * as Utils from '../util/utils';
 import ConnectionManager from '../transport/connectionmanager';
 import ProtocolMessage from '../types/protocolmessage';
@@ -16,6 +16,7 @@ import {
   fromValuesArray as presenceMessagesFromValuesArray,
 } from '../types/presencemessage';
 import { Http } from 'common/types/http';
+import Defaults from '../util/defaults';
 
 /**
  `DefaultRealtime` is the class that the non tree-shakable version of the SDK exports as `Realtime`. It ensures that this version of the SDK includes all of the functionality which is optionally available in the tree-shakable version.
@@ -27,18 +28,20 @@ export class DefaultRealtime extends BaseRealtime {
       throw new Error('Expected DefaultRealtime._MsgPack to have been set');
     }
 
-    super(options, {
-      ...allCommonModules,
-      Crypto: DefaultRealtime.Crypto ?? undefined,
-      MsgPack,
-      RealtimePresence: {
-        RealtimePresence,
-        presenceMessageFromValues,
-        presenceMessagesFromValuesArray,
-      },
-      WebSocketTransport: initialiseWebSocketTransport,
-      MessageInteractions: FilteredSubscriptions,
-    });
+    super(
+      Defaults.objectifyOptions(options, {
+        ...allCommonModularPlugins,
+        Crypto: DefaultRealtime.Crypto ?? undefined,
+        MsgPack,
+        RealtimePresence: {
+          RealtimePresence,
+          presenceMessageFromValues,
+          presenceMessagesFromValuesArray,
+        },
+        WebSocketTransport: initialiseWebSocketTransport,
+        MessageInteractions: FilteredSubscriptions,
+      }),
+    );
   }
 
   static Utils = Utils;

src/common/lib/client/defaultrest.ts
@@ -1,11 +1,12 @@
 import { BaseRest } from './baserest';
 import ClientOptions from '../../types/ClientOptions';
-import { allCommonModules } from './modulesmap';
+import { allCommonModularPlugins } from './modularplugins';
 import Platform from 'common/platform';
 import { DefaultMessage } from '../types/defaultmessage';
 import { MsgPack } from 'common/types/msgpack';
 import { DefaultPresenceMessage } from '../types/defaultpresencemessage';
 import { Http } from 'common/types/http';
+import Defaults from '../util/defaults';
 
 /**
  `DefaultRest` is the class that the non tree-shakable version of the SDK exports as `Rest`. It ensures that this version of the SDK includes all of the functionality which is optionally available in the tree-shakable version.
@@ -17,11 +18,13 @@ export class DefaultRest extends BaseRest {
       throw new Error('Expected DefaultRest._MsgPack to have been set');
     }
 
-    super(options, {
-      ...allCommonModules,
-      Crypto: DefaultRest.Crypto ?? undefined,
-      MsgPack: DefaultRest._MsgPack ?? undefined,
-    });
+    super(
+      Defaults.objectifyOptions(options, {
+        ...allCommonModularPlugins,
+        Crypto: DefaultRest.Crypto ?? undefined,
+        MsgPack: DefaultRest._MsgPack ?? undefined,
+      }),
+    );
   }
 
   private static _Crypto: typeof Platform.Crypto = null;

src/common/lib/util/defaults.ts
@@ -8,6 +8,7 @@ import IDefaults from '../../types/IDefaults';
 import { MsgPack } from 'common/types/msgpack';
 import { IUntypedCryptoStatic } from 'common/types/ICryptoStatic';
 import { ChannelOptions } from 'common/types/channel';
+import { ModularPlugins } from '../client/modularplugins';
 
 let agent = 'ably-js/' + version;
 
@@ -43,7 +44,7 @@ type CompleteDefaults = IDefaults & {
   getHosts(options: NormalisedClientOptions): string[];
   checkHost(host: string): void;
   getRealtimeHost(options: ClientOptions, production: boolean, environment: string): string;
-  objectifyOptions(options: ClientOptions | string): ClientOptions;
+  objectifyOptions(options: ClientOptions | string, modularPluginsToInclude?: ModularPlugins): ClientOptions;
   normaliseOptions(options: InternalClientOptions, MsgPack: MsgPack | null): NormalisedClientOptions;
   defaultGetHeaders(options: NormalisedClientOptions, headersOptions?: HeadersOptions): Record<string, string>;
   defaultPostHeaders(options: NormalisedClientOptions, headersOptions?: HeadersOptions): Record<string, string>;
@@ -181,11 +182,18 @@ export function getAgentString(options: ClientOptions): string {
   return agentStr;
 }
 
-export function objectifyOptions(options: ClientOptions | string): ClientOptions {
-  if (typeof options == 'string') {
-    return options.indexOf(':') == -1 ? { token: options } : { key: options };
+export function objectifyOptions(
+  options: ClientOptions | string,
+  modularPluginsToInclude?: ModularPlugins,
+): ClientOptions {
+  let optionsObj =
+    typeof options === 'string' ? (options.indexOf(':') == -1 ? { token: options } : { key: options }) : options;
+
+  if (modularPluginsToInclude) {
+    optionsObj = { ...optionsObj, plugins: { ...modularPluginsToInclude, ...optionsObj.plugins } };
   }
-  return options;
+
+  return optionsObj;
 }
 
 export function normaliseOptions(options: InternalClientOptions, MsgPack: MsgPack | null): NormalisedClientOptions {
@@ -270,7 +278,7 @@ export function normaliseOptions(options: InternalClientOptions, MsgPack: MsgPac
 export function normaliseChannelOptions(Crypto: IUntypedCryptoStatic | null, options?: ChannelOptions) {
   const channelOptions = options || {};
   if (channelOptions.cipher) {
-    if (!Crypto) Utils.throwMissingModuleError('Crypto');
+    if (!Crypto) Utils.throwMissingPluginError('Crypto');
     const cipher = Crypto.getCipher(channelOptions.cipher);
     channelOptions.cipher = cipher.cipherParams;
     channelOptions.channelCipher = cipher.cipher;

ably.d.ts
@@ -370,7 +370,7 @@ export interface ChannelMetrics {
 /**
  * Passes additional client-specific properties to the REST constructor or the Realtime constructor.
  */
-export interface ClientOptions extends AuthOptions {
+export interface ClientOptions<Plugins = CorePlugins> extends AuthOptions {
   /**
    * When `true`, the client connects to Ably as soon as it is instantiated. You can set this to `false` and explicitly connect to Ably using the {@link Connection.connect | `connect()`} method. The default is `true`.
    *
@@ -573,12 +573,17 @@ export interface ClientOptions extends AuthOptions {
   /**
    * A map between a plugin type and a plugin object.
    */
-  plugins?: {
-    /**
-     * A plugin capable of decoding `vcdiff`-encoded messages. For more information on how to configure a channel to use delta encoding, see the [documentation for the `@ably-forks/vcdiff-decoder` package](https://github.com/ably-forks/vcdiff-decoder#usage).
-     */
-    vcdiff?: any;
-  };
+  plugins?: Plugins;
+}
+
+/**
+ * Describes the {@link ClientOptions.plugins | plugins} accepted by all variants of the SDK.
+ */
+export interface CorePlugins {
+  /**
+   * A plugin capable of decoding `vcdiff`-encoded messages. For more information on how to configure a channel to use delta encoding, see the [documentation for the `@ably-forks/vcdiff-decoder` package](https://github.com/ably-forks/vcdiff-decoder#usage).
+   */
+  vcdiff?: any;
 }
 
 /**

commit 673aa5b3319c130a43701110844f983f1a8edfe9

Improve errors for incorrect constructor args
This fixes the regression that I introduced in e8c3012, which stopped
the user from getting a useful error message when not passing any args
to the constructor. I’ve also introduced a wider range of error messages
to give the user useful guidance depending on which variant of the
library they’re using.

Resolves #1700.

src/common/lib/client/baseclient.ts
@@ -48,12 +48,6 @@ class BaseClient {
   constructor(options: ClientOptions) {
     this._additionalHTTPRequestImplementations = options.plugins ?? null;
 
-    if (!options) {
-      const msg = 'no options provided';
-      Logger.logAction(Logger.LOG_ERROR, 'BaseClient()', msg);
-      throw new Error(msg);
-    }
-
     Logger.setLog(options.logLevel, options.logHandler);
     Logger.logAction(
       Logger.LOG_MICRO,

src/common/lib/client/baserealtime.ts
@@ -24,8 +24,16 @@ class BaseRealtime extends BaseClient {
   _channels: any;
   connection: Connection;
 
-  constructor(options: ClientOptions | string) {
-    super(Defaults.objectifyOptions(options));
+  /*
+   * The public typings declare that this only accepts an object, but since we want to emit a good error message in the case where a non-TypeScript user does one of these things:
+   *
+   * 1. passes a string (which is quite likely if they’re e.g. migrating from the default variant to the modular variant)
+   * 2. passes no argument at all
+   *
+   * tell the compiler that these cases are possible so that it forces us to handle them.
+   */
+  constructor(options?: ClientOptions | string) {
+    super(Defaults.objectifyOptions(options, false, 'BaseRealtime'));
     Logger.logAction(Logger.LOG_MINOR, 'Realtime()', '');
     this._additionalTransportImplementations = BaseRealtime.transportImplementationsFromPlugins(this.options.plugins);
     this._RealtimePresence = this.options.plugins?.RealtimePresence ?? null;

src/common/lib/client/baserest.ts
@@ -9,7 +9,15 @@ import Defaults from '../util/defaults';
  It always includes the `Rest` plugin.
  */
 export class BaseRest extends BaseClient {
-  constructor(options: ClientOptions | string) {
-    super(Defaults.objectifyOptions(options, { Rest }));
+  /*
+   * The public typings declare that this only accepts an object, but since we want to emit a good error message in the case where a non-TypeScript user does one of these things:
+   *
+   * 1. passes a string (which is quite likely if they’re e.g. migrating from the default variant to the modular variant)
+   * 2. passes no argument at all
+   *
+   * tell the compiler that these cases are possible so that it forces us to handle them.
+   */
+  constructor(options?: ClientOptions | string) {
+    super(Defaults.objectifyOptions(options, false, 'BaseRest', { Rest }));
   }
 }

src/common/lib/client/defaultrealtime.ts
@@ -22,14 +22,15 @@ import Defaults from '../util/defaults';
  `DefaultRealtime` is the class that the non tree-shakable version of the SDK exports as `Realtime`. It ensures that this version of the SDK includes all of the functionality which is optionally available in the tree-shakable version.
  */
 export class DefaultRealtime extends BaseRealtime {
-  constructor(options: ClientOptions | string) {
+  // The public typings declare that this requires an argument to be passed, but since we want to emit a good error message in the case where a non-TypeScript user does not pass an argument, tell the compiler that this is possible so that it forces us to handle it.
+  constructor(options?: ClientOptions | string) {
     const MsgPack = DefaultRealtime._MsgPack;
     if (!MsgPack) {
       throw new Error('Expected DefaultRealtime._MsgPack to have been set');
     }
 
     super(
-      Defaults.objectifyOptions(options, {
+      Defaults.objectifyOptions(options, true, 'Realtime', {
         ...allCommonModularPlugins,
         Crypto: DefaultRealtime.Crypto ?? undefined,
         MsgPack,

src/common/lib/client/defaultrest.ts
@@ -12,14 +12,15 @@ import Defaults from '../util/defaults';
  `DefaultRest` is the class that the non tree-shakable version of the SDK exports as `Rest`. It ensures that this version of the SDK includes all of the functionality which is optionally available in the tree-shakable version.
  */
 export class DefaultRest extends BaseRest {
-  constructor(options: ClientOptions | string) {
+  // The public typings declare that this requires an argument to be passed, but since we want to emit a good error message in the case where a non-TypeScript user does not pass an argument, tell the compiler that this is possible so that it forces us to handle it.
+  constructor(options?: ClientOptions | string) {
     const MsgPack = DefaultRest._MsgPack;
     if (!MsgPack) {
       throw new Error('Expected DefaultRest._MsgPack to have been set');
     }
 
     super(
-      Defaults.objectifyOptions(options, {
+      Defaults.objectifyOptions(options, true, 'Rest', {
         ...allCommonModularPlugins,
         Crypto: DefaultRest.Crypto ?? undefined,
         MsgPack: DefaultRest._MsgPack ?? undefined,

src/common/lib/util/defaults.ts
@@ -44,7 +44,12 @@ type CompleteDefaults = IDefaults & {
   getHosts(options: NormalisedClientOptions): string[];
   checkHost(host: string): void;
   getRealtimeHost(options: ClientOptions, production: boolean, environment: string): string;
-  objectifyOptions(options: ClientOptions | string, modularPluginsToInclude?: ModularPlugins): ClientOptions;
+  objectifyOptions(
+    options: undefined | ClientOptions | string,
+    allowKeyOrToken: boolean,
+    sourceForErrorMessage: string,
+    modularPluginsToInclude?: ModularPlugins,
+  ): ClientOptions;
   normaliseOptions(options: ClientOptions, MsgPack: MsgPack | null): NormalisedClientOptions;
   defaultGetHeaders(options: NormalisedClientOptions, headersOptions?: HeadersOptions): Record<string, string>;
   defaultPostHeaders(options: NormalisedClientOptions, headersOptions?: HeadersOptions): Record<string, string>;
@@ -183,11 +188,42 @@ export function getAgentString(options: ClientOptions): string {
 }
 
 export function objectifyOptions(
-  options: ClientOptions | string,
+  options: undefined | ClientOptions | string,
+  allowKeyOrToken: boolean,
+  sourceForErrorMessage: string,
   modularPluginsToInclude?: ModularPlugins,
 ): ClientOptions {
-  let optionsObj =
-    typeof options === 'string' ? (options.indexOf(':') == -1 ? { token: options } : { key: options }) : options;
+  if (options === undefined) {
+    const msg = allowKeyOrToken
+      ? `${sourceForErrorMessage} must be initialized with either a client options object, an Ably API key, or an Ably Token`
+      : `${sourceForErrorMessage} must be initialized with a client options object`;
+    Logger.logAction(Logger.LOG_ERROR, `${sourceForErrorMessage}()`, msg);
+    throw new Error(msg);
+  }
+
+  let optionsObj: ClientOptions;
+
+  if (typeof options === 'string') {
+    if (options.indexOf(':') == -1) {
+      if (!allowKeyOrToken) {
+        const msg = `${sourceForErrorMessage} cannot be initialized with just an Ably Token; you must provide a client options object with a \`plugins\` property. (Set this Ably Token as the object’s \`token\` property.)`;
+        Logger.logAction(Logger.LOG_ERROR, `${sourceForErrorMessage}()`, msg);
+        throw new Error(msg);
+      }
+
+      optionsObj = { token: options };
+    } else {
+      if (!allowKeyOrToken) {
+        const msg = `${sourceForErrorMessage} cannot be initialized with just an Ably API key; you must provide a client options object with a \`plugins\` property. (Set this Ably API key as the object’s \`key\` property.)`;
+        Logger.logAction(Logger.LOG_ERROR, `${sourceForErrorMessage}()`, msg);
+        throw new Error(msg);
+      }
+
+      optionsObj = { key: options };
+    }
+  } else {
+    optionsObj = options;
+  }
 
   if (modularPluginsToInclude) {
     optionsObj = { ...optionsObj, plugins: { ...modularPluginsToInclude, ...optionsObj.plugins } };

test/browser/modular.test.js
@@ -42,6 +42,40 @@ function registerAblyModularTests(helper) {
       helper.setupApp(done);
     });
 
+    describe('attempting to initialize with no client options', () => {
+      for (const clientClass of [BaseRest, BaseRealtime]) {
+        describe(clientClass.name, () => {
+          it('throws an error', () => {
+            expect(() => new clientClass()).to.throw('must be initialized with a client options object');
+          });
+        });
+      }
+    });
+
+    describe('attempting to initialize with just an API key', () => {
+      for (const clientClass of [BaseRest, BaseRealtime]) {
+        describe(clientClass.name, () => {
+          it('throws an error', () => {
+            expect(() => new clientClass('foo:bar')).to.throw(
+              'cannot be initialized with just an Ably API key; you must provide a client options object with a `plugins` property',
+            );
+          });
+        });
+      }
+    });
+
+    describe('attempting to initialize with just a token', () => {
+      for (const clientClass of [BaseRest, BaseRealtime]) {
+        describe(clientClass.name, () => {
+          it('throws an error', () => {
+            expect(() => new clientClass('foo')).to.throw(
+              'cannot be initialized with just an Ably Token; you must provide a client options object with a `plugins` property',
+            );
+          });
+        });
+      }
+    });
+
     describe('without any plugins', () => {
       for (const clientClass of [BaseRest, BaseRealtime]) {
         describe(clientClass.name, () => {

test/realtime/api.test.js
@@ -8,6 +8,12 @@ define(['ably', 'chai'], function (Ably, chai) {
       expect(typeof Ably.Realtime).to.equal('function');
     });
 
+    it('constructor without any arguments', function () {
+      expect(() => new Ably.Realtime()).to.throw(
+        'must be initialized with either a client options object, an Ably API key, or an Ably Token',
+      );
+    });
+
     it('Crypto', function () {
       expect(typeof Ably.Realtime.Crypto).to.equal('function');
       expect(typeof Ably.Realtime.Crypto.getDefaultParams).to.equal('function');

test/rest/api.test.js
@@ -8,6 +8,12 @@ define(['ably', 'chai'], function (Ably, chai) {
       expect(typeof Ably.Rest).to.equal('function');
     });
 
+    it('constructor without any arguments', function () {
+      expect(() => new Ably.Rest()).to.throw(
+        'must be initialized with either a client options object, an Ably API key, or an Ably Token',
+      );
+    });
+
     it('Crypto', function () {
       expect(typeof Ably.Rest.Crypto).to.equal('function');
       expect(typeof Ably.Rest.Crypto.getDefaultParams).to.equal('function');