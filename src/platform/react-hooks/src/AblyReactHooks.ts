b8950355190dbe004fe0a5053a2b49c3ee14ef22
Andrew Bulat <andrii.bulat@gmail.com>
2024-03-25 15:27:57 +0000

    chore: bump version for 2.0.1 release

--- src/platform/react-hooks/src/AblyReactHooks.ts
+++ src/platform/react-hooks/src/AblyReactHooks.ts
@@ -12,7 +12,7 @@ export type ChannelNameAndOptions = {
 export type ChannelNameAndAblyId = Pick<ChannelNameAndOptions, 'channelName' | 'ablyId'>;
 export type ChannelParameters = string | ChannelNameAndOptions;
 
-export const version = '2.0.0';
+export const version = '2.0.1';
 
 export function channelOptionsWithAgent(options?: Ably.ChannelOptions) {
   return {
