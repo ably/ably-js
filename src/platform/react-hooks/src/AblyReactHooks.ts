c41e2373873d9fe97288ed5133c0a2f9a93c78e4
owenpearson <owen.pearson@ably.com>
2024-07-10 15:53:38 +0100

    chore: bump version for 2.3.0 release

--- src/platform/react-hooks/src/AblyReactHooks.ts
+++ src/platform/react-hooks/src/AblyReactHooks.ts
@@ -12,7 +12,7 @@ export type ChannelNameAndOptions = {
 export type ChannelNameAndAblyId = Pick<ChannelNameAndOptions, 'channelName' | 'ablyId'>;
 export type ChannelParameters = string | ChannelNameAndOptions;
 
-export const version = '2.2.1';
+export const version = '2.3.0';
 
 export function channelOptionsWithAgent(options?: Ably.ChannelOptions) {
   return {
