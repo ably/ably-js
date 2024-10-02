ae331d9209852081b4cc1ccf08e7dd4d62c50ae4
Andrew Bulat <andrii.bulat@gmail.com>
2024-10-02 04:13:46 +0100

    Implement LiveCounter access API
    Resolves DTP-960

--- src/plugins/liveobjects/livecounter.ts
+++ src/plugins/liveobjects/livecounter.ts
@@ -5,6 +5,10 @@ export interface LiveCounterData {
 }
 
 export class LiveCounter extends LiveObject<LiveCounterData> {
+  value(): number {
+    return this._dataRef.data;
+  }
+
   protected _getZeroValueData(): LiveCounterData {
     return { data: 0 };
   }
