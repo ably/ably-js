9a691c4b12c4d71734617357d3eeea9dda5e6664
Andrew Bulat <andrii.bulat@gmail.com>
2024-10-09 06:44:32 +0100

    Add StateMessage to LiveObjects plugin

--- src/plugins/liveobjects/index.ts
+++ src/plugins/liveobjects/index.ts
@@ -1,7 +1,9 @@
 import { LiveObjects } from './liveobjects';
+import { StateMessage } from './statemessage';
 
-export { LiveObjects };
+export { LiveObjects, StateMessage };
 
 export default {
   LiveObjects,
+  StateMessage,
 };
