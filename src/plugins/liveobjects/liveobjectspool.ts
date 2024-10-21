8cf7ec8499644c8d03f10e58b06f3e39cb1672b1
Andrew Bulat <andrii.bulat@gmail.com>
2024-10-21 13:17:54 +0100

    Implement application of incoming state operation messages outside of sync sequence
    Resolves DTP-956

--- src/plugins/liveobjects/liveobjectspool.ts
+++ src/plugins/liveobjects/liveobjectspool.ts
@@ -1,10 +1,11 @@
 import type BaseClient from 'common/lib/client/baseclient';
+import type RealtimeChannel from 'common/lib/client/realtimechannel';
 import { LiveCounter } from './livecounter';
 import { LiveMap } from './livemap';
 import { LiveObject } from './liveobject';
 import { LiveObjects } from './liveobjects';
 import { ObjectId } from './objectid';
-import { MapSemantics } from './statemessage';
+import { MapSemantics, StateMessage, StateOperation, StateOperationAction } from './statemessage';
 
 export const ROOT_OBJECT_ID = 'root';
 
@@ -13,10 +14,12 @@ export const ROOT_OBJECT_ID = 'root';
  */
 export class LiveObjectsPool {
   private _client: BaseClient;
+  private _channel: RealtimeChannel;
   private _pool: Map<string, LiveObject>;
 
   constructor(private _liveObjects: LiveObjects) {
     this._client = this._liveObjects.getClient();
+    this._channel = this._liveObjects.getChannel();
     this._pool = this._getInitialPool();
   }
 
@@ -63,10 +66,90 @@ export class LiveObjectsPool {
     this.set(objectId, zeroValueObject);
   }
 
+  applyStateMessages(stateMessages: StateMessage[]): void {
+    for (const stateMessage of stateMessages) {
+      if (!stateMessage.operation) {
+        this._client.Logger.logAction(
+          this._client.logger,
+          this._client.Logger.LOG_MAJOR,
+          'LiveObjects.LiveObjectsPool.applyStateMessages()',
+          `state operation message is received without 'operation' field, skipping message; message id: ${stateMessage.id}, channel: ${this._channel.name}`,
+        );
+        continue;
+      }
+
+      const stateOperation = stateMessage.operation;
+
+      switch (stateOperation.action) {
+        case StateOperationAction.MAP_CREATE:
+        case StateOperationAction.COUNTER_CREATE:
+          if (this.get(stateOperation.objectId)) {
+            // object wich such id already exists (we may have created a zero-value object before, or this is a duplicate *_CREATE op),
+            // so delegate application of the op to that object
+            // TODO: invoke subscription callbacks for an object when applied
+            this.get(stateOperation.objectId)!.applyOperation(stateOperation, stateMessage);
+            break;
+          }
+
+          // otherwise we can create new objects in the pool
+          if (stateOperation.counter) {
+            this._handleCounterCreate(stateOperation);
+          } else if (stateOperation.map) {
+            this._handleMapCreate(stateOperation);
+          } else {
+            this._client.Logger.logAction(
+              this._client.logger,
+              this._client.Logger.LOG_MAJOR,
+              'LiveObjects.LiveObjectsPool.applyStateMessages()',
+              `received unsupported operation in state operation message, expected 'counter' or 'map' to be present, skipping message; message id: ${stateMessage.id}, channel: ${this._channel.name}`,
+            );
+          }
+          break;
+
+        case StateOperationAction.MAP_SET:
+        case StateOperationAction.MAP_REMOVE:
+        case StateOperationAction.COUNTER_INC:
+          // we can receive an op for an object id we don't have yet in the pool. instead of buffering such operations,
+          // we create a zero-value object for the provided object id, and apply operation for that zero-value object.
+          // when we eventually receive a corresponding *_CREATE op for that object, its application will be handled by that zero-value object.
+          this.createZeroValueObjectIfNotExists(stateOperation.objectId);
+          // TODO: invoke subscription callbacks for an object when applied
+          this.get(stateOperation.objectId)!.applyOperation(stateOperation, stateMessage);
+          break;
+
+        default:
+          this._client.Logger.logAction(
+            this._client.logger,
+            this._client.Logger.LOG_MAJOR,
+            'LiveObjects.LiveObjectsPool.applyStateMessages()',
+            `received unsupported action in state operation message: ${stateOperation.action}, skipping message; message id: ${stateMessage.id}, channel: ${this._channel.name}`,
+          );
+      }
+    }
+  }
+
   private _getInitialPool(): Map<string, LiveObject> {
     const pool = new Map<string, LiveObject>();
     const root = new LiveMap(this._liveObjects, MapSemantics.LWW, null, ROOT_OBJECT_ID);
     pool.set(root.getObjectId(), root);
     return pool;
+  }
+
+  private _handleCounterCreate(stateOperation: StateOperation): void {
+    const counter = stateOperation.counter!;
+    this.set(
+      stateOperation.objectId,
+      new LiveCounter(this._liveObjects, true, { data: counter.count ?? 0 }, stateOperation.objectId),
+    );
+  }
+
+  private _handleMapCreate(stateOperation: StateOperation): void {
+    const map = stateOperation.map!;
+    const objectData = LiveMap.liveMapDataFromMapEntries(this._client, map.entries ?? {});
+
+    this.set(
+      stateOperation.objectId,
+      new LiveMap(this._liveObjects, map.semantics ?? MapSemantics.LWW, objectData, stateOperation.objectId),
+    );
   }
 }
