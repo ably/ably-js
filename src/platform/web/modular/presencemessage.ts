a35dfa85a955331b462f6f0e760d54f913272d8e
Simon Woolf <simon.woolf@ably.com>
2024-12-05 18:25:28 +0000

    Add decryption-capable versions of modular presencemessage decode functions

--- src/platform/web/modular/presencemessage.ts
+++ src/platform/web/modular/presencemessage.ts
@@ -1,5 +1,6 @@
 import * as API from '../../../../ably';
 import { fromEncoded, fromEncodedArray, fromValues } from '../../../common/lib/types/presencemessage';
+import { Crypto } from './crypto';
 import Logger from '../../../common/lib/util/logger';
 
 // The type assertions for the functions below are due to https://github.com/ably/ably-js/issues/1421
@@ -8,8 +9,16 @@ export const decodePresenceMessage = ((obj, options) => {
   return fromEncoded(Logger.defaultLogger, null, obj, options);
 }) as API.PresenceMessageStatic['fromEncoded'];
 
+export const decodeEncryptedPresenceMessage = ((obj, options) => {
+  return fromEncoded(Logger.defaultLogger, Crypto, obj, options);
+}) as API.PresenceMessageStatic['fromEncoded'];
+
 export const decodePresenceMessages = ((obj, options) => {
   return fromEncodedArray(Logger.defaultLogger, null, obj, options);
+}) as API.PresenceMessageStatic['fromEncodedArray'];
+
+export const decodeEncryptedPresenceMessages = ((obj, options) => {
+  return fromEncodedArray(Logger.defaultLogger, Crypto, obj, options);
 }) as API.PresenceMessageStatic['fromEncodedArray'];
 
 export const constructPresenceMessage = fromValues as API.PresenceMessageStatic['fromValues'];
