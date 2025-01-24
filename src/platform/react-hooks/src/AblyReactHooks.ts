65ea196af254f192c5a455b3132388d107eae458
evgeny <khokhlov.e.n@gmail.com>
2025-01-24 13:07:23 +0000

    chore: bump version for 2.6.3 release

--- src/platform/react-hooks/src/AblyReactHooks.ts
+++ src/platform/react-hooks/src/AblyReactHooks.ts
@@ -12,7 +12,7 @@ export type ChannelNameAndOptions = {
 export type ChannelNameAndAblyId = Pick<ChannelNameAndOptions, 'channelName' | 'ablyId'>;
 export type ChannelParameters = string | ChannelNameAndOptions;
 
-export const version = '2.6.2';
+export const version = '2.6.3';
 
 /**
  * channel options for react-hooks
