82117264cf9effab3a5daf4750ddb6b5323a951c
Laura Martin <laura.martin@ably.com>
2025-05-22 15:53:11 +0100

    fixup! fixup! fixup! Add endpoint option

--- test/rest/defaults.test.js
+++ test/rest/defaults.test.js
@@ -45,6 +45,122 @@ define(['ably', 'chai'], function (Ably, chai) {
     });
 
     /**
+     * @spec TO3k8
+     * @spec TO3k4
+     * @spec TO3k5
+     * @spec TO3k6
+     * @spec TO3d
+     * @spec REC1a
+     */
+    it('Init with given endpoint', function () {
+      const helper = this.test.helper;
+
+      helper.recordPrivateApi('call.Defaults.normaliseOptions');
+      var normalisedOptions = Defaults.normaliseOptions({ endpoint: 'nonprod:sandbox' }, null, null);
+
+      expect(normalisedOptions.port).to.equal(80);
+      expect(normalisedOptions.tlsPort).to.equal(443);
+      expect(normalisedOptions.fallbackHosts.sort()).to.deep.equal(
+        Defaults.getEndpointFallbackHosts('nonprod:sandbox').sort(),
+      );
+      expect(normalisedOptions.tls).to.equal(true);
+
+      helper.recordPrivateApi('call.Defaults.getHosts');
+      expect(Defaults.getHosts(normalisedOptions).length).to.deep.equal(4);
+      expect(Defaults.getHosts(normalisedOptions)[0]).to.deep.equal(normalisedOptions.restHost);
+      helper.recordPrivateApi('call.Defaults.getHost');
+      expect(Defaults.getHost(normalisedOptions, 'sandbox.realtime.ably-nonprod.net', false)).to.deep.equal(
+        'sandbox.realtime.ably-nonprod.net',
+      );
+      expect(Defaults.getHost(normalisedOptions, 'sandbox.realtime.ably-nonprod.net', true)).to.deep.equal(
+        'sandbox.realtime.ably-nonprod.net',
+      );
+
+      helper.recordPrivateApi('call.Defaults.getPort');
+      expect(Defaults.getPort(normalisedOptions)).to.equal(443);
+    });
+
+    /**
+     * @spec TO3k8
+     * @spec REC1b2
+     */
+    it('Init with given endpoint as FQDN', function () {
+      const helper = this.test.helper;
+
+      helper.recordPrivateApi('call.Defaults.normaliseOptions');
+      var normalisedOptions = Defaults.normaliseOptions({ endpoint: 'example.com' }, null, null);
+
+      helper.recordPrivateApi('call.Defaults.getHosts');
+      expect(Defaults.getHosts(normalisedOptions).length).to.deep.equal(1);
+      helper.recordPrivateApi('call.Defaults.getHost');
+      expect(Defaults.getHost(normalisedOptions, 'example.com', false)).to.deep.equal('example.com');
+      expect(Defaults.getHost(normalisedOptions, 'example.com', true)).to.deep.equal('example.com');
+
+      helper.recordPrivateApi('call.Defaults.getPort');
+      expect(Defaults.getPort(normalisedOptions)).to.equal(443);
+    });
+
+    /**
+     * @spec TO3k8
+     * @spec REC1b2
+     */
+    it('Init with given endpoint as IPv4 address', function () {
+      const helper = this.test.helper;
+
+      helper.recordPrivateApi('call.Defaults.normaliseOptions');
+      var normalisedOptions = Defaults.normaliseOptions({ endpoint: '127.0.0.1' }, null, null);
+
+      helper.recordPrivateApi('call.Defaults.getHosts');
+      expect(Defaults.getHosts(normalisedOptions).length).to.deep.equal(1);
+      helper.recordPrivateApi('call.Defaults.getHost');
+      expect(Defaults.getHost(normalisedOptions, '127.0.0.1', false)).to.deep.equal('example.com');
+      expect(Defaults.getHost(normalisedOptions, '127.0.0.1', true)).to.deep.equal('example.com');
+
+      helper.recordPrivateApi('call.Defaults.getPort');
+      expect(Defaults.getPort(normalisedOptions)).to.equal(443);
+    });
+
+    /**
+     * @spec TO3k8
+     * @spec REC1b2
+     */
+    it('Init with given endpoint as IPv6 address', function () {
+      const helper = this.test.helper;
+
+      helper.recordPrivateApi('call.Defaults.normaliseOptions');
+      var normalisedOptions = Defaults.normaliseOptions({ endpoint: '::1' }, null, null);
+
+      helper.recordPrivateApi('call.Defaults.getHosts');
+      expect(Defaults.getHosts(normalisedOptions).length).to.deep.equal(1);
+      helper.recordPrivateApi('call.Defaults.getHost');
+      expect(Defaults.getHost(normalisedOptions, '::1', false)).to.deep.equal('example.com');
+      expect(Defaults.getHost(normalisedOptions, '::1', true)).to.deep.equal('example.com');
+
+      helper.recordPrivateApi('call.Defaults.getPort');
+      expect(Defaults.getPort(normalisedOptions)).to.equal(443);
+    });
+
+    /**
+     * @spec TO3k8
+     * @spec REC1b2
+     */
+    it('Init with given endpoint as localhost', function () {
+      const helper = this.test.helper;
+
+      helper.recordPrivateApi('call.Defaults.normaliseOptions');
+      var normalisedOptions = Defaults.normaliseOptions({ endpoint: 'localhost' }, null, null);
+
+      helper.recordPrivateApi('call.Defaults.getHosts');
+      expect(Defaults.getHosts(normalisedOptions).length).to.deep.equal(1);
+      helper.recordPrivateApi('call.Defaults.getHost');
+      expect(Defaults.getHost(normalisedOptions, 'localhost', false)).to.deep.equal('example.com');
+      expect(Defaults.getHost(normalisedOptions, 'localhost', true)).to.deep.equal('example.com');
+
+      helper.recordPrivateApi('call.Defaults.getPort');
+      expect(Defaults.getPort(normalisedOptions)).to.equal(443);
+    });
+
+    /**
      * @spec TO3k1
      * @spec TO3k2
      * @spec TO3k3
