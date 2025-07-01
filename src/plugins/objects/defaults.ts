fc70477704fafa224545840f9a4054d35e6bab3b
Andrew Bulat <andrii.bulat@gmail.com>
2025-07-01 04:38:19 +0100

    Use server-provided GC grace period for tombstone removal
    `gcGracePeriod` is now set from connectionDetails received on CONNECTED
    event. Added to realtime in [1]
    
    Resolves PUB-1702
    
    [1] https://ably.atlassian.net/browse/PUB-1127

--- src/plugins/objects/defaults.ts
+++ src/plugins/objects/defaults.ts
@@ -1,6 +1,9 @@
 export const DEFAULTS = {
   gcInterval: 1000 * 60 * 5, // 5 minutes
   /**
+   * The SDK will attempt to use the `gcGracePeriod` value provided by the server in the `connectionDetails` object of the `CONNECTED` event.
+   * If the server does not provide this value, the SDK will fall back to this default value.
+   *
    * Must be > 2 minutes to ensure we keep tombstones long enough to avoid the possibility of receiving an operation
    * with an earlier serial that would not have been applied if the tombstone still existed.
    *
